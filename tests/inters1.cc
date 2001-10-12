2b915ff7a9287d14c3ec3826293137c778e83033
Roberto Bagnara <bagnara@cs.unipr.it>
2001-10-12 07:55:09 +0000

    Infrastructure for the tests directory and `make check'. inters1.cc is a first example of how test programs could be written.

---
+++ tests/inters1.cc
@@ -0,0 +1,114 @@
+/* Intersection of an icosahedron with a column.
+   Copyright (C) 2001 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include <config.h>
+#include "ppl.hh"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#define NOISY 0
+
+void
+print(const Polyhedron& ph, const string& intro = "", ostream& s = cout) {
+  if (!intro.empty())
+    s << intro << endl;
+  if (ph.is_zero_dim())
+    s << "true.";
+  else if (ph.check_empty())
+    s << "false.";
+  else {
+    const ConSys& cs = ph.constraints();
+    ConSys::const_iterator i = cs.begin();
+    ConSys::const_iterator cs_end = cs.end();
+    while (i != cs_end) {
+      s << *i++;
+      if (i != cs_end)
+	s << "," << endl;
+    }
+    s << "." << endl;
+  }
+}
+
+int
+main() {
+  Variable x(0);
+  Variable y(1);
+  Variable z(2);
+ 
+  Polyhedron p1(3);
+  p1.insert(4*x - 2*y - z + 14 >= 0);
+  p1.insert(4*x + 2*y - z + 2 >= 0);
+  p1.insert(x + y - 1 >= 0);
+  p1.insert(x + y + 2*z - 5 >= 0); 
+  p1.insert(x + 1 >= 0); 
+  p1.insert(x + z - 1 >= 0); 
+  p1.insert(2*x + y -2*z + 7 >= 0); 
+  p1.insert(x - y + 2*z + 1 >= 0); 
+  p1.insert(x - y + 5 >= 0); 
+  p1.insert(2*x - y - 2*z + 13 >= 0); 
+  p1.insert(-2*x - y + 2*z + 1 >= 0); 
+  p1.insert(-x + y - 1 >= 0); 
+  p1.insert(-x + y -2*z + 7 >= 0); 
+  p1.insert(-4*x + 2*y + z - 4 >= 0); 
+  p1.insert(-2*x + y + 2*z - 5 >= 0); 
+  p1.insert(-x + 1 >= 0); 
+  p1.insert(-x - z + 5 >= 0); 
+  p1.insert(-4*x - 2*y + z + 8 >= 0); 
+  p1.insert(-x - y + 5 >= 0); 
+  p1.insert(-x - y -2*z +13 >= 0); 
+
+  Polyhedron p2(3);
+  p2.insert(y >= 2);
+  p2.insert(y <= 4);
+  p2.insert(x >= 0);
+  p2.insert(x <= 1);
+  
+  Polyhedron computed_result = p1;
+  computed_result.intersection_assign(p2);
+
+  Polyhedron known_result(3);
+  known_result.insert(-4*x - 2*y + z >= -8);
+  known_result.insert(-4*x + 2*y + z >= 4);
+  known_result.insert(-2*x - y + 2*z >= -1);
+  known_result.insert(-2*x + y + 2*z >= 5);
+  known_result.insert(-x - y - 2*z >= -13);
+  known_result.insert(-x - z >= -5);
+  known_result.insert(-x >= -1);
+  known_result.insert(-x + y - 2*z >= -7);
+  known_result.insert(-y >= -4);
+  known_result.insert(y >= 2);
+  known_result.insert(x >= 0);
+
+  int retval = (computed_result == known_result) ? 0 : 1;
+
+#if NOISY
+  print(p1, "*** p1 ***");
+  print(p2, "*** p2 ***");
+  print(computed_result, "*** computed_result ***");
+#endif
+
+  return retval;
+}
