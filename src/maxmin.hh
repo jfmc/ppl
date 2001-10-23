e09b20e850dc5fa155d2cd72ca46bf0c935e03d9
Roberto Bagnara <bagnara@cs.unipr.it>
2001-10-23 09:39:18 +0000

    Put everything in our namespace.

--- src/maxmin.hh
+++ src/maxmin.hh
@@ -2,34 +2,37 @@
 #ifndef _maxmin_hh
 #define _maxmin_hh 1
 
-inline int
-max(int x, int y) {
-  return x < y ? y : x;
-}
+namespace Parma_Polyhedra_Library {
 
-inline int
-min(int x, int y) {
-  return x < y ? x : y;
-}
+  inline int
+  max(int x, int y) {
+    return x < y ? y : x;
+  }
 
-inline int
-max(unsigned int x, unsigned int y) {
-  return x < y ? y : x;
-}
+  inline int
+  min(int x, int y) {
+    return x < y ? x : y;
+  }
 
-inline int
-min(unsigned int x, unsigned int y) {
-  return x < y ? x : y;
-}
+  inline int
+  max(unsigned int x, unsigned int y) {
+    return x < y ? y : x;
+  }
 
-inline int
-max(char x, char y) {
-  return x < y ? y : x;
-}
+  inline int
+  min(unsigned int x, unsigned int y) {
+    return x < y ? x : y;
+  }
 
-inline int
-min(char x, char y) {
-  return x < y ? x : y;
-}
+  inline int
+  max(char x, char y) {
+    return x < y ? y : x;
+  }
 
+  inline int
+  min(char x, char y) {
+    return x < y ? x : y;
+  }
+
+} // namespace Parma_Polyhedra_Library
 #endif
