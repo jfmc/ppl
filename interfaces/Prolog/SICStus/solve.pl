ae44709cbcad49adfbb6211e568594643f8b4330
Patricia M. Hill <hill@comp.leeds.ac.uk>
2001-10-30 12:25:07 +0000

    More improvements.

--- interfaces/Prolog/SICStus/solve.pl
+++ interfaces/Prolog/SICStus/solve.pl
@@ -2,13 +2,16 @@
 ?- use_module(library(clpq)).
 
 solve(Goal):-
-    numbervars(Goal,0,M),
-    ppl_new_polyhedron(P, M),
-    solve(Goal,P,M,_),
+    ppl_new_polyhedron(P, 0),
+    solve(Goal,P,0,_),
     check_constraints(P).
 
 solve(true,_P,M,M):-
     !.
+
+solve(A=B,P,M,N):-
+    solve({A==B},P,M,N).
+
 solve((A,B),P,M,N):- 
     !, 
     solve(A,P,M,M1),
@@ -16,18 +19,20 @@ solve((A,B),P,M,N):-
 solve({Cs},P,M,N):- 
     !,
     numbervars(Cs,M,N),
+    M1 is N-M,
+    ppl_add_dimensions_and_embed(P, M1),
     solve_constraints(Cs,P).
 solve(A,P,M,N):-
     clause(A,B), 
-    numbervars(B,M,N1),
-    M1 is N1-M,
-    ppl_add_dimensions_and_embed(P, M1),
-    solve(B,P,N1,N).
+    solve(B,P,M,N).
 
 solve_constraints((C,D),P):- 
     !,
     solve_constraints(C,P),
     solve_constraints(D,P).
+solve_constraints(A=B,P):- 
+    !,
+    solve_constraints(A==B,P).
 solve_constraints(C,P):- 
     ppl_insert_constraint(P,C).
 
@@ -46,6 +51,7 @@ check_constraints(X) :-
 
 % ?- solve({X+Y>=3,Y>=0,X=<2}).
 
-:- dynamic p1/2, p2/3.
+:- dynamic p1/2, p2/3, p3/2.
 p1(A,B):- {A>=B}, p2(A,B,C).
 p2(X,Y,Z):- {X+Y=<4}.
+p3(X,Y):- {X+Y==4}.
