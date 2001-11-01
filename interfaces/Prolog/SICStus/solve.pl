c911cfbb511fceb1e128ad4dc3336b542321c82f
Patricia M. Hill <hill@comp.leeds.ac.uk>
2001-11-01 13:34:33 +0000

    Names of terms made more descriptive.

--- interfaces/Prolog/SICStus/solve.pl
+++ interfaces/Prolog/SICStus/solve.pl
@@ -4,22 +4,23 @@
 solve(Goal):-
     ppl_new_polyhedron(Polyhedron, 0),
     solve(Goal,Polyhedron,0,_),
-    check_constraints(Polyhedron).
+    check_constraints(Polyhedron),
+    ppl_delete_polyhedron(Polyhedron).
 
-solve(true,_Polyhedron,M,M):-
+solve(true,_Polyhedron,Dims,Dims):-
     !.
 
-solve(A=B,Polyhedron,M,N):-
-    solve({A=B},Polyhedron,M,N).
+solve(A=B,Polyhedron,InDims,OutDims):-
+    solve({A=B},Polyhedron,InDims,OutDims).
 
-solve((A,B),Polyhedron,M,N):- 
+solve((A,B),Polyhedron,InDims,OutDims):- 
     !, 
     ppl_copy_polyhedron(Polyhedron,Q),
-    solve(A,Polyhedron,M,M1),
-    solve(B,Polyhedron,M1,N),
+    solve(A,Polyhedron,InDims,AOutDims),
+    solve(B,Polyhedron,AOutDims,OutDims),
     (ppl_check_empty(Polyhedron)
      ->
-     ppl_renew_polyhedron(Polyhedron,M),
+     ppl_renew_polyhedron(Polyhedron,InDims),
      ppl_copy_polyhedron(Q,Polyhedron),
      fail
      ;
@@ -34,36 +35,36 @@ solve({Cs},Polyhedron,M,N):-
     ppl_add_dimensions_and_embed(Polyhedron, M1),
     solve_constraints(Cs,Polyhedron).
 
-solve(Atom,Polyhedron,M,N):-
+solve(Atom,Polyhedron,InDims,OutDims):-
     functor(Atom,Pred,Arity),
     functor(Atom1,Pred,Arity),
     clause(Atom1,Body1),
     Atom =.. [Pred|Args],
     Atom1 =.. [Pred|Args1],
     rename(Args,Args1),
-    numbervars(Args,M,N),
-    numbervars(Args1,N,_),
-    NewDims is N - M,
-    try_clause(Args,Args1,Body1,M,NewDims,Polyhedron).
+    numbervars(Args,InDims,OutDims),
+    numbervars(Args1,OutDims,_),
+    AddedDims is OutDims - InDims,
+    try_clause(Args,Args1,Body1,InDims,AddedDims,Polyhedron).
 
 
-try_clause(Args,Args1,Body1,M,NewDims,Polyhedron):-
+try_clause(Args,Args1,Body1,InDims,AddedDims,Polyhedron):-
     ppl_copy_polyhedron(Polyhedron,Q),
-    M2 is 2* NewDims,
-    ppl_add_dimensions_and_embed(Q, M2),
+    QAddedDims is 2* AddedDims,
+    ppl_add_dimensions_and_embed(Q, QAddedDims),
     solve_equal_list(Args,Args1,Q),
-    M3 is M + M2,
-    solve(Body1,Q,M3,_),
+    QInDims is InDims + QAddedDims,
+    solve(Body1,Q,QInDims,_),
     (ppl_check_empty(Q)
      ->
-     (ppl_renew_polyhedron(Q,M3),
+     (ppl_renew_polyhedron(Q,QInDims),
      fail)
      ;
-     M1 is M + NewDims,
-     ppl_project_dimensions(Q,M1),
-     ppl_get_constraints(Q,CQs),
-     ppl_add_dimensions_and_embed(Polyhedron, NewDims),
-     ppl_insert_constraints(Polyhedron,CQs)
+     OutDims is InDims + AddedDims,
+     ppl_project_dimensions(Q,OutDims),
+     ppl_get_constraints(Q,QConstraints),
+     ppl_add_dimensions_and_embed(Polyhedron, AddedDims),
+     ppl_insert_constraints(Polyhedron,QConstraints)
     ),
     ppl_delete_polyhedron(Q).
 
@@ -87,27 +88,18 @@ solve_equal_list([A|As],[B|Bs],Polyhedron):-
     solve_constraints(A=B,Polyhedron),
     solve_equal_list(As,Bs,Polyhedron).
 
-ppl_renew_polyhedron(Polyhedron,M):-
-    ppl_space_dimension(Polyhedron,D),
-    MaxCode is D,
-    MinCode is M,
-    make_var_list(MaxCode,MinCode,VarList),
+ppl_renew_polyhedron(Polyhedron,NewDims):-
+    ppl_space_dimension(Polyhedron,CurrentDims),
+    make_var_list(CurrentDims,NewDims,VarList),
     ppl_remove_dimensions(Polyhedron, VarList),
-    ppl_new_polyhedron(Q,M),
+    ppl_new_polyhedron(Q,NewDims),
     ppl_convex_hull_assign(Polyhedron,Q),
-    ppl_delete_polyhedron(Q).
+    ppl_delete_polyhedron(Q).  
 
-/*
-ppl_renew_polyhedron(Polyhedron,M):-
-    ppl_delete_polyhedron(Polyhedron),
-    ppl_new_polyhedron(Polyhedron,M).
-*/
-    
-
-ppl_project_dimensions(Polyhedron,M):-
-    ppl_space_dimension(Polyhedron,D),
-    MaxCode is D,
-    MinCode is M,
+ppl_project_dimensions(Polyhedron,NewDims):-
+    ppl_space_dimension(Polyhedron,CurrentDims),
+    MaxCode is CurrentDims,
+    MinCode is NewDims,
     make_var_list(MaxCode,MinCode,VarList),
     ppl_remove_dimensions(Polyhedron, VarList).
 
@@ -160,3 +152,18 @@ p4(A,B):- {A >= B + 1}.
 p4(A,B):- {A = B}.
 
 runp4(A,B):- solve((p4(A,B),{A=1,B=1})).
+
+% To run all the tests at once.
+runall([A,B,C,D,E,F,X]):- 
+   runp1(A,B),nl, runp3(C,D),nl, runp4(E,F),nl, runfib(4,X),nl.
+
+/*
+Test results
+| ?- runall(L).
+2 * [1*A+ -1*B>=0,-1*A+ -1*B>= -4] %
+2 * [1*A+1*B=4] %
+2 * [1*A=1,1*B=1] %
+1 * [1*A=5] %
+ 
+L = [A,B,A,B,A,B,A] ?
+*/
