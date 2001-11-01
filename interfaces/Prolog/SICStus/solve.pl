6644ae6ff17f9c7ecf37f98814af61bad87dae6f
Patricia M. Hill <hill@comp.leeds.ac.uk>
2001-11-01 16:27:59 +0000

    Bug is fixed and test added to check bug is dead. Also, more care is taken to remove polyhedron that are no longer needed.

--- interfaces/Prolog/SICStus/solve.pl
+++ interfaces/Prolog/SICStus/solve.pl
@@ -16,7 +16,7 @@ solve(A=B,Polyhedron,InDims,OutDims):-
 solve((A,B),Polyhedron,InDims,OutDims):- 
     !, 
     ppl_copy_polyhedron(Polyhedron,Q),
-    (try_solve((A,B),Polyhedron,InDims,OutDims,Q)
+    (conjunct_solve(A,B,Polyhedron,InDims,OutDims,Q)
      ->
      ppl_delete_polyhedron(Q)
      ;
@@ -24,23 +24,11 @@ solve((A,B),Polyhedron,InDims,OutDims):-
      fail
     ).
 
-try_solve((A,B),Polyhedron,InDims,OutDims,Q):-
-    solve(A,Polyhedron,InDims,AOutDims),
-    solve(B,Polyhedron,AOutDims,OutDims),
-    (ppl_check_empty(Polyhedron)
-     ->
-     ppl_renew_polyhedron(Polyhedron,InDims),
-     ppl_copy_polyhedron(Q,Polyhedron),
-     fail
-     ;
-     true
-    ).
-
-solve({Cs},Polyhedron,M,N):- 
+solve({Cs},Polyhedron,InDims,OutDims):- 
     !,
-    numbervars(Cs,M,N),
-    M1 is N-M,
-    ppl_add_dimensions_and_embed(Polyhedron, M1),
+    numbervars(Cs,InDims,OutDims),
+    AddedDims is OutDims-InDims,
+    ppl_add_dimensions_and_embed(Polyhedron, AddedDims),
     solve_constraints(Cs,Polyhedron).
 
 solve(Atom,Polyhedron,InDims,OutDims):-
@@ -52,29 +40,47 @@ solve(Atom,Polyhedron,InDims,OutDims):-
     rename(Args,Args1),
     numbervars(Args,InDims,OutDims),
     numbervars(Args1,OutDims,_),
-    AddedDims is OutDims - InDims,
-    try_clause(Args,Args1,Body1,InDims,AddedDims,Polyhedron).
+%    AddedDims is OutDims - InDims,
+%    try_clause(Args,Args1,Body1,InDims,AddedDims,Polyhedron).
+    try_clause(Args,Args1,Body1,InDims,OutDims,Polyhedron).
 
 
-try_clause(Args,Args1,Body1,InDims,AddedDims,Polyhedron):-
+try_clause(Args,Args1,Body1,InDims,OutDims,Polyhedron):-
     ppl_copy_polyhedron(Polyhedron,Q),
-    QAddedDims is 2* AddedDims,
-    ppl_add_dimensions_and_embed(Q, QAddedDims),
-    solve_equal_list(Args,Args1,Q),
-    QInDims is InDims + QAddedDims,
-    solve(Body1,Q,QInDims,_),
-    (ppl_check_empty(Q)
+    AddedDims is 2 * (OutDims - InDims),
+    ppl_add_dimensions_and_embed(Polyhedron, AddedDims),
+    solve_equal_list(Args,Args1,Polyhedron),
+    NewInDims is InDims + AddedDims,
+  (
+    (solve(Body1,Polyhedron,NewInDims,_),
+    (ppl_check_empty(Polyhedron)
      ->
-     (ppl_renew_polyhedron(Q,QInDims),
-     fail)
-     ;
-     OutDims is InDims + AddedDims,
-     ppl_project_dimensions(Q,OutDims),
+     ppl_renew_polyhedron(Polyhedron,InDims),
      ppl_get_constraints(Q,QConstraints),
-     ppl_add_dimensions_and_embed(Polyhedron, AddedDims),
-     ppl_insert_constraints(Polyhedron,QConstraints)
-    ),
-    ppl_delete_polyhedron(Q).
+     ppl_insert_constraints(Polyhedron,QConstraints),
+     fail
+     ;
+     ppl_project_dimensions(Polyhedron,OutDims)
+    ))
+  ->
+    ppl_delete_polyhedron(Q)
+  ;
+    ppl_delete_polyhedron(Q),
+    fail
+  ).
+
+conjunct_solve(A,B,Polyhedron,InDims,OutDims,Q):-
+    solve(A,Polyhedron,InDims,AOutDims),
+    solve(B,Polyhedron,AOutDims,OutDims),
+    (ppl_check_empty(Polyhedron)
+     ->
+     ppl_renew_polyhedron(Polyhedron,InDims),
+     ppl_get_constraints(Q,QConstraints),
+     ppl_insert_constraints(Polyhedron,QConstraints),
+     fail
+     ;
+     true
+    ).
 
 solve_constraints((C,D),Polyhedron):- 
     !,
@@ -160,18 +166,23 @@ p4(A,B):- {A >= B + 1}.
 p4(A,B):- {A = B}.
 
 runp4(A,B):- solve((p4(A,B),{A=1,B=1})).
+runp4a(A,B):- solve(({A=1}, p4(A,B),{B=1})).
 
 % To run all the tests at once.
-runall([A,B,C,D,E,F,X]):- 
-   runp1(A,B),nl, runp3(C,D),nl, runp4(E,F),nl, runfib(4,X),nl.
+runall:- 
+   runp1(_A,_B),nl, 
+   runp3(_C,_D),nl, 
+   runp4(_E,_F),nl, 
+   runp4a(_E1,_F1),nl, 
+   runfib(4,_X),nl.
 
 /*
 Test results
-| ?- runall(L).
+| ?- runall.
 2 * [1*A+ -1*B>=0,-1*A+ -1*B>= -4] %
 2 * [1*A+1*B=4] %
 2 * [1*A=1,1*B=1] %
+2 * [1*A=1,1*B=1] %
 1 * [1*A=5] %
- 
-L = [A,B,A,B,A,B,A] ?
+
 */
