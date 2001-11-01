da1c0cd8eb7cc325c9e8780a7750b2078a979c4c
Patricia M. Hill <hill@comp.leeds.ac.uk>
2001-11-01 12:53:54 +0000

    Now temporary polyhedra are deleted when they are no longer needed.

--- interfaces/Prolog/SICStus/solve.pl
+++ interfaces/Prolog/SICStus/solve.pl
@@ -2,57 +2,58 @@
 ?- use_module(library(clpq)).
 
 solve(Goal):-
-    ppl_new_polyhedron(P, 0),
-    solve(Goal,P,0,_),
-    check_constraints(P).
+    ppl_new_polyhedron(Polyhedron, 0),
+    solve(Goal,Polyhedron,0,_),
+    check_constraints(Polyhedron).
 
-solve(true,_P,M,M):-
+solve(true,_Polyhedron,M,M):-
     !.
 
-solve(A=B,P,M,N):-
-    solve({A=B},P,M,N).
+solve(A=B,Polyhedron,M,N):-
+    solve({A=B},Polyhedron,M,N).
 
-solve((A,B),P,M,N):- 
+solve((A,B),Polyhedron,M,N):- 
     !, 
-    ppl_copy_polyhedron(P,Q),
-    solve(A,P,M,M1),
-    solve(B,P,M1,N),
-    (ppl_check_empty(P)
+    ppl_copy_polyhedron(Polyhedron,Q),
+    solve(A,Polyhedron,M,M1),
+    solve(B,Polyhedron,M1,N),
+    (ppl_check_empty(Polyhedron)
      ->
-     ppl_renew_polyhedron(P,M),
-     ppl_copy_polyhedron(Q,P),
-     check, fail
+     ppl_renew_polyhedron(Polyhedron,M),
+     ppl_copy_polyhedron(Q,Polyhedron),
+     fail
      ;
      true
-    ).
+    ),
+    ppl_delete_polyhedron(Q).
 
-solve({Cs},P,M,N):- 
+solve({Cs},Polyhedron,M,N):- 
     !,
     numbervars(Cs,M,N),
     M1 is N-M,
-    ppl_add_dimensions_and_embed(P, M1),
-    solve_constraints(Cs,P).
+    ppl_add_dimensions_and_embed(Polyhedron, M1),
+    solve_constraints(Cs,Polyhedron).
 
-solve(A,P,M,N):-
-    functor(A,Pred,Arity),
-    functor(A1,Pred,Arity),
-    clause(A1,B1),
-    A =.. [Pred|Args],
-    A1 =.. [Pred|Args1],
+solve(Atom,Polyhedron,M,N):-
+    functor(Atom,Pred,Arity),
+    functor(Atom1,Pred,Arity),
+    clause(Atom1,Body1),
+    Atom =.. [Pred|Args],
+    Atom1 =.. [Pred|Args1],
     rename(Args,Args1),
     numbervars(Args,M,N),
     numbervars(Args1,N,_),
     NewDims is N - M,
-    try_clause(Args,Args1,B1,M,NewDims,P).
+    try_clause(Args,Args1,Body1,M,NewDims,Polyhedron).
 
 
-try_clause(Args,Args1,B1,M,NewDims,P):-
-    ppl_copy_polyhedron(P,Q),
+try_clause(Args,Args1,Body1,M,NewDims,Polyhedron):-
+    ppl_copy_polyhedron(Polyhedron,Q),
     M2 is 2* NewDims,
     ppl_add_dimensions_and_embed(Q, M2),
     solve_equal_list(Args,Args1,Q),
     M3 is M + M2,
-    solve(B1,Q,M3,_),
+    solve(Body1,Q,M3,_),
     (ppl_check_empty(Q)
      ->
      (ppl_renew_polyhedron(Q,M3),
@@ -61,52 +62,54 @@ try_clause(Args,Args1,B1,M,NewDims,P):-
      M1 is M + NewDims,
      ppl_project_dimensions(Q,M1),
      ppl_get_constraints(Q,CQs),
-     ppl_add_dimensions_and_embed(P, NewDims),
-     ppl_insert_constraints(P,CQs)
-    ).
+     ppl_add_dimensions_and_embed(Polyhedron, NewDims),
+     ppl_insert_constraints(Polyhedron,CQs)
+    ),
+    ppl_delete_polyhedron(Q).
 
-solve_constraints((C,D),P):- 
+solve_constraints((C,D),Polyhedron):- 
     !,
-    solve_constraints(C,P),
-    solve_constraints(D,P).
-solve_constraints(C,P):- 
-    ppl_insert_constraint(P,C).
+    solve_constraints(C,Polyhedron),
+    solve_constraints(D,Polyhedron).
+solve_constraints(C,Polyhedron):- 
+    ppl_insert_constraint(Polyhedron,C).
     
 rename([],[]).
-rename([A|As],[B|Bs]):-
+rename([A|As],[_B|Bs]):-
     var(A),
     !,
     rename(As,Bs).
 rename([A|As],[A|Bs]):-
     rename(As,Bs).
 
-solve_equal_list([],[],P).
-solve_equal_list([A|As],[B|Bs],P):-
-    solve_constraints(A=B,P),
-    solve_equal_list(As,Bs,P).
+solve_equal_list([],[],_Polyhedron).
+solve_equal_list([A|As],[B|Bs],Polyhedron):-
+    solve_constraints(A=B,Polyhedron),
+    solve_equal_list(As,Bs,Polyhedron).
 
-ppl_renew_polyhedron(P,M):-
-    ppl_space_dimension(P,D),
+ppl_renew_polyhedron(Polyhedron,M):-
+    ppl_space_dimension(Polyhedron,D),
     MaxCode is D,
     MinCode is M,
     make_var_list(MaxCode,MinCode,VarList),
-    ppl_remove_dimensions(P, VarList),
+    ppl_remove_dimensions(Polyhedron, VarList),
     ppl_new_polyhedron(Q,M),
-    ppl_convex_hull_assign(P,Q).
+    ppl_convex_hull_assign(Polyhedron,Q),
+    ppl_delete_polyhedron(Q).
 
 /*
-ppl_renew_polyhedron(P,M):-
-    ppl_delete_polyhedron(P),
-    ppl_new_polyhedron(P,M).
+ppl_renew_polyhedron(Polyhedron,M):-
+    ppl_delete_polyhedron(Polyhedron),
+    ppl_new_polyhedron(Polyhedron,M).
 */
     
 
-ppl_project_dimensions(P,M):-
-    ppl_space_dimension(P,D),
+ppl_project_dimensions(Polyhedron,M):-
+    ppl_space_dimension(Polyhedron,D),
     MaxCode is D,
     MinCode is M,
     make_var_list(MaxCode,MinCode,VarList),
-    ppl_remove_dimensions(P, VarList).
+    ppl_remove_dimensions(Polyhedron, VarList).
 
 make_var_list(MaxCode,MaxCode,[]).
 make_var_list(MaxCode,VarCode,['$VAR'(VarCode)|VarList]):-
@@ -114,13 +117,13 @@ make_var_list(MaxCode,VarCode,['$VAR'(VarCode)|VarList]):-
     VarCode1 is VarCode+1,
     make_var_list(MaxCode,VarCode1,VarList).
 
-check_constraints(X) :-
-    ppl_space_dimension(X, D),
+check_constraints(Polyhedron) :-
+    ppl_space_dimension(Polyhedron, D),
     write(D), write(' * '),
-    ppl_get_constraints(X, CS),
+    ppl_get_constraints(Polyhedron, CS),
     write(CS), write(' % ')
 %,
-%	ppl_get_generators(X, GS),
+%	ppl_get_generators(Polyhedron, GS),
 %	write(GS), write(' ')
 .
 
@@ -133,8 +136,8 @@ check.
 
 % Some basic checks
 :- dynamic p1/2, p2/3, p3/2, p4/2.
-p1(A,B):- {A>=B}, p2(A,B,C).
-p2(X,Y,Z):- {X+Y=<4}.
+p1(A,B):- {A>=B}, p2(A,B,_C).
+p2(X,Y,_Z):- {X+Y=<4}.
 p3(X,Y):- {X+Y=4}.
 
 runp1(A,B):- solve(p1(A,B)).
