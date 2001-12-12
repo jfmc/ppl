f1ece0cf3347b835867af217bd17de1c9bd2329a
Roberto Bagnara <bagnara@cs.unipr.it>
2001-12-12 17:41:39 +0000

    New test.

---
+++ tests/addconstraints3.cc
@@ -0,0 +1,53 @@
+/* Adding constraints from a different space dimension.
+   Copyright (C) 2001 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_install.hh"
+#include "print.hh"
+#include "ehandlers.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#define NOISY 0
+
+int
+main() try {
+  set_handlers();
+
+  Variable x(0);
+  Variable y(1);
+
+  ConSys cs;
+  cs.insert(x + y >= 0);
+  Polyhedron p(cs);
+
+  LinExpression e(1);
+  ConSys c;
+  c.insert(e == 0);
+  p.add_constraints(c);
+
+  return 0;
+}
+catch (...) {
+  return 1;
+}
