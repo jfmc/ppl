bb9d4295e9be5eb6286bce7e9716f88ee7143c92
Elisa Ricci <ericci@no.email>
2001-12-13 15:52:48 +0000

    Added a test for the function Polyhedron::add_generators_and_minimize(gs) and one for the function Polyhedron::add_generators(gs).

---
+++ tests/addgenerators1.cc
@@ -0,0 +1,67 @@
+/* Adds a system of generators to a polyhedron and minimizes it.
+   Copyright (C) 2001 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_install.hh"
+#include "print.hh"
+#include "ehandlers.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#define NOISY 0
+
+int
+main() {
+  set_handlers();
+  
+  Variable x(0);
+  Variable y(1);
+  
+  GenSys gs1;
+  gs1.insert(vertex(x));
+  gs1.insert(vertex(y));
+  gs1.insert(line(x));
+  Polyhedron ph1(gs1);
+
+#if NOISY
+  print_generators(ph1, "*** before ***");
+#endif
+
+  GenSys gs2;
+  gs2.insert(vertex());
+  
+  ph1.add_generators_and_minimize(gs2);
+  
+#if NOISY
+  print_generators(ph1, "*** add_generators_and_minimize ***");
+#endif
+
+  Polyhedron known_result(2, Polyhedron::EMPTY);
+  known_result.insert(vertex());
+  known_result.insert(vertex(y));
+  known_result.insert(line(x));
+
+  int retval = (ph1 == known_result) ? 0: 1;
+
+  return retval;
+}
---
+++ tests/addgenerators2.cc
@@ -0,0 +1,64 @@
+/* Adds a system of generators to an empty polyhedron.
+   Copyright (C) 2001 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_install.hh"
+#include "print.hh"
+#include "ehandlers.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#define NOISY 0
+
+int
+main() {
+  set_handlers();
+
+  Variable x(0);
+
+  Polyhedron ph1(2, Polyhedron::EMPTY);
+  
+#if NOISY
+  print_generators(ph1, "*** before ***");
+#endif
+  
+  GenSys gs;
+  gs.insert(vertex());
+  gs.insert(line(x));
+  
+  
+  ph1.add_generators(gs);
+  
+#if NOISY
+  print_generators(ph1, "*** add_generators ***");
+#endif
+  
+  Polyhedron known_result(2, Polyhedron::EMPTY);
+  known_result.insert(vertex());
+  known_result.insert(line(x));
+  
+  int retval = (ph1 == known_result) ? 0 : 1;
+  
+  return retval;
+}
+
