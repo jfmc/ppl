6425a3d647cb2314b2571dffa579480ca25d4f40
Roberto Bagnara <bagnara@cs.unipr.it>
2002-02-06 07:06:01 +0000

    bug2 shows another problem this time cropping up in SatMatrix::columns_erase_to_end().

---
+++ tests/bug2.cc
@@ -0,0 +1,29 @@
+#include <config.h>
+
+#include "ppl_install.hh"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+int
+main() {
+  ifstream s("bug2.dat");
+  if (!s) {
+    cerr << "Cannot open data file!!!" << endl;
+    exit(1);
+  }
+
+  Polyhedron x;
+  Polyhedron y;
+  assert(x.OK());
+  assert(y.OK());
+
+  s >> x;
+  s >> y;
+
+  // Now see the program explode in unexpected and interesting ways.
+  x.convex_hull_assign_and_minimize(y);
+
+}
