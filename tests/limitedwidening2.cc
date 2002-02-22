46db2eec9befb78ec9c24b04467d175efe3bbb64
Roberto Bagnara <bagnara@cs.unipr.it>
2002-02-22 18:47:29 +0000

    New test to check that the case where the passed constraint system is inconsistent with respect to the widened polyhedron. Note: it currently fails, since Polyhedron::limited_widening_assign() is buggy, as it is now.

---
+++ tests/limitedwidening2.cc
@@ -0,0 +1,80 @@
+/* Testing Polyhedron::limited_widening().
+   Copyright (C) 2001 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_install.hh"
+#include "print.hh"
+#include "ehandlers.hh"
+#include <iostream>
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#define NOISY 1
+
+int
+main() {
+  Variable x(0);
+  Variable y(1);
+
+  ConSys cs1;
+  cs1.insert(x >= 0);
+  cs1.insert(x <= 1);
+  cs1.insert(y == 0);
+
+  Polyhedron ph1(cs1);
+
+#if NOISY
+  print_constraints(ph1, "*** ph1 ****");
+#endif
+
+  ConSys cs2;
+  cs2.insert(x <= 2);
+  cs2.insert(y >= 0);
+  cs2.insert(y <= x);
+
+  Polyhedron ph2(cs2);
+
+#if NOISY
+  print_constraints(ph2, "*** ph2 ****");
+#endif
+
+  // Note: this is inconsistent with both `ph1' and `ph2'.
+  ConSys cs(y <= -1);
+
+#if NOISY
+  print_constraints(cs, "*** cs ****");
+#endif
+
+  Polyhedron computed_result = ph2;
+  bool retval = computed_result.limited_widening_assign(ph1, cs);
+
+#if NOISY
+  cout << "limited_widening_assign returned "
+       << (retval ? "true" : "false")
+       << endl;
+  print_constraints(computed_result, "*** After limited_widening_assign ****");
+#endif
+
+  // Must have returned `false' and the result must be empty.
+  return (!retval && computed_result.check_empty()) ? 0 : 1;
+}
