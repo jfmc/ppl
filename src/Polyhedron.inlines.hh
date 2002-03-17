e09ad0bd2775782433668834173744a82c576b6e
Roberto Bagnara <bagnara@cs.unipr.it>
2002-03-17 07:15:32 +0000

    Add missing `inline' qualifiers.

--- src/Polyhedron.inlines.hh
+++ src/Polyhedron.inlines.hh
@@ -27,28 +27,33 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 namespace Parma_Polyhedra_Library {
 
+inline
 Polyhedron::Polyhedron(size_t num_dimensions, Degenerate_Kind kind)
   : PolyBase(NECESSARILY_CLOSED, num_dimensions, kind) {
 }
 
+inline
 Polyhedron::Polyhedron(const Polyhedron& y)
   : PolyBase(y) {
 }
 
+inline
 Polyhedron::Polyhedron(ConSys& cs)
   : PolyBase(NECESSARILY_CLOSED, cs) {
 }
 
+inline
 Polyhedron::Polyhedron(GenSys& gs)
   : PolyBase(NECESSARILY_CLOSED, gs) {
 }
 
-Polyhedron&
+inline Polyhedron&
 Polyhedron::operator=(const Polyhedron& y) {
   PolyBase::operator=(y);
   return *this;
 }
 
+inline
 Polyhedron::~Polyhedron() {
 }
 
