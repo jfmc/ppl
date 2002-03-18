b12e651d3646555a52be8cd7e18807510a1620d1
Enea Zaffanella <zaffanella@cs.unipr.it>
2002-03-18 23:50:57 +0000

    Methods reordered according to Polyhedron.defs.hh.

--- src/Polyhedron.inlines.hh
+++ src/Polyhedron.inlines.hh
@@ -33,11 +33,6 @@ Polyhedron::Polyhedron(size_t num_dimensions, Degenerate_Kind kind)
 }
 
 inline
-Polyhedron::Polyhedron(const Polyhedron& y)
-  : PolyBase(y) {
-}
-
-inline
 Polyhedron::Polyhedron(ConSys& cs)
   : PolyBase(NECESSARILY_CLOSED, cs) {
 }
@@ -45,6 +40,11 @@ Polyhedron::Polyhedron(ConSys& cs)
 inline
 Polyhedron::Polyhedron(GenSys& gs)
   : PolyBase(NECESSARILY_CLOSED, gs) {
+}
+
+inline
+Polyhedron::Polyhedron(const Polyhedron& y)
+  : PolyBase(y) {
 }
 
 inline Polyhedron&
