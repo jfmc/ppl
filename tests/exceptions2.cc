f6c3bccf49f691c9a2ccb4598dff7a9e421d71ee
Elisa Ricci <ericci@no.email>
2002-03-20 10:20:57 +0000

    Added some other tests that show when an exception is thrown: one of them shows that there is still a problem with the function Matrix::grow().

--- tests/exceptions2.cc
+++ tests/exceptions2.cc
@@ -43,8 +43,8 @@ error1() {
   
   try {
     // This is an invalid use of the constructor of a polyhedron:
-    // it is impossible to built a closed polyhedron starting from a system
-    // of constraints that contains strict-inequalities.
+    // it is impossible to built a closed polyhedron starting from
+    // a system of constraints that contains strict-inequalities.
     Polyhedron ph(cs);
   }
   catch(invalid_argument& e) {
@@ -72,8 +72,8 @@ error2() {
   
   try {
     // This is an invalid use of the constructor of a polyhedron:
-    // it is impossible to built a closed polyhedron starting from a system
-    // of generators that contains closure points.
+    // it is impossible to built a closed polyhedron starting from
+    // a system of generators that contains closure points.
     Polyhedron ph(gs);
   }
   catch(invalid_argument& e) {
@@ -86,11 +86,184 @@ error2() {
   }
 }
 
+void
+error3() {
+  set_handlers();
+
+  Variable x(0);
+  Variable y(1);
+
+  Polyhedron ph(3);
+  
+  try {
+    // This is an invalid use of the function insert(c): it is impossible
+    // to insert a strict-inequality into a system of constraints of
+    // a closed polyhedron.
+    ph.insert(x - y > 0);
+  }
+  catch(invalid_argument& e) {
+#if NOISY
+    cout << "invalid_constraint: " << e.what() << endl << endl;
+#endif
+  }
+  catch (...) {
+    exit(1);
+  }
+}
+
+void
+error4() {
+  set_handlers();
+  
+  Polyhedron ph(3, Polyhedron::EMPTY);
+
+  try {
+    // This is an incorrect use of the function insert(g): it is impossible
+    // to insert a closure-point into a system of generators of a closed
+    // polyhedron.
+    ph.insert(closure_point(LinExpression(2)));
+  }
+  catch(invalid_argument& e) {
+# if NOISY
+    cout << "invalid_generator: " << e.what() << endl << endl;
+#endif
+  }
+  catch (...) {
+    exit(1);
+  }
+}
+
+void
+error5() {
+  set_handlers();
+  
+  Variable x(0);
+  Variable y(1);
+
+  Polyhedron ph(3);
+  ph.insert(x >= 2);
+  ph.insert(y >= 2);
+
+  ConSys cs;
+  cs.insert(x == y);
+  cs.insert(x < 5);
+
+  try {
+    // This is an incorrect use of the function
+    // add_constraints_and_minimize(cs): it is impossible to add a system of
+    // constraints that contains strict-inequalities to a closed polyhedron.
+    ph.add_constraints_and_minimize(cs);
+  }
+  catch(invalid_argument& e) {
+#if NOISY
+    cout << "invalid_system_of_constraints: " << e.what() << endl << endl;
+#endif
+  }
+  catch (...) {
+    exit(1);
+  }
+}
+
+void
+error6() {
+  set_handlers();
+  
+  Variable x(0);
+  Variable y(1);
+
+  Polyhedron ph(3);
+  ConSys cs;
+  cs.insert(x > 3);
+  cs.insert(x > y);
+
+  try {
+    // This is an invalid use of the function add_constraints(cs):
+    // it is impossible to add a system of constraints that constains
+    // strict-inequalities to a closed polyhedron.
+    ph.add_constraints(cs);
+  }
+  catch(invalid_argument& e) {
+#if NOISY   
+    cout << "invalid_system_of_constraints: " << e.what() << endl << endl;
+#endif
+  }
+  catch (...) {
+    exit(1);
+  }
+}
+
+void
+error7() {
+  set_handlers();
+  
+  Variable x(0);
+  Variable y(1);
+
+  Polyhedron ph(3);
+
+  ConSys cs;
+  cs.insert(x > 2);
+  cs.insert(x == y);
+  
+  try {
+    // This is an incorrect use of the function
+    // add_dimensions_and_constraints(cs): it is impossible to apply
+    // this function to a closed polyhedron with a system of constraints
+    // that contains strict-inequalities.
+    ph.add_dimensions_and_constraints(cs);
+  }
+  catch(invalid_argument& e) {
+#if NOISY
+    cout << "invalid_system_of_constraints: " << e.what() << endl << endl;
+#endif
+  }
+  catch (...) {
+    exit(1);
+  }
+}
+
+void
+error8() {
+  set_handlers();
+  
+  Variable x(0);
+  Variable y(1);
+
+  Polyhedron ph(3);
+  GenSys gs;
+  gs.insert(point());
+  gs.insert(closure_point(-x));
+  gs.insert(ray(x));
+  gs.insert(ray(y));
+
+  try {
+    // This is an incorrect use of the function
+    // add_generators_and_minimize(gs): it is impossible to add a
+    // system of generators that constains closure-points to a closed
+    // polyhedron.
+    ph.add_generators_and_minimize(gs);
+  }
+  catch(invalid_argument& e) {
+#if NOISY
+    cout << "invalid_system_of_generators: " << e.what() << endl << endl;
+#endif
+  }
+  catch (...) {
+    exit(1);
+  }
+}
+
 int
 main() {
   
   error1();
   error2();
+  error3();
+  error4();
+  error5();
+  error6();
+  error7();
+  error8();
 
   return 0;
 }
