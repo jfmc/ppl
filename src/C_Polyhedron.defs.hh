097b9a6029d79915333ec00a2992b040416b368f
Enea Zaffanella <zaffanella@cs.unipr.it>
2002-04-03 08:48:57 +0000

    Added wrapper class C_Polyhedron to implement necessarily closed polyhedra.

---
+++ src/C_Polyhedron.defs.hh
@@ -0,0 +1,103 @@
+/* C_Polyhedron class declaration.
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef _C_Polyhedron_defs_hh
+#define _C_Polyhedron_defs_hh 1
+
+#include "C_Polyhedron.types.hh"
+#include "NNC_Polyhedron.types.hh"
+#include "PolyBase.defs.hh"
+
+//! A necessarily closed convex polyhedron.
+/*!
+    An object of the class C_Polyhedron represents a
+    <EM>necessarily closed</EM> convex polyhedron
+    in the vector space \f$\Rset^n\f$.
+
+    When building a necessarily closed polyhedron starting from
+    a system of constraints, an exception is thrown if the system
+    contains a <EM>strict inequality</EM> constraint.
+    Similarly, an exception is thrown when building a necessarily closed
+    polyhedron starting from a system of generators containing a
+    <EM>closure point</EM>.
+
+    \note
+    Such an exception will be obtained even if the system of
+    constraints (resp., generators) actually defines
+    a topologically closed subset of the vector space, i.e.,
+    even if all the strict inequalities (resp., closure points)
+    in the system happen to be redundant with respect to the
+    system obtained by removing all the strict inequality constraints
+    (resp., all the closure points).
+    In contrast, when building a necessarily closed polyhedron
+    starting from an object of the class NNC_Polyhedron,
+    the precise topological closure test will be performed.
+*/
+
+class Parma_Polyhedra_Library::C_Polyhedron : public PolyBase {
+public:
+
+  //! Builds either the universe or the empty polyhedron of dimension
+  //! \p num_dimensions. Both parameters are optional:
+  //! by default, a 0-dimension space universe polyhedron is built.
+  explicit C_Polyhedron(size_t num_dimensions = 0,
+			Degenerate_Kind kind = UNIVERSE);
+
+  //! Builds a polyhedron from a system of constraints.
+  //! The polyhedron inherits the space dimension of the constraint system.
+  //! \param cs       The system of constraints defining the polyhedron.
+  //!                 It is not declared <CODE>const</CODE>
+  //!                 because it can be modified.
+  //! \exception std::invalid_argument thrown if the system of constraints
+  //!                                  contains strict inequalities.
+  C_Polyhedron(ConSys& cs);
+
+  //! Builds a polyhedron from a system of generators.
+  //! The polyhedron inherits the space dimension of the generator system.
+  //! \param gs       The system of generators defining the polyhedron.
+  //!                 It is not declared <CODE>const</CODE>
+  //!                 because it can be modified.
+  //! \exception std::invalid_argument thrown if the system of generators
+  //!                                  is not empty but has no points,
+  //!                                  or if it contains closure points.
+  C_Polyhedron(GenSys& gs);
+
+  //! Ordinary copy-constructor.
+  C_Polyhedron(const C_Polyhedron& y);
+
+  //! Builds a necessarily closed polyhedron from the NNC_Polyhedron \p y.
+  //! \exception std::invalid_argument thrown if the polyhedron \p y
+  //!                                  is not topologically closed.
+  explicit C_Polyhedron(const NNC_Polyhedron& y);
+
+  //! The assignment operator.
+  //! (Note that \p *this and \p y can be dimension-incompatible.)
+  C_Polyhedron& operator=(const C_Polyhedron& y);
+
+  // Destructor
+  ~C_Polyhedron();
+};
+
+#include "C_Polyhedron.inlines.hh"
+
+#endif
---
+++ src/C_Polyhedron.inlines.hh
@@ -0,0 +1,62 @@
+/* C_Polyhedron class implementation: inline functions.
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+
+#ifndef _C_Polyhedron_inlines_hh
+#define _C_Polyhedron_inlines_hh 1
+
+namespace Parma_Polyhedra_Library {
+
+inline
+C_Polyhedron::C_Polyhedron(size_t num_dimensions, Degenerate_Kind kind)
+  : PolyBase(NECESSARILY_CLOSED, num_dimensions, kind) {
+}
+
+inline
+C_Polyhedron::C_Polyhedron(ConSys& cs)
+  : PolyBase(NECESSARILY_CLOSED, cs) {
+}
+
+inline
+C_Polyhedron::C_Polyhedron(GenSys& gs)
+  : PolyBase(NECESSARILY_CLOSED, gs) {
+}
+
+inline
+C_Polyhedron::C_Polyhedron(const C_Polyhedron& y)
+  : PolyBase(y) {
+}
+
+inline C_Polyhedron&
+C_Polyhedron::operator=(const C_Polyhedron& y) {
+  PolyBase::operator=(y);
+  return *this;
+}
+
+inline
+C_Polyhedron::~C_Polyhedron() {
+}
+
+} // namespace Parma_Polyhedra_Library
+
+#endif
