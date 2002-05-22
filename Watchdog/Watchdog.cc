4cf1259562d103802ab17b968a82bac7fc4fbfc9
Roberto Bagnara <bagnara@cs.unipr.it>
2002-05-22 09:02:18 +0000

    Even though the Watchdog library is completely independent from the PPL, it will live with it, at least for a while.

---
+++ Watchdog/Watchdog.cc
@@ -0,0 +1,226 @@
+/* Watchdog and associated classes' implementation (non-inline functions).
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include <config.h>
+
+#include "Watchdog.hh"
+
+#include <csignal>
+#include <iostream>
+#include <ctime>
+#include <sys/time.h>
+
+namespace PWL = Parma_Watchdog_Library;
+
+using std::cerr;
+using std::endl;
+
+// Pass this to getitimer.
+itimerval PWL::Watchdog::current_timer_status;
+
+// Pass this to setitimer.
+itimerval PWL::Watchdog::signal_once;
+
+// Last time value we set the timer to.
+PWL::Watchdog::Time PWL::Watchdog::last_time_requested;
+
+// Records the time elapsed since last fresh start.
+PWL::Watchdog::Time PWL::Watchdog::time_so_far;
+
+// The ordered queue of pending watchdog events.
+PWL::Watchdog::Pending PWL::Watchdog::pending;
+
+// Whether the alarm clock is running.
+volatile bool PWL::Watchdog::alarm_clock_running = false;
+
+// Whether we are changing data which are also changed by the signal handler.
+volatile bool PWL::Watchdog::in_critical_section = false;
+
+void
+PWL::Watchdog::get_timer(Time& time) {
+  getitimer(ITIMER_PROF, &current_timer_status);
+  time.seconds = current_timer_status.it_value.tv_sec;
+  time.microseconds = current_timer_status.it_value.tv_usec;
+}
+
+void
+PWL::Watchdog::set_timer(const Time& time) {
+  if (time.seconds == 0 && time.microseconds == 0)
+    abort();
+  last_time_requested = time;
+  signal_once.it_value.tv_sec = time.seconds;
+  signal_once.it_value.tv_usec = time.microseconds;
+  setitimer(ITIMER_PROF, &signal_once, 0);
+}
+
+void
+PWL::Watchdog::stop_timer() {
+  signal_once.it_value.tv_sec = 0;
+  signal_once.it_value.tv_usec = 0;
+  setitimer(ITIMER_PROF, &signal_once, 0);
+}
+
+void
+PWL::Watchdog::handle_timeout(int) {
+  if (in_critical_section)
+    reschedule();
+  else {
+    time_so_far += last_time_requested;
+    if (!pending.empty()) {
+      // Why the hell these cannot be a const_iterators?
+      Pending::iterator i(pending.begin());
+      Pending::iterator in;
+      do {
+	(*i).handler->act();
+	*((*i).p_expired_flag) = true;
+	in = i;
+	++in;
+	pending.erase(i);
+	i = in;
+      } while (i != pending.end() && (*i).deadline <= time_so_far);
+      if (pending.empty())
+	alarm_clock_running = false;
+      else
+	set_timer((*pending.begin()).deadline - time_so_far);
+    }
+    else
+      alarm_clock_running = false;
+  }
+}
+
+PWL::Watchdog::Pending::iterator
+PWL::Watchdog::insert_pending(const Time& deadline,
+			 const Handler* handler,
+			 bool* p_expired) {
+  Pending::iterator i(pending.begin());
+  Pending::iterator pend(pending.end());
+  while (i != pend && (*i).deadline < deadline)
+    ++i;
+  return pending.insert(i, Pending::value_type(deadline, handler, p_expired));
+}
+
+PWL::Watchdog::Pending::iterator
+PWL::Watchdog::new_watchdog_event(int units,
+			     const Handler* handler,
+			     bool* p_expired) {
+  assert(units > 0);
+  Pending::iterator position;
+  Time deadline(units);
+  if (!alarm_clock_running) {
+    position = insert_pending(deadline, handler, p_expired);
+    time_so_far.reset();
+    set_timer(deadline);
+    alarm_clock_running = true;
+  }
+  else {
+    Time time_to_shoot;
+    get_timer(time_to_shoot);
+    Time elapsed_time(last_time_requested);
+    elapsed_time -= time_to_shoot;
+    Time current_time(time_so_far);
+    current_time += elapsed_time;
+    Time real_deadline(deadline);
+    real_deadline += current_time;
+    position = insert_pending(real_deadline, handler, p_expired);
+    if (deadline < time_to_shoot) {
+      time_so_far = current_time;
+      set_timer(deadline);
+    }
+  }
+  return position;
+}
+
+void
+PWL::Watchdog::remove_watchdog_event(Pending::iterator position) {
+  assert(!pending.empty());
+  if (position == pending.begin()) {
+    Pending::iterator next(position);
+    ++next;
+    if (next != pending.end()) {
+      Time first_deadline((*position).deadline);
+      Time next_deadline((*next).deadline);
+      if (first_deadline != next_deadline) {
+	Time time_to_shoot;
+	get_timer(time_to_shoot);
+	Time elapsed_time(last_time_requested);
+	elapsed_time -= time_to_shoot;
+	time_so_far += elapsed_time;
+	next_deadline -= first_deadline;
+	time_to_shoot += next_deadline;
+	set_timer(time_to_shoot);
+      }
+    }
+    else {
+      stop_timer();
+      alarm_clock_running = false;
+    }
+  }
+  pending.erase(position);
+}
+
+PWL::Watchdog::Watchdog(int units,
+		   Timeout& exception,
+		   volatile Timeout** flag)
+  : expired(false), handler(new Handler_Flag(exception, flag)) {
+  in_critical_section = true;
+  pending_position = new_watchdog_event(units, handler, &expired);
+  in_critical_section = false;
+}
+
+PWL::Watchdog::Watchdog(int units, void (*function)())
+  : expired(false), handler(new Handler_Function(function)) {
+  in_critical_section = true;
+  pending_position = new_watchdog_event(units, handler, &expired);
+  in_critical_section = false;
+}
+
+PWL::Watchdog::~Watchdog() {
+  if (!expired) {
+    in_critical_section = true;
+    remove_watchdog_event(pending_position);
+    in_critical_section = false;
+  }
+  //  delete handler;
+}
+
+void
+PWL::Watchdog::initialize() {
+  signal_once.it_interval.tv_sec = 0;
+  signal_once.it_interval.tv_usec = 0;
+
+  sigset_t mask;
+  sigemptyset(&mask);
+
+  struct sigaction s;
+  s.sa_handler = handle_timeout;
+  s.sa_mask = mask;
+  s.sa_flags = 0;  // Was SA_ONESHOT: why?
+
+  int r = sigaction(SIGPROF, &s, 0);
+  if (r) {
+    cerr << "sigaction failed: " << r << endl;
+    exit(1);
+  }
+}
+
+PWL::Watchdog::Time PWL::Watchdog::reschedule_time(1);
---
+++ Watchdog/Watchdog.hh
@@ -0,0 +1,233 @@
+/* Watchdog and associated classes' declaration and inline functions.
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Watchdog Library (PWL).
+
+The PWL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PWL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Watchdog Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef _Watchdog_defs_hh
+#define _Watchdog_defs_hh 1
+
+namespace Parma_Watchdog_Library {
+  class Watchdog;
+  class Timeout;
+}
+
+#include <list>
+#include <cassert>
+#include <sys/time.h>
+
+//! A base class for timeout exceptions.
+class Parma_Watchdog_Library::Timeout {
+public:
+  virtual int priority() const = 0;
+};
+
+//! A watchdog timer.
+class Parma_Watchdog_Library::Watchdog {
+private:
+  // Positive times only here!
+  class Time {
+  public:
+    int seconds;
+    int microseconds;
+
+    Time()
+      : seconds(0), microseconds(0) {
+    }
+
+    explicit Time(int units)
+      : seconds(units / 100), microseconds((units * 10000) % 1000000) {
+      assert(units >= 0);
+    }
+
+    void reset() {
+      seconds = microseconds = 0;
+    }
+
+    Time& operator +=(const Time& y) {
+      int secs = seconds + y.seconds;
+      int usecs = microseconds + y.microseconds;
+      if (usecs >= 1000000) {
+	++secs;
+	usecs %= 1000000;
+      }
+      seconds = secs;
+      microseconds = usecs;
+      return *this;
+    }
+
+    Time& operator -=(const Time& y) {
+      int secs = seconds - y.seconds;
+      int usecs = microseconds - y.microseconds;
+      if (usecs < 0) {
+	--secs;
+	usecs += 1000000;
+      }
+      if (secs < 0)
+	secs = usecs = 0;
+      seconds = secs;
+      microseconds = usecs;
+      return *this;
+    }
+
+    friend Time operator -(const Time& x, const Time& y) {
+      Time z(x);
+      z -= y;
+      return z;
+    }
+
+    friend bool operator ==(const Time& x, const Time& y) {
+      return x.seconds == y.seconds && x.microseconds == y.microseconds;
+    }
+
+    friend bool operator !=(const Time& x, const Time& y) {
+      return !(x == y);
+    }
+
+    friend bool operator <(const Time& x, const Time& y) {
+      return x.seconds < y.seconds
+	|| (x.seconds == y.seconds && x.microseconds < y.microseconds);
+    }
+
+    friend bool operator <=(const Time& x, const Time& y) {
+      return x < y || x == y;
+    }
+
+    friend bool operator >(const Time& x, const Time& y) {
+      return y < x;
+    }
+
+    friend bool operator >=(const Time& x, const Time& y) {
+      return y <= x;
+    }
+  };
+
+  // Different kinds of handler for the watchdog events.
+  class Handler {
+  public:
+    virtual void act() const = 0;
+  };
+
+  class Handler_Flag : virtual public Handler {
+  private:
+    Timeout& exception;
+    volatile Timeout** flag;
+  public:
+    Handler_Flag(Timeout& e,
+		 volatile Timeout** f)
+      : exception(e), flag(f)
+    { }
+    void act() const {
+      *flag = &exception;
+    }
+  };
+
+  class Handler_Function : virtual public Handler {
+  private:
+    void (*function)();
+  public:
+    Handler_Function(void (*f)())
+      : function(f)
+    { }
+    void act() const {
+      (*function)();
+    }
+  };
+
+  // The pending watchdog events.
+  class Pending_Element {
+  public:
+    Time deadline;
+    const Handler* handler;
+    bool* p_expired_flag;
+    Pending_Element(const Time& d, const Handler* h, bool* p)
+      : deadline(d), handler(h), p_expired_flag(p)
+    { }
+  };
+
+  typedef std::list<Pending_Element> Pending;
+
+  bool expired;
+  const Handler* handler;
+  Pending::iterator pending_position;
+
+public:
+  static void initialize();
+
+  Watchdog(int units,
+	   Timeout& exception,
+	   volatile Timeout** flag);
+  Watchdog(int units,
+	   void (*function)());
+  ~Watchdog();
+
+private:
+  // Just to prevent their use.
+  Watchdog(const Watchdog&);
+  Watchdog& operator = (const Watchdog&);
+
+  // Pass this to getitimer.
+  static itimerval current_timer_status;
+  // Get the timer value.
+  static void get_timer(Time& time);
+  // Pass this to setitimer.
+  static itimerval signal_once;
+  // Last time value we set the timer to.
+  static Time last_time_requested;
+  // Set the timer value.
+  static void set_timer(const Time& time);
+  // Stops the timer.
+  static void stop_timer();
+  // Quick reschedule to avoid race conditions.
+  static void reschedule();
+  // Used by the above.
+  static Time reschedule_time;
+  // Records the time elapsed since last fresh start.
+  static Time time_so_far;
+  // The ordered queue of pending watchdog events.
+  static Pending pending;
+  // The actual signal handler.
+  static void handle_timeout(int);
+  // Inserts a new watchdog even at the right place in pending.
+  static Pending::iterator insert_pending(const Time& deadline,
+					  const Handler* handler,
+					  bool* p_expired);
+  // Handle the addition of a new watchdog event.
+  static Pending::iterator new_watchdog_event(int units,
+					      const Handler* handler,
+					      bool* p_expired);
+  // Handle the removal of a watchdog event.
+  void remove_watchdog_event(Pending::iterator position);
+  // Whether the alarm clock is running.
+  static volatile bool alarm_clock_running;
+  // Whether we are changing data which is also changed by the signal handler.
+  static volatile bool in_critical_section;
+};
+
+namespace Parma_Watchdog_Library {
+
+inline void
+Watchdog::reschedule() {
+  set_timer(reschedule_time);
+}
+
+} // namespace Parma_Watchdog_Library
+
+#endif
