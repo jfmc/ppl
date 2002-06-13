4d8c9893b3a6050cdb1785760dd1b91ffcc370c4
Roberto Bagnara <bagnara@cs.unipr.it>
2002-06-13 11:17:03 +0000

    Start completing the BoundingBox class.

---
+++ src/BoundingBox.cc
@@ -0,0 +1,52 @@
+/* BoundingBox class implementation (non-inline functions).
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "BoundingBox.hh"
+#include <iostream>
+
+std::ostream&
+operator<<(std::ostream& s, const PPL::BoundingBox& bbox) {
+  if (bbox.is_empty()) {
+    s << "empty";
+    return s;
+  }
+  unsigned int dimension = bbox.space_dimension();
+  for (unsigned int k = 0; k < dimension; ++k) {
+    bool closed = false;
+    PPL::Integer n;
+    PPL::Integer d;
+    bbox.get_lower_bound(k, closed, n, d);
+    s << (closed ? "[" : "(")
+      << n;
+    if (d != 1)
+      s << "/" << d;
+    s << ", ";
+    bbox.get_upper_bound(k, closed, n, d);
+    s << n;
+    if (d != 1)
+      s << "/" << d;
+    s << (closed ? "]" : ")")
+      << std::endl;
+  }
+  return s;
+}
--- src/BoundingBox.hh
+++ src/BoundingBox.hh
@@ -1,4 +1,4 @@
-/* The BoundingBox class.
+/* BoundingBox class declaration.
    Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
