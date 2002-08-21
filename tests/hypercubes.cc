18fa4c42c7bb6b078dd28661786fd90aff26d6b3
Enea Zaffanella <zaffanella@cs.unipr.it>
2002-08-21 23:10:40 +0000

    Some more tests.

--- tests/hypercubes.cc
+++ tests/hypercubes.cc
@@ -32,45 +32,170 @@ using namespace Parma_Polyhedra_Library;
 
 #define NOISY 0
 
-int
-main() {
-  // set_handlers();
+const Integer lb = 0;
+const Integer ub = 5;
 
-  const size_t maxdim = 15;
-  const Integer half_side = 5;
+void gen_closed_hypercubes() {
+  const size_t mindim = 6;
+  const size_t maxdim = 11;
 
-  for (size_t dim = 1; dim <= maxdim; dim++) {
+  cout << "=========================================" << endl
+       << "Computing generators of CLOSED hypercubes" << endl
+       << endl;
 
-    start_clock();
-
+  for (size_t dim = mindim; dim <= maxdim; dim++) {
     ConSys cs;
-    // Inserting lower bounds.
-    LinExpression tmpl = 0*Variable(dim-1);
-    tmpl += half_side;
-    for (size_t axis = 0; axis < dim; axis++) {
-      LinExpression lb = tmpl;
-      lb -= Variable(axis);
-      cs.insert(lb >= 0);
-    }
-    // Inserting upper bounds.
     for (size_t axis = dim; axis-- > 0; ) {
-      LinExpression ub = tmpl;
-      ub += Variable(axis);
-      cs.insert(ub >= 0);
+      cs.insert(Variable(axis) >= lb);
+      cs.insert(Variable(axis) <= ub);
     }
     NNC_Polyhedron ph(cs);
+    cout << dim << "-dimensional hypercube: ";
+    start_clock();
     ph.generators();
+    print_clock(cout);
+    cout << " secs." << endl;
+#if NOISY
+    print_constraints(ph, "--- ph ---");
+    print_generators(ph, "--- ph ---");
+#endif
+  }
+  cout << "=========================================" << endl
+       << endl;
+}
 
+
+void gen_open_hypercubes() {
+  const size_t mindim = 6;
+  const size_t maxdim = 11;
+
+  cout << "=========================================" << endl
+       << "Computing generators of OPEN hypercubes" << endl
+       << endl;
+
+  for (size_t dim = mindim; dim <= maxdim; dim++) {
+    ConSys cs;
+    for (size_t axis = dim; axis-- > 0; ) {
+      cs.insert(Variable(axis) > lb);
+      cs.insert(Variable(axis) < ub);
+    }
+    NNC_Polyhedron ph(cs);
+
+    cout << dim << "-dimensional hypercube: ";
+    start_clock();
+    ph.generators();
+    print_clock(cout);
+    cout << " secs." << endl;
 #if NOISY
     print_generators(ph, "--- ph ---");
 #endif
-
-    cout << "Hypercube of dimension "
-	 << dim
-	 << " generated after ";
-    print_clock(cout);
-    cout << " secs." << endl;
   }
+  cout << "=========================================" << endl
+       << endl;
+}
 
+
+void remove_vertices(size_t &to_be_removed,
+		     LinExpression expr,
+		     size_t axis,
+		     ConSys &cs) {
+  // Return if we have already removed all vertices.
+  if (to_be_removed == 0)
+    return;
+
+  if (axis == 0) {
+    // Adding first a constraint where variable with index `axis'
+    // has coefficient 1.
+    expr += Variable(axis);
+    expr -= ub;
+    cs.insert(expr < 0);
+    --to_be_removed;
+    if (to_be_removed == 0)
+      return;
+    // Restoring previous value of `expr'.
+    expr -= Variable(axis);
+    expr += ub;
+    // Then add a constraint where variable with index `axis'
+    // has coefficient -1.
+    expr -= Variable(axis);
+    cs.insert(expr < 0);
+    --to_be_removed;
+  }
+  else {
+    // axis > 0.
+    // First recursive call with variable with index `axis'
+    // having coefficient 1.
+    expr += Variable(axis);
+    expr -= ub;
+    remove_vertices(to_be_removed, expr, axis-1, cs);
+    if (to_be_removed == 0)
+      return;
+    // Restoring previous value of `expr'.
+    expr -= Variable(axis);
+    expr += ub;
+    // Second recursive call with variable with index `axis'
+    // having coefficient -1.
+    expr -= Variable(axis);
+    remove_vertices(to_be_removed, expr, axis-1, cs);
+  }
+}
+
+
+void gen_nnc_hypercubes() {
+  const size_t mindim = 6;
+  const size_t maxdim = 8;
+
+  cout << "=========================================" << endl
+       << "Computing generators of NNC hypercubes" << endl
+       << "missing some of the vertices" << endl
+       << endl;
+
+  for (size_t dim = mindim; dim <= maxdim; dim++) {
+    // First build the constraint system defining
+    // the closed hypercube.
+    ConSys cs;
+    for (size_t axis = dim; axis-- > 0; ) {
+      cs.insert(Variable(axis) >= lb);
+      cs.insert(Variable(axis) <= ub);
+    }
+
+    // Number of vertices in the closed hypercube.
+    size_t num_vertices = 1;
+    for (size_t axis = dim; axis-- > 0; )
+      num_vertices *= 2;
+
+    for (size_t tenths = 1; tenths <= 10; ++tenths) {
+      size_t to_be_removed = (num_vertices * tenths) / 10;
+
+      cout << dim << "-dimensional hypercube with ";
+      cout << (num_vertices - to_be_removed) << " vertices: ";
+
+      // Now add strict inequality constraints
+      // each one removing one vertex.
+      ConSys cs_copy = cs;
+      remove_vertices(to_be_removed, LinExpression(), dim-1, cs_copy);
+      NNC_Polyhedron ph(cs_copy);
+
+      start_clock();
+      ph.generators();
+      print_clock(cout);
+      cout << " secs." << endl;
+
+#if NOISY
+      ph.minimized_generators();
+      print_generators(ph, "--- ph ---");
+#endif
+    }
+  }
+  cout << "=========================================" << endl
+       << endl;
+}
+
+
+int
+main() {
+  gen_closed_hypercubes();
+  gen_open_hypercubes();
+  gen_nnc_hypercubes();
   return 0;
 }
