1d73bd9d5049860e45dff3dc23e94f41d05c7f19
Elisa Ricci <ericci@no.email>
2002-10-03 13:01:13 +0000

    Corrected the test: we want "return 1" if there is at least a test that exits with "return 1".

--- tests/generalizedaffineimage7.cc
+++ tests/generalizedaffineimage7.cc
@@ -216,9 +216,9 @@ main() {
   set_handlers();
 
   return test1()
-    && test2()
-    && test3()
-    && test4()
-    && test5()
-    && test6();
+    || test2()
+    || test3()
+    || test4()
+    || test5()
+    || test6();
 }
