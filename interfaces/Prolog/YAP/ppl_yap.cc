0276829d94c4aeb4d2761ead18d99fbad31e88ec
Roberto Bagnara <bagnara@cs.unipr.it>
2002-10-11 16:33:32 +0000

    Use the new C interface provided by YAP.

--- interfaces/Prolog/YAP/ppl_yap.cc
+++ interfaces/Prolog/YAP/ppl_yap.cc
@@ -24,20 +24,12 @@ site: http://www.cs.unipr.it/ppl/ . */
 #include <config.h>
 
 #include "Integer.defs.hh"
-
-// YAP 4.3.20 and 4.3.22 miss the `extern "C"' wrapper.
-
-extern "C" {
-
-#include <Yap/c_interface.h>
-
-} // extern "C"
-
+#include <Yap/YapInterface.h>
 #include <cassert>
 
-typedef Term Prolog_term_ref;
-typedef Atom Prolog_atom;
-typedef Bool Prolog_foreign_return_type;
+typedef YAP_Term Prolog_term_ref;
+typedef YAP_Atom Prolog_atom;
+typedef YAP_Bool Prolog_foreign_return_type;
 static const Prolog_foreign_return_type PROLOG_SUCCESS = TRUE;
 static const Prolog_foreign_return_type PROLOG_FAILURE = FALSE;
 
@@ -68,7 +60,7 @@ Prolog_put_term(Prolog_term_ref& t, Prolog_term_ref u) {
 */
 static inline bool
 Prolog_put_long(Prolog_term_ref& t, long i) {
-  t = MkIntTerm(i);
+  t = YAP_MkIntTerm(i);
   return true;
 }
 
@@ -79,7 +71,7 @@ Prolog_put_long(Prolog_term_ref& t, long i) {
 static inline bool
 Prolog_put_atom_chars(Prolog_term_ref& t, const char* s) {
   // FIXME: the following cast is really a bug in YAP.
-  t = MkAtomTerm(FullLookupAtom(const_cast<char*>(s)));
+  t = YAP_MkAtomTerm(YAP_FullLookupAtom(const_cast<char*>(s)));
   return true;
 }
 
@@ -88,7 +80,7 @@ Prolog_put_atom_chars(Prolog_term_ref& t, const char* s) {
 */
 static inline bool
 Prolog_put_atom(Prolog_term_ref& t, Prolog_atom a) {
-  t = MkAtomTerm(a);
+  t = YAP_MkAtomTerm(a);
   return true;
 }
 
@@ -97,7 +89,7 @@ Prolog_put_atom(Prolog_term_ref& t, Prolog_atom a) {
 */
 static inline bool
 Prolog_put_address(Prolog_term_ref& t, void* p) {
-  t = MkIntTerm(reinterpret_cast<long>(p));
+  t = YAP_MkIntTerm(reinterpret_cast<long>(p));
   return true;
 }
 
@@ -107,7 +99,7 @@ Prolog_put_address(Prolog_term_ref& t, void* p) {
 Prolog_atom
 Prolog_atom_from_string(const char* s) {
   // FIXME: the following cast is really a bug in YAP.
-  return FullLookupAtom(const_cast<char*>(s));
+  return YAP_FullLookupAtom(const_cast<char*>(s));
 }
 
 static Prolog_term_ref args[4];
@@ -120,7 +112,7 @@ static inline bool
 Prolog_construct_compound(Prolog_term_ref& t, Prolog_atom f,
 			  Prolog_term_ref a1) {
   args[0] = a1;
-  t = MkApplTerm(MkFunctor(f, 1), 1, args);
+  t = YAP_MkApplTerm(YAP_MkFunctor(f, 1), 1, args);
   return true;
 }
 
@@ -133,7 +125,7 @@ Prolog_construct_compound(Prolog_term_ref& t, Prolog_atom f,
 			  Prolog_term_ref a1, Prolog_term_ref a2) {
   args[0] = a1;
   args[1] = a2;
-  t = MkApplTerm(MkFunctor(f, 2), 2, args);
+  t = YAP_MkApplTerm(YAP_MkFunctor(f, 2), 2, args);
   return true;
 }
 
@@ -148,7 +140,7 @@ Prolog_construct_compound(Prolog_term_ref& t, Prolog_atom f,
   args[0] = a1;
   args[1] = a2;
   args[2] = a3;
-  t = MkApplTerm(MkFunctor(f, 3), 3, args);
+  t = YAP_MkApplTerm(YAP_MkFunctor(f, 3), 3, args);
   return true;
 }
 
@@ -164,7 +156,7 @@ Prolog_construct_compound(Prolog_term_ref& t, Prolog_atom f,
   args[1] = a2;
   args[2] = a3;
   args[3] = a4;
-  t = MkApplTerm(MkFunctor(f, 4), 4, args);
+  t = YAP_MkApplTerm(YAP_MkFunctor(f, 4), 4, args);
   return true;
 }
 
@@ -174,7 +166,7 @@ Prolog_construct_compound(Prolog_term_ref& t, Prolog_atom f,
 static inline bool
 Prolog_construct_cons(Prolog_term_ref& c,
 		      Prolog_term_ref h, Prolog_term_ref t) {
-  c = MkPairTerm(h, t);
+  c = YAP_MkPairTerm(h, t);
   return true;
 }
 
@@ -182,7 +174,7 @@ static Prolog_atom a_throw;
 
 static void
 ppl_Prolog_sysdep_init() {
-  a_throw = LookupAtom("throw");
+  a_throw = YAP_LookupAtom("throw");
 }
 
 static void
@@ -194,12 +186,7 @@ ppl_Prolog_sysdep_deinit() {
 */
 static inline void
 Prolog_raise_exception(Prolog_term_ref t) {
-#if 0
-  args[0] = t;
-  YapCallProlog(MkApplTerm(MkFunctor(a_throw, 1), 1, args));
-#else
-  YapThrow(t);
-#endif
+  YAP_Throw(t);
 }
 
 /*!
@@ -207,7 +194,7 @@ Prolog_raise_exception(Prolog_term_ref t) {
 */
 static inline bool
 Prolog_is_variable(Prolog_term_ref t) {
-  return IsVarTerm(t) != FALSE;
+  return YAP_IsVarTerm(t) != FALSE;
 }
 
 /*!
@@ -215,7 +202,7 @@ Prolog_is_variable(Prolog_term_ref t) {
 */
 static inline bool
 Prolog_is_atom(Prolog_term_ref t) {
-  return IsAtomTerm(t) != FALSE;
+  return YAP_IsAtomTerm(t) != FALSE;
 }
 
 /*!
@@ -223,7 +210,7 @@ Prolog_is_atom(Prolog_term_ref t) {
 */
 static inline bool
 Prolog_is_integer(Prolog_term_ref t) {
-  return IsIntTerm(t) != FALSE;
+  return YAP_IsIntTerm(t) != FALSE;
 }
 
 /*!
@@ -231,7 +218,7 @@ Prolog_is_integer(Prolog_term_ref t) {
 */
 static inline bool
 Prolog_is_address(Prolog_term_ref t) {
-  return IsIntTerm(t) != FALSE;
+  return YAP_IsIntTerm(t) != FALSE;
 }
 
 /*!
@@ -239,7 +226,7 @@ Prolog_is_address(Prolog_term_ref t) {
 */
 static inline bool
 Prolog_is_compound(Prolog_term_ref t) {
-  return IsApplTerm(t) != FALSE;
+  return YAP_IsApplTerm(t) != FALSE;
 }
 
 /*!
@@ -247,7 +234,7 @@ Prolog_is_compound(Prolog_term_ref t) {
 */
 static inline bool
 Prolog_is_cons(Prolog_term_ref t) {
-  return IsPairTerm(t) != FALSE;
+  return YAP_IsPairTerm(t) != FALSE;
 }
 
 /*!
@@ -259,7 +246,7 @@ Prolog_is_cons(Prolog_term_ref t) {
 static inline bool
 Prolog_get_long(Prolog_term_ref t, long& v) {
   assert(Prolog_is_integer(t));
-  v = IntOfTerm(t);
+  v = YAP_IntOfTerm(t);
   return true;
 }
 
@@ -271,7 +258,7 @@ Prolog_get_long(Prolog_term_ref t, long& v) {
 static inline bool
 Prolog_get_address(Prolog_term_ref t, void*& p) {
   assert(Prolog_is_address(t));
-  p = reinterpret_cast<void*>(IntOfTerm(t));
+  p = reinterpret_cast<void*>(YAP_IntOfTerm(t));
   return true;
 }
 
@@ -282,7 +269,7 @@ Prolog_get_address(Prolog_term_ref t, void*& p) {
 static inline bool
 Prolog_get_atom_name(Prolog_term_ref t, Prolog_atom& name) {
   assert(Prolog_is_atom(t));
-  name = AtomOfTerm(t);
+  name = YAP_AtomOfTerm(t);
   return true;
 }
 
@@ -295,9 +282,9 @@ static inline bool
 Prolog_get_compound_name_arity(Prolog_term_ref t,
 			       Prolog_atom& name, int& arity) {
   assert(Prolog_is_compound(t));
-  Functor f = FunctorOfTerm(t);
-  name = NameOfFunctor(f);
-  arity = ArityOfFunctor(f);
+  YAP_Functor f = YAP_FunctorOfTerm(t);
+  name = YAP_NameOfFunctor(f);
+  arity = YAP_ArityOfFunctor(f);
   return true;
 }
 
@@ -310,7 +297,7 @@ Prolog_get_compound_name_arity(Prolog_term_ref t,
 static inline bool
 Prolog_get_arg(int i, Prolog_term_ref t, Prolog_term_ref& a) {
   assert(Prolog_is_compound(t));
-  a = ArgOfTerm(i, t);
+  a = YAP_ArgOfTerm(i, t);
   return true;
 }
 
@@ -322,8 +309,8 @@ Prolog_get_arg(int i, Prolog_term_ref t, Prolog_term_ref& a) {
 static inline bool
 Prolog_get_cons(Prolog_term_ref c, Prolog_term_ref& h, Prolog_term_ref& t) {
   assert(Prolog_is_cons(c));
-  h = HeadOfTerm(c);
-  t = TailOfTerm(c);
+  h = YAP_HeadOfTerm(c);
+  t = YAP_TailOfTerm(c);
   return true;
 }
 
@@ -333,7 +320,7 @@ Prolog_get_cons(Prolog_term_ref c, Prolog_term_ref& h, Prolog_term_ref& t) {
 */
 static inline bool
 Prolog_unify(Prolog_term_ref t, Prolog_term_ref u) {
-  return unify(t, u) != 0;
+  return YAP_Unify(t, u) != 0;
 }
 
 static PPL::Integer
@@ -349,7 +336,7 @@ Integer_to_integer_term(const PPL::Integer& n) {
   // FIXME: does YAP support unlimited precision integer?
   if (!n.fits_slong_p())
     throw_unknown_interface_error("Integer_to_integer_term()");
-  return MkIntTerm(n.get_si());
+  return YAP_MkIntTerm(n.get_si());
 }
 
 #include "../ppl_prolog.icc"
@@ -363,34 +350,34 @@ yap_stub_##name() { \
 #define YAP_STUB_1(name) \
 extern "C" Prolog_foreign_return_type \
 yap_stub_##name() { \
-  Prolog_term_ref arg1 = ARG1; \
+  Prolog_term_ref arg1 = YAP_ARG1; \
   return name(arg1); \
 }
 
 #define YAP_STUB_2(name) \
 extern "C" Prolog_foreign_return_type \
 yap_stub_##name() { \
-  Prolog_term_ref arg1 = ARG1; \
-  Prolog_term_ref arg2 = ARG2; \
+  Prolog_term_ref arg1 = YAP_ARG1; \
+  Prolog_term_ref arg2 = YAP_ARG2; \
   return name(arg1, arg2); \
 }
 
 #define YAP_STUB_3(name) \
 extern "C" Prolog_foreign_return_type \
 yap_stub_##name() { \
-  Prolog_term_ref arg1 = ARG1; \
-  Prolog_term_ref arg2 = ARG2; \
-  Prolog_term_ref arg3 = ARG3; \
+  Prolog_term_ref arg1 = YAP_ARG1; \
+  Prolog_term_ref arg2 = YAP_ARG2; \
+  Prolog_term_ref arg3 = YAP_ARG3; \
   return name(arg1, arg2, arg3); \
 }
 
 #define YAP_STUB_4(name) \
 extern "C" Prolog_foreign_return_type \
 yap_stub_##name() { \
-  Prolog_term_ref arg1 = ARG1; \
-  Prolog_term_ref arg2 = ARG2; \
-  Prolog_term_ref arg3 = ARG3; \
-  Prolog_term_ref arg4 = ARG4; \
+  Prolog_term_ref arg1 = YAP_ARG1; \
+  Prolog_term_ref arg2 = YAP_ARG2; \
+  Prolog_term_ref arg3 = YAP_ARG3; \
+  Prolog_term_ref arg4 = YAP_ARG4; \
   return name(arg1, arg2, arg3, arg4); \
 }
 
@@ -445,7 +432,7 @@ YAP_STUB_2(ppl_Polyhedron_equals_Polyhedron)
 YAP_STUB_2(ppl_Polyhedron_get_bounding_box)
 
 #define YAP_USER_C_PREDICATE(name, arity) \
- UserCPredicate(#name, reinterpret_cast<int(*)()>(yap_stub_##name), arity)
+ YAP_UserCPredicate(#name, reinterpret_cast<int(*)()>(yap_stub_##name), arity)
 
 extern "C" void
 init() {
