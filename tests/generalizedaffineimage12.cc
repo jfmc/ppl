c3d6bef146baf6ff768c258745f038ae8f335166
Elisa Ricci <ericci@no.email>
2002-10-15 13:27:53 +0000

    Added new tests for some methods of the class Polyhedron.

---
+++ tests/generalizedaffineimage12.cc
@@ -0,0 +1,66 @@
+/* Test Polyhedron::generalized_affine_image() with a linear expression
+   as the left hand side: when the set of variables of left hand side
+   and the set of the variable of the right hand side are not disjoint
+   and the relation is PPL_LT.
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#ifndef NOISY
+#define NOISY 0
+#endif
+
+int
+main() {
+  set_handlers();
+
+  Variable A(0);
+  Variable B(1);
+  
+  NNC_Polyhedron ph(2);
+  ph.add_constraint(A >= 0);
+  ph.add_constraint(A <= 2);
+  ph.add_constraint(B >= 0);
+  ph.add_constraint(B <= 2);
+
+#if NOISY
+  print_constraints(ph, "*** ph ***");
+#endif
+
+  ph.generalized_affine_image(A + B, PPL_LT, 2*A - 3*B);
+
+  NNC_Polyhedron known_result(2);
+  known_result.add_constraint(A + B < 4);
+
+  int retval = (ph == known_result) ? 0 : 1;
+
+#if NOISY
+  print_constraints(ph, "*** After ph.generalized_affine_image"
+		    "(A + B, PPL_LT, 2*A - 3*B) ***");
+#endif
+
+  return retval;
+}
