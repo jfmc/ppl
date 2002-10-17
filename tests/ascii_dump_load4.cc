85eb9fefaac80382ad03d109dae079a0f2c6b3da
Elisa Ricci <ericci@no.email>
2002-10-17 13:39:56 +0000

    The ascii_dump_load4.cc test verifies some particular cases of ConSys::ascii_dump(s) and ConSys::ascii_load(s). The relations18.cc test verifies Polyhedron::relation_with(g), where "g" is a closure point and the system of constraints of the polyhedron contains equalities. The writepolyhedron3.cc test verifies operator<<(std::ostream&, const Polyhedron&) with an universe polyhedron.

---
+++ tests/ascii_dump_load4.cc
@@ -0,0 +1,141 @@
+/* Test Polyhedron::ascii_dump() e Polyhedron::ascii_load():
+   we test these functions in the case that the file does not contain
+   the right thing.
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include <string>
+#include <fstream>
+#include "ppl_test.hh"
+#include "files.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#ifndef NOISY
+#define NOISY 0
+#endif
+
+const char* my_file = "ascii_dump_load_4.dat";
+const char* my_temporary_file = "tmp_ascii_dump_load_4.dat";
+
+void
+test1() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph(2);
+  ph.add_constraint(A == 2);   
+  ph.add_constraint(B >= 0);   
+  
+  fstream f;
+  open(f, my_file, ios_base::out);
+  ph.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  C_Polyhedron ph2;
+  bool ok = ph2.ascii_load(f);
+  close(f);
+
+  if (!ok)
+    exit(1);
+}
+
+void
+test2() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph(2);
+  ph.add_constraint(A == 2);   
+  ph.add_constraint(B >= 0);   
+  
+  fstream f;
+  open(f, my_file, ios_base::out);
+  ph.ascii_dump(f);
+  close(f);
+  
+  open(f, my_file, ios_base::in | ios_base::out);
+  std::string str;
+  do
+    f >> str;
+  while(str != "=");
+  f << "A";
+  close(f);
+  
+  open(f, my_file, ios_base::in);
+  C_Polyhedron ph2;
+  bool ok = !ph2.ascii_load(f);
+  close(f);
+
+  if (!ok)
+    exit(1);
+}
+
+void
+test3() {
+  Variable A(0);
+  Variable B(1);
+
+  NNC_Polyhedron ph1(2);
+  ph1.add_constraint(A == 2);   
+  ph1.add_constraint(B >= 0);
+
+  ConSys cs = ph1.constraints();
+
+  C_Polyhedron ph(6);
+  ph.add_constraints(cs);
+  
+  fstream f;
+  open(f, my_file, ios_base::out);
+  ph.ascii_dump(f);
+  close(f);
+  
+  open(f, my_file, ios_base::in | ios_base::out);
+  std::string str;
+  do
+    f >> str;
+  while(str != "=");
+  f.seekp(-6, ios_base::cur);
+  f << "A";
+  close(f);
+  
+  open(f, my_file, ios_base::in);
+  C_Polyhedron ph2;
+  bool ok = !ph2.ascii_load(f);
+  close(f);
+
+  if (!ok)
+    exit(1);
+}
+
+int
+main() {
+  set_handlers();
+  
+  test1();
+  test2();
+  test3();
+
+  return 0;
+}
