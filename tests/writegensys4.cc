c1b23b2eb502009c801962069dbe29b564780788
Elisa Ricci <ericci@no.email>
2002-10-29 17:35:29 +0000

    Added a test to print a variable with id > 26.

---
+++ tests/writegensys4.cc
@@ -0,0 +1,53 @@
+/* Test operator<<(std::ostream& s, const Variable& v) with
+   Variable(27).
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include <string>
+#include <fstream>
+#include "ppl_test.hh"
+#include "files.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#ifndef NOISY
+#define NOISY 0
+#endif
+
+const char* my_file = "writegensys4.dat";
+
+int
+main() {
+  set_handlers();
+  
+  GenSys gs;
+  gs.insert(ray(Variable(27)));
+  gs.insert(point());
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  f << gs << endl;
+  close(f);
+  
+  return 0;
+}
