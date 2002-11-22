e03f3d5e3ddca8793f5efd10d7478312ea2315b0
Roberto Bagnara <bagnara@cs.unipr.it>
2002-11-22 08:34:58 +0000

    Include ehandlers.hh.

--- tests/ehandlers.cc
+++ tests/ehandlers.cc
@@ -21,6 +21,7 @@ USA.
 For the most up-to-date information see the Parma Polyhedra Library
 site: http://www.cs.unipr.it/ppl/ . */
 
+#include "ehandlers.hh"
 #include <exception>
 #include <iostream>
 #include <cstdlib>
