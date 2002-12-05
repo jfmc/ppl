dc0c575c7c51d98c6f0668be72026ef87fa2d8b9
Roberto Bagnara <bagnara@cs.unipr.it>
2002-12-05 07:46:19 +0000

    New test showing a bug triggered by an invocation of Polyhedron::minimized_constraints().

---
+++ tests/minconstraints4.cc
@@ -0,0 +1,46 @@
+/* Test Polyhedron::minimized_constraints().
+   Copyright (C) 2001, 2002 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#ifndef NOISY
+#define NOISY 0
+#endif
+
+int
+main() {
+  set_handlers();
+
+  Variable x2(0), x3(1), x5(2);
+  NNC_Polyhedron poly(3, Polyhedron::UNIVERSE);
+
+  poly.add_constraint(x5 > x2);
+  poly.add_constraint(x3 >= x2);
+  poly.add_constraint(x2 + 1 > x3);
+
+  poly.minimized_constraints();
+  return 0;
+}
