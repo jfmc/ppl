2701feaf0daef202b96f012247bda24afc748409
Enea Zaffanella <zaffanella@cs.unipr.it>
2003-04-05 07:37:06 +0000

    Modified class documentation to also consider the flags about pending constraints and generators. Moved Doxygen comments from friend declaration to the real function declaration. Avoid using preproc flag PPL_DOXYGEN_HYDE_ IMPLEMENTATION_DETAILS when useless, i.e., when we are inside a class whose documentation is already hidden.

--- src/Status.defs.hh
+++ src/Status.defs.hh
@@ -30,40 +30,67 @@ site: http://www.cs.unipr.it/ppl/ . */
 namespace Parma_Polyhedra_Library {
 
 // Put them in the namespace here to declare them friends later.
+#if PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
+//! Intersection: yields the assertions that are in \p x <EM>and</EM> \p y.
+#endif // PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
 Status operator&(const Status& x, const Status& y);
+#if PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
+//! Union: yields the assertions that are in \p x <EM>or</EM> \p y.
+#endif // PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
 Status operator|(const Status& x, const Status& y);
 
 } // namespace Parma_Polyhedra_Library
 
 
 #ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
+//! A conjunctive assertion about a polyhedron.
 /*!
-  This class implements conjunctions of assertions about a polyhedron.
   The assertions supported are:
   - <EM>zero-dim universe</EM>: the polyhedron is the zero-dimension
     vector space \f$\Rset^0 = \{\cdot\}\f$;
   - <EM>empty</EM>: the polyhedron is the empty set;
+  - <EM>constraints pending</EM>: the polyhedron is correctly
+    characterized by the attached system of constraints, which is
+    split in two non-empty subsets: the already processed constraints,
+    which are in minimal form, and the pending constraints, which
+    still have to be processed and may thus be inconsistent or
+    contain redundancies;
+  - <EM>generators pending</EM>: the polyhedron is correctly
+    characterized by the attached system of generators, which is
+    split in two non-empty subsets: the already processed generators,
+    which are in minimal form, and the pending generators, which still
+    have to be processed and may thus contain redundancies;
   - <EM>constraints up-to-date</EM>: the polyhedron is correctly
-    characterized by the attached system of constraints;
+    characterized by the attached system of constraints, modulo the
+    processing of pending generators;
   - <EM>generators up-to-date</EM>: the polyhedron is correctly
-    characterized by the attached system of generators;
-  - <EM>constraints minimized</EM>: the system of constraints attached
-    to the polyhedron is minimized;
-  - <EM>generators minimized</EM>: the system of generators attached
-    to the polyhedron is minimized.
+    characterized by the attached system of generators, modulo the
+    processing of pending constraints;
+  - <EM>constraints minimized</EM>: the non-pending part of the system
+    of constraints attached to the polyhedron is in minimal form;
+  - <EM>generators minimized</EM>: the non-pending part of the system
+    of generators attached to the polyhedron is in minimal form;
   - <EM>constraints' saturation matrix up-to-date</EM>: the attached
-    saturation matrix having rows indexed by generators and columns
-    indexed by constraints correctly expresses the saturation
-    relation between the attached constraints and generators;
+    saturation matrix having rows indexed by non-pending generators and
+    columns indexed by non-pending constraints correctly expresses
+    the saturation relation between the attached non-pending constraints
+    and generators;
   - <EM>generators' saturation matrix up-to-date</EM>: the attached
-    saturation matrix having rows indexed by constraints and
-    columns indexed by generators correctly expresses the saturation
-    relation between the attached constraints and generators;
+    saturation matrix having rows indexed by non-pending constraints and
+    columns indexed by non-pending generators correctly expresses
+    the saturation relation between the attached non-pending constraints
+    and generators;
 
   Not all the conjunctions of these elementary assertions constitute
   a legal Status.  In fact:
   - <EM>zero-dim universe</EM> excludes any other assertion;
   - <EM>empty</EM>: excludes any other assertion;
+  - <EM>constraints pending</EM> and <EM>generators pending</EM>
+    are mutually exclusive;
+  - <EM>constraints pending</EM> implies both <EM>constraints minimized</EM>
+    and <EM>generators minimized</EM>;
+  - <EM>generators pending</EM> implies both <EM>constraints minimized</EM>
+    and <EM>generators minimized</EM>;
   - <EM>constraints minimized</EM> implies <EM>constraints up-to-date</EM>;
   - <EM>generators minimized</EM> implies <EM>generators up-to-date</EM>;
   - <EM>constraints' saturation matrix up-to-date</EM> implies both
@@ -78,11 +105,9 @@ public:
   //! By default Status is the <EM>zero-dim universe</EM> assertion.
   Status();
 
-  //! Intersection: yields the assertions that are in \p x <EM>and</EM> \p y.
   friend Status
   Parma_Polyhedra_Library::operator&(const Status& x, const Status& y);
 
-  //! Union: yields the assertions that are in \p x <EM>or</EM> \p y.
   friend Status
   Parma_Polyhedra_Library::operator|(const Status& x, const Status& y);
 
@@ -132,19 +157,15 @@ public:
   //! Checks if all the invariants are satisfied.
   bool OK() const;
 
-#ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
   //! \brief
   //! Writes to \p s an ASCII representation of the internal
   //! representation of \p *this.
-#endif // PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
   void ascii_dump(std::ostream& s) const;
 
-#ifdef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
   //! \brief
   //! Loads from \p s an ASCII representation (as produced by \ref
   //! ascii_dump) and sets \p *this accordingly.  Returns <CODE>true</CODE>
   //! if successful, <CODE>false</CODE> otherwise.
-#endif // PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
   bool ascii_load(std::istream& s);
 
 private:
