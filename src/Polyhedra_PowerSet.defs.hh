7e1ce26fbc313d8ebabf29539f7bba4f064c371c
Roberto Bagnara <bagnara@cs.unipr.it>
2003-07-13 09:52:55 +0000

    The powerset construction instantiated on PPL polyhedra.

---
+++ src/Polyhedra_PowerSet.defs.hh
@@ -0,0 +1,175 @@
+/* Polyhedra_PowerSet class declaration.
+   Copyright (C) 2001-2003 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Polyhedra_PowerSet_defs_hh
+#define PPL_Polyhedra_PowerSet_defs_hh
+
+#include "Polyhedra_PowerSet.types.hh"
+#include "ConSys.types.hh"
+#include "Constraint.types.hh"
+#include "Polyhedron.defs.hh"
+#include "Variable.defs.hh"
+#include "Determinate.defs.hh"
+#include "PowerSet.defs.hh"
+#include "globals.hh"
+#include <iosfwd>
+#include <list>
+#include <set>
+
+//! The powerset construction instantiated on PPL polyhedra.
+template <typename PH>
+class Parma_Polyhedra_Library::Polyhedra_PowerSet
+  : public Parma_Polyhedra_Library::PowerSet
+<Parma_Polyhedra_Library::Determinate<PH> > {
+private:
+  typedef Determinate<PH> CS;
+  typedef PowerSet<CS> Base;
+
+public:
+  //! Builds a universe (top) or empty (bottom) Polyhedra_PowerSet.
+  /*!
+    \param num_dimensions   The number of dimensions of the vector
+                            space enclosing the powerset.
+    \param universe         If <CODE>true</CODE>, a universe Polyhedra_PowerSet
+                            is built;  an empty Polyhedra_PowerSet is built otherwise.
+  */
+  explicit
+  Polyhedra_PowerSet(dimension_type num_dimensions = 0,
+		     Polyhedron::Degenerate_Kind kind = Polyhedron::UNIVERSE);
+
+  //! Ordinary copy-constructor.
+  Polyhedra_PowerSet(const Polyhedra_PowerSet& y);
+
+  //! Creates a Polyhedra_PowerSet with the same information contents as \p cs.
+  Polyhedra_PowerSet(const ConSys& cs);
+
+  //! \brief
+  //! The assignment operator.
+  //! (\p *this and \p y can be dimension-incompatible.)
+  Polyhedra_PowerSet& operator=(const Polyhedra_PowerSet& y);
+  
+private:
+  //! The number of dimensions of the enclosing vector space.
+  dimension_type space_dim;
+
+public:
+  //! Returns the dimension of the vector space enclosing \p *this.
+  dimension_type space_dimension() const;
+
+  //! Intersects \p *this with (a copy of) constraint \p c.
+  /*!
+    \exception std::invalid_argument thrown if \p *this and constraint \p c
+                                     are topology-incompatible
+                                     or dimension-incompatible.
+  */
+  void add_constraint(const Constraint& c);
+
+  //! Intersects \p *this with the constraints in \p cs.
+  /*!
+    \param  cs             The constraints to intersect with.
+                           This parameter is not declared
+                           <CODE>const</CODE> because  it can be modified.
+    \exception std::invalid_argument thrown if \p *this and \p cs
+                                     are topology-incompatible
+                                     or dimension-incompatible.
+  */
+  void add_constraints(ConSys& cs);
+
+  //! Assigns to \p *this the concatenation of \p *this and \p y.
+  /*!
+    Seeing a powerset as a set of tuples, this method assigns to
+    \p *this all the tuples that can be obtained by concatenating,
+    in the order given, a tuple of \p *this with a tuple of \p y.
+  */
+  void concatenate_assign(const Polyhedra_PowerSet& y);
+
+  //! \brief
+  //! Adds \p m new dimensions and embeds the old polyhedron
+  //! into the new space.
+  void add_dimensions_and_embed(dimension_type m);
+
+  //! \brief
+  //! Adds \p m new dimensions to the polyhedron
+  //! and does not embed it in the new space.
+  void add_dimensions_and_project(dimension_type m);
+
+  //! Removes all the specified dimensions.
+  /*!
+    \param to_be_removed  The set of Variable objects corresponding
+                          to the dimensions to be removed.
+    \exception std::invalid_argument thrown if \p *this is
+                                     dimension-incompatible with one
+				     of the Variable objects contained
+				     in \p to_be_removed.
+  */
+  void remove_dimensions(const Variables_Set& to_be_removed);
+
+  //! \brief
+  //! Removes the higher dimensions so that the resulting space
+  //! will have dimension \p new_dimension.
+  /*!
+    \exception std::invalid_argument thrown if \p new_dimensions is greater
+                                     than the space dimension of \p *this.
+  */
+  void remove_higher_dimensions(dimension_type new_dimension);
+
+  template <typename PartialFunction>
+  void map_dimensions(const PartialFunction& pfunc);
+
+  void pairwise_reduce();
+
+private:
+  void extrapolation_assign(const Polyhedra_PowerSet& y,
+			    void (Polyhedron::*wm)
+			    (const Polyhedron&, unsigned*));
+  void widening_assign(const Polyhedra_PowerSet& y,
+		       void (Polyhedron::*wm)(const Polyhedron&, unsigned*),
+		       unsigned max_disjuncts);
+
+public:
+  void H79_extrapolation_assign(const Polyhedra_PowerSet& y);
+  void BHRZ03_extrapolation_assign(const Polyhedra_PowerSet& y);
+
+  void collapse(unsigned max_disjuncts);
+
+  void H79_widening_assign(const Polyhedra_PowerSet& y,
+			   unsigned max_disjuncts);
+  void BHRZ03_widening_assign(const Polyhedra_PowerSet& y,
+			      unsigned max_disjuncts);
+
+  //! Checks if all the invariants are satisfied.
+  bool OK() const;
+
+  typedef typename Sequence::size_type size_type;
+
+  typedef typename Base::Sequence Sequence;
+  typedef typename Base::iterator iterator;
+  typedef typename Base::const_iterator const_iterator;
+  typedef typename Base::reverse_iterator reverse_iterator;
+  typedef typename Base::const_reverse_iterator const_reverse_iterator;
+  typedef typename Base::value_type value_type;
+};
+
+#include "Polyhedra_PowerSet.inlines.hh"
+
+#endif // !defined(PPL_Polyhedra_PowerSet_defs_hh)
---
+++ src/Polyhedra_PowerSet.inlines.hh
@@ -0,0 +1,319 @@
+/* Polyhedra_PowerSet class implementation: inline functions.
+   Copyright (C) 2001-2003 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Polyhedra_PowerSet_inlines_hh
+#define PPL_Polyhedra_PowerSet_inlines_hh 1
+
+#include "ConSys.defs.hh"
+#include "ConSys.inlines.hh"
+#include <algorithm>
+#include <deque>
+
+namespace Parma_Polyhedra_Library {
+
+template <typename PH>
+Polyhedra_PowerSet<PH>::Polyhedra_PowerSet(dimension_type num_dimensions,
+					   Polyhedron::Degenerate_Kind kind)
+  : space_dim(num_dimensions) {
+  if (kind == Polyhedron::UNIVERSE)
+    push_back(Determinate<PH>(num_dimensions, true));
+}
+
+template <typename PH>
+Polyhedra_PowerSet<PH>::Polyhedra_PowerSet(const Polyhedra_PowerSet& y)
+  : Base(y), space_dim(y.space_dim) {
+}
+
+template <typename PH>
+Polyhedra_PowerSet<PH>::Polyhedra_PowerSet(const ConSys& cs)
+  : space_dim(cs.space_dimension()) {
+  push_back(Determinate<PH>(cs));
+}
+
+template <typename PH>
+Polyhedra_PowerSet<PH>&
+Polyhedra_PowerSet<PH>::operator=(const Polyhedra_PowerSet& y) {
+  Base::operator=(y);
+  space_dim = y.space_dim;
+  return *this;
+}
+
+template <typename PH>
+dimension_type
+Polyhedra_PowerSet<PH>::space_dimension() const {
+  return space_dim;
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::concatenate_assign(const Polyhedra_PowerSet& y) {
+  space_dim += y.space_dim;
+  Sequence new_sequence;
+  const Polyhedra_PowerSet<PH>& x = *this;
+  for (const_iterator xi = x.begin(), x_end = x.end(); xi != x_end; ++xi)
+    for (const_iterator yi = y.begin(), yend = y.end(); yi != yend; ++yi) {
+      CS zi = *xi;
+      zi.concatenate_assign(*yi);
+      assert(!zi.is_bottom());
+      new_sequence.push_back(zi);
+    }
+  std::swap(sequence, new_sequence);
+  omega_reduction();
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::add_constraint(const Constraint& c) {
+  for (iterator xi = begin(), xin = xi, x_end = end(); xi != x_end; xi = xin) {
+    ++xin;
+    CS& xv = *xi;
+    xv.add_constraint(c);
+    if (xv.is_bottom()) {
+      erase(xi);
+      x_end = end();
+    }
+  }
+  omega_reduction();
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::add_constraints(ConSys& cs) {
+  for (iterator xi = begin(), xin = xi, x_end = end(); xi != x_end; xi = xin) {
+    ++xin;
+    CS& xv = *xi;
+    xv.add_constraints(cs);
+    if (xv.is_bottom()) {
+      erase(xi);
+      x_end = end();
+    }
+  }
+  omega_reduction();
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::add_dimensions_and_embed(dimension_type m) {
+  space_dim += m;
+  for (iterator i = begin(), send = end(); i != send; ++i)
+    i->add_dimensions_and_embed(m);
+  omega_reduction();
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::add_dimensions_and_project(dimension_type m) {
+  space_dim += m;
+  for (iterator i = begin(), send = end(); i != send; ++i)
+    i->add_dimensions_and_project(m);
+  omega_reduction();
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::remove_dimensions(const Variables_Set& to_be_removed) {
+  // FIXME: set space_dim
+  for (iterator i = begin(), send = end(); i != send; ++i)
+    i->remove_dimensions(to_be_removed);
+  omega_reduction();
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::remove_higher_dimensions(dimension_type
+						 new_dimension) {
+  space_dim = new_dimension;
+  for (iterator i = begin(), send = end(); i != send; ++i)
+    i->remove_higher_dimensions(new_dimension);
+  omega_reduction();
+}
+
+template <typename PH>
+template <typename PartialFunction>
+void
+Polyhedra_PowerSet<PH>::map_dimensions(const PartialFunction& pfunc) {
+  if (is_bottom()) {
+    dimension_type n = 0;
+    for (dimension_type i = space_dim; i-- > 0; ) {
+      dimension_type new_i;
+      if (pfunc.maps(i, new_i))
+	++n;
+    }
+    space_dim = n;
+  }
+  else {
+    iterator sbegin = begin();
+    for (iterator i = sbegin, send = end(); i != send; ++i)
+      i->map_dimensions(pfunc);
+    space_dim = sbegin->space_dimension();
+    omega_reduction();
+  }
+  assert(OK());
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::pairwise_reduce() {
+  size_type n = size();
+  size_type deleted;
+  do {
+    Sequence new_sequence;
+    std::deque<bool> marked(n, false);
+    deleted = 0;
+    iterator sbegin = begin();
+    iterator send = end();
+    unsigned i_index = 0;
+    for (iterator i = sbegin; i != send; ++i, ++i_index) {
+      if (marked[i_index])
+	continue;
+      PH& pi = i->polyhedron();
+      int j_index = 0;
+      const_iterator j;
+      for (j = i, ++j; j != send; ++j, ++j_index) {
+	if (marked[j_index])
+	  continue;
+	const PH& pj = j->polyhedron();
+	if (poly_hull_assign_if_exact(pi, pj)) {
+	  marked[i_index] = marked[j_index] = true;
+	  new_sequence.push_back(pi);
+	  ++deleted;
+	  goto next;
+	}
+      }
+    next:
+      ;
+    }
+    i_index = 0;
+    for (const_iterator i = sbegin; i != send; ++i, ++i_index)
+      if (!marked[i_index])
+	new_sequence.push_back(*i);
+    std::swap(sequence, new_sequence);
+    n -= deleted;
+  } while (deleted > 0);
+  assert(OK());
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::extrapolation_assign(const Polyhedra_PowerSet& y,
+					     void (Polyhedron::*wm)
+					     (const Polyhedron&, unsigned*)) {
+  pairwise_reduce();
+  size_type n = size();
+  Sequence new_sequence;
+  std::deque<bool> marked(n, false);
+  iterator sbegin = begin();
+  iterator send = end();
+  unsigned i_index = 0;
+  for (iterator i = sbegin; i != send; ++i, ++i_index)
+    for (const_iterator j = y.begin(), y_end = y.end(); j != y_end; ++j) {
+      PH& pi = i->polyhedron();
+      const PH& pj = j->polyhedron();
+      if (pi.contains(pj)) {
+	(pi.*wm)(pj, 0);
+	new_sequence.push_back(pi);
+	marked[i_index] = true;
+      }
+    }
+  i_index = 0;
+  for (iterator i = sbegin; i != send; ++i, ++i_index)
+    if (!marked[i_index])
+      new_sequence.push_back(*i);
+  std::swap(sequence, new_sequence);
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::H79_extrapolation_assign(const Polyhedra_PowerSet& y) {
+  extrapolation_assign(y, &PH::H79_widening_assign);
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::BHRZ03_extrapolation_assign(const
+						    Polyhedra_PowerSet& y) {
+  extrapolation_assign(y, &PH::BHRZ03_widening_assign);
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::collapse(unsigned max_disjuncts) {
+  size_type n = size();
+  if (n > max_disjuncts) {
+    iterator i = begin();
+    unsigned k = 1;
+    // Move to the last polyhedron that will survive.
+    for ( ; k < max_disjuncts; ++k)
+      ++i;
+    // This polyhedron will be assigned the poly-hull of itself
+    // and of all the polyhedra that follow.
+    PH& ph = i->polyhedron();
+    ++i;
+    const_iterator j = i;
+    for (++k; k < n; ++k)
+      ph.poly_hull_assign(j->polyhedron());
+    // Erase the surplus polyhedra.
+    erase(i, end());
+    assert(size() == max_disjuncts);
+  }
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::widening_assign(const Polyhedra_PowerSet& y,
+					void (Polyhedron::*wm)
+					(const Polyhedron&, unsigned*),
+					unsigned max_disjuncts) {
+  collapse(max_disjuncts);
+  extrapolation_assign(y, wm);
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::H79_widening_assign(const Polyhedra_PowerSet& y,
+					    unsigned max_disjuncts) {
+  widening_assign(y, &PH::H79_widening_assign, max_disjuncts);
+}
+
+template <typename PH>
+void
+Polyhedra_PowerSet<PH>::BHRZ03_widening_assign(const Polyhedra_PowerSet& y,
+					       unsigned max_disjuncts) {
+  widening_assign(y, &PH::BHRZ03_widening_assign, max_disjuncts);
+}
+
+template <typename PH>
+bool
+Polyhedra_PowerSet<PH>::OK() const {
+  for (const_iterator i = begin(), send = end(); i != send; ++i) {
+    if (i->space_dimension() != space_dim)
+      return false;
+    if (!Base::OK())
+      return false;
+  }
+  return true;
+}
+
+} // namespace Parma_Polyhedra_Library
+
+#endif // !defined(PPL_Polyhedra_PowerSet_inlines_hh)
