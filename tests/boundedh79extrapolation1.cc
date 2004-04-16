ab423fbf694528537acee1279f362b9369b842d9
Roberto Bagnara <bagnara@cs.unipr.it>
2004-04-16 14:41:02 +0000

    Test Polyhedron::bounded_H79_extrapolation_assign().

---
+++ tests/boundedh79extrapolation1.cc
@@ -0,0 +1,73 @@
+/* Test Polyhedron::bounded_H79_extrapolation_assign().
+   Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#ifndef NOISY
+#define NOISY 0
+#endif
+
+int
+main() TRY {
+  Variable x(0);
+  Variable y(1);
+
+  C_Polyhedron ph1(2);
+  ph1.add_constraint(x >= 3);
+  ph1.add_constraint(x <= 7);
+
+
+#if NOISY
+  print_constraints(ph1, "*** ph1 ****");
+#endif
+
+  C_Polyhedron ph2(2);
+  ph2.add_constraint(x >= 3);
+  ph2.add_constraint(x <= 8);
+
+
+#if NOISY
+  print_constraints(ph2, "*** ph2 ****");
+#endif
+
+  ConSys cs;
+  cs.insert(x <= 100);
+
+#if NOISY
+  print_constraints(cs, "*** cs ****");
+#endif
+
+  C_Polyhedron computed_result = ph2;
+  computed_result.bounded_H79_extrapolation_assign(ph1, cs);
+
+#if NOISY
+  print_constraints(computed_result,
+		    "*** After bounded_H79_extrapolation_assign ****");
+#endif
+
+  return (computed_result == ph2) ? 0 : 1;
+}
+CATCH
