a027b92ed6eacd73671b8d949cabc36ac05d142e
Roberto Bagnara <bagnara@cs.unipr.it>
2004-05-05 13:31:52 +0000

    Use also pl_check.pl to test the SICStus-Prolog interface.

---
+++ interfaces/Prolog/SICStus/sp_pl_check.pl
@@ -0,0 +1,39 @@
+/* Main program for the toy PPL/SICStus-Prolog CLP(Q) predicate checker.
+   Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+:- set_prolog_flag(language, iso).
+
+:- ensure_loaded('ppl_sicstus.pl').
+:- use_module(library(lists), [append/3, member/2]).
+
+main :-
+    ensure_loaded(pl_check),
+    set_prolog_flag(language, iso),	% FIXME: this is not ISO Prolog
+    nofileerrors,
+    (check_all ->
+	write('OK')
+    ;
+	write('FAILURE')
+    ),
+    nl,
+    halt.
