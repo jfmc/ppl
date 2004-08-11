74d452caacb97c3075fa2301bab30cb426f8e52e
Patricia M. Hill <hill@comp.leeds.ac.uk>
2004-08-11 19:44:20 +0000

    More tests added for testing expand_dimensions().

--- tests/expanddim1.cc
+++ tests/expanddim1.cc
@@ -27,12 +27,14 @@ using namespace std;
 using namespace Parma_Polyhedra_Library;
 
 #ifndef NOISY
-#define NOISY 1
+#define NOISY 0
 #endif
 
 Variable A(0);
 Variable B(1);
 Variable C(2);
+Variable D(3);
+Variable E(4);
 
 // Test with a universe polyhedron.
 static void
@@ -80,9 +82,31 @@ test2() {
     exit(1);
 }
 
-// To be written.
+// Test trivial expansion
 static void
 test3() {
+  C_Polyhedron ph1(2);
+  ph1.add_constraint(A >= 0);
+  ph1.add_constraint(A + B <= 2);
+
+#if NOISY
+  print_constraints(ph1, "*** ph1 ***");
+#endif
+
+  ph1.expand_dimension(A, 0);
+
+  C_Polyhedron known_result(2);
+  known_result.add_constraint(A >= 0);
+  known_result.add_constraint(A + B <= 2);
+
+  bool ok = (ph1 == known_result);
+
+#if NOISY
+  print_constraints(ph1, "*** After ph1.expand_dimension(A, 0) ***");
+#endif
+
+  if (!ok)
+    exit(1);
 }
 
 // Test with given generators.
@@ -91,6 +115,7 @@ test4() {
   C_Polyhedron ph1(2, C_Polyhedron::EMPTY);
   ph1.add_generator(point(A));
   ph1.add_generator(point(A + B));
+  ph1.add_generator(point(B));
 
 #if NOISY
   print_generators(ph1, "*** ph1 ***");
@@ -98,7 +123,12 @@ test4() {
 
   ph1.expand_dimension(A, 1);
 
-  C_Polyhedron known_result = ph1;
+  C_Polyhedron known_result(3, C_Polyhedron::EMPTY);
+  known_result.add_generator(point(A + C));
+  known_result.add_generator(point(A + B));
+  known_result.add_generator(point(A + B + C));
+  known_result.add_generator(point(B));
+  known_result.add_generator(point(B + C));
 
   bool ok = (ph1 == known_result);
 
@@ -122,37 +152,156 @@ test5() {
 
   ph1.expand_dimension(A, 1);
 
-  C_Polyhedron known_result = ph1;
+  C_Polyhedron known_result(3);
+  known_result.add_constraint(A >= 0);
+  known_result.add_constraint(A + B <= 2);
+  known_result.add_constraint(C >= 0);
+  known_result.add_constraint(C + B <= 2);
 
   bool ok = (ph1 == known_result);
 
 #if NOISY
   print_constraints(ph1, "*** After ph1.expand_dimension(A, 1) ***");
-  print_generators(ph1, "***  After ph1.expand_dimension(A, 1) ***");
 #endif
 
   if (!ok)
     exit(1);
 }
 
-// To be written.
+// Test using constraints expanding 2 dimensions
 static void
 test6() {
+  C_Polyhedron ph1(2);
+  ph1.add_constraint(A >= 0);
+  ph1.add_constraint(A + B <= 2);
+#if NOISY
+  print_constraints(ph1, "*** ph1 ***");
+#endif
+
+  ph1.expand_dimension(A, 2);
+
+  C_Polyhedron known_result(4);
+  known_result.add_constraint(A >= 0);
+  known_result.add_constraint(A + B <= 2);
+  known_result.add_constraint(C >= 0);
+  known_result.add_constraint(C + B <= 2);
+  known_result.add_constraint(D >= 0);
+  known_result.add_constraint(D + B <= 2);
+
+  bool ok = (ph1 == known_result);
+
+#if NOISY
+  print_constraints(ph1, "*** After ph1.expand_dimension(A, 2) ***");
+#endif
+
+  if (!ok)
+    exit(1);
 }
 
-// To be written.
+// Test using constraints for NNC polyhedron
 static void
 test7() {
+
+  NNC_Polyhedron ph1(2);
+  ph1.add_constraint(A - B > 2);
+  ph1.add_constraint(A + 2*B < 6);
+  ph1.add_constraint(B < 6);
+
+#if NOISY
+  print_constraints(ph1, "*** ph1 ***");
+#endif
+
+  ph1.expand_dimension(B, 2);
+
+  NNC_Polyhedron known_result(4);
+  known_result.add_constraint(A - B > 2);
+  known_result.add_constraint(A + 2*B < 6);
+  known_result.add_constraint(B < 6);
+  known_result.add_constraint(A - C > 2);
+  known_result.add_constraint(A + 2*C < 6);
+  known_result.add_constraint(C < 6);
+  known_result.add_constraint(A - D > 2);
+  known_result.add_constraint(A + 2*D < 6);
+  known_result.add_constraint(D < 6);
+
+  bool ok = (ph1 == known_result);
+
+#if NOISY
+  print_constraints(ph1, "*** After ph1.expand_dimension(B, 2) ***");
+#endif
+
+  if (!ok)
+    exit(1);
 }
 
-// To be written.
+// Test using constraints with equality constraint
 static void
 test8() {
+  C_Polyhedron ph1(3);
+  ph1.add_constraint(A <= 1);
+  ph1.add_constraint(C == 1);
+  ph1.add_constraint(A + B >= 1);
+  ph1.add_constraint(B <= 1);
+
+#if NOISY
+  print_constraints(ph1, "*** ph1 ***");
+#endif
+
+  ph1.expand_dimension(A, 1);
+  ph1.expand_dimension(C, 1);
+
+  C_Polyhedron known_result(5);
+  known_result.add_constraint(A <= 1);
+  known_result.add_constraint(A + B >= 1);
+  known_result.add_constraint(C == 1);
+  known_result.add_constraint(E == 1);
+  known_result.add_constraint(B <= 1);
+  known_result.add_constraint(D <= 1);
+  known_result.add_constraint(D + B >= 1);
+
+  bool ok = (ph1 == known_result);
+
+#if NOISY
+  print_constraints(ph1, "***  After ph1.expand_dimension(A, 1); ph1.expand_dimension(C, 1) ***");
+#endif
+
+  if (!ok)
+    exit(1);
 }
 
-// To be written.
+// Test using generators for NNC polyhedron
 static void
 test9() {
+  NNC_Polyhedron ph1(2, NNC_Polyhedron::EMPTY);
+  ph1.add_generator(point(A));
+  ph1.add_generator(closure_point(A + B));
+  ph1.add_generator(ray(A - B));
+
+#if NOISY
+  print_generators(ph1, "*** ph1 ***");
+#endif
+
+  ph1.expand_dimension(A, 2);
+
+  NNC_Polyhedron known_result(4, NNC_Polyhedron::EMPTY);
+  known_result.add_generator(point(A + C + D));
+  known_result.add_generator(ray(A -B + C + D));
+  known_result.add_generator(closure_point(A + C + 2*D));
+  known_result.add_generator(closure_point(A + 2*C + D));
+  known_result.add_generator(closure_point(A + 2*C + 2*D));
+  known_result.add_generator(closure_point(A + B + C + D));
+  known_result.add_generator(closure_point(2*A + C + D));
+  known_result.add_generator(closure_point(2*A + C + 2*D));
+  known_result.add_generator(closure_point(2*A + 2*C + D));
+
+  bool ok = (ph1 == known_result);
+
+#if NOISY
+  print_generators(ph1, "***  After ph1.expand_dimension(A, 2) ***");
+#endif
+
+  if (!ok)
+    exit(1);
 }
 
 int
