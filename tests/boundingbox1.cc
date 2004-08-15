c7b382f4f07ed02b06b5b8637e44004205354531
Roberto Bagnara <bagnara@cs.unipr.it>
2004-08-15 09:29:00 +0000

    Comments corrected/revised.

--- tests/boundingbox1.cc
+++ tests/boundingbox1.cc
@@ -1,4 +1,4 @@
-/* Test C_Polyhedron::shrink_bounding_box().
+/* Test Polyhedron::shrink_bounding_box().
    Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
--- tests/boundingbox4.cc
+++ tests/boundingbox4.cc
@@ -1,4 +1,4 @@
-/* Test building an NNC_Polyhedron from a closed interval-based bounding box.
+/* Test building an NNC_Polyhedron from non-closed bounding boxes.
    Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
