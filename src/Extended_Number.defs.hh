506253a219e43ec6ac9a383a50eca4dacd703a03
Abramo Bagnara <abramo.bagnara@gmail.com>
2004-12-28 18:39:24 +0000

    Added preliminary Extended_Number implementation.

---
+++ src/Extended_Number.defs.hh
@@ -0,0 +1,295 @@
+/* Extended_Number class declaration.
+   Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Extended_Number_defs_hh
+#define PPL_Extended_Number_defs_hh 1
+
+#include "Extended_Number.types.hh"
+#include "checked_ext.defs.hh"
+#include <gmpxx.h>
+#include <iostream>
+
+namespace Parma_Polyhedra_Library {
+
+struct Minus_Infinity {
+};
+
+struct Plus_Infinity {
+};
+
+struct Not_A_Number {
+};
+
+struct Extended_Policy {
+  static const int check_overflow = 1;
+  static const int check_divbyzero = 1;
+  static const int check_sqrt_neg = 1;
+  static const int round_inexact = 1;
+  static const int store_nan = 1;
+  static const int store_infinity = 1;
+  static const int fpu_classify = 0;
+  static const int fpu_check_inexact = 0;
+};
+
+template <typename T, typename Policy>
+class Extended_Number {
+public:
+  //! Default constructor.
+  Extended_Number();
+
+  T& raw_value();
+  const T& raw_value() const;
+
+  Result classify(bool nan = true, bool inf = true, bool sign = true);
+
+  Result assign(const Minus_Infinity&, const Rounding& mode = Rounding::IGNORE);
+  Result assign(const Plus_Infinity&, const Rounding& mode = Rounding::IGNORE);
+  Result assign(const Not_A_Number&, const Rounding& mode = Rounding::IGNORE);
+
+#define FUNC1(name) \
+  template <typename From> \
+  Result name(const From& x, const Rounding& mode = Rounding::IGNORE); \
+  template <typename From, typename From_Policy> \
+  Result name(const Extended_Number<From, From_Policy>& x, const Rounding& mode = Rounding::IGNORE);
+
+  FUNC1(assign)
+  FUNC1(assign_neg)
+  FUNC1(assign_abs)
+  FUNC1(assign_sqrt)
+
+#undef FUNC1
+
+#define FUNC2(name) \
+  template <typename From1, typename From2> \
+  Result name(const From1& x, const From2& y, const Rounding& mode = Rounding::IGNORE); \
+  template <typename From1, \
+	    typename From2, typename From2_Policy> \
+  Result name(const From1& x, const Extended_Number<From2, From2_Policy>& y, const Rounding& mode = Rounding::IGNORE); \
+  template <typename From1, typename From1_Policy, \
+	    typename From2> \
+  Result name(const Extended_Number<From1, From1_Policy>& x, const From2& y, const Rounding& mode = Rounding::IGNORE); \
+  template <typename From1, typename From1_Policy, \
+	    typename From2, typename From2_Policy> \
+  Result name(const Extended_Number<From1, From1_Policy>& x, const Extended_Number<From2, From2_Policy>& y, const Rounding& mode = Rounding::IGNORE);
+
+  FUNC2(assign_add)
+  FUNC2(assign_sub)
+  FUNC2(assign_mul)
+  FUNC2(assign_div)
+  FUNC2(assign_mod)
+
+#undef FUNC2
+
+  template <typename From>
+  Extended_Number(const From& y);
+
+  template <typename From>
+  Extended_Number& operator=(const From& y);
+  template <typename From>
+  Extended_Number& operator+=(const From& y);
+  template <typename From>
+  Extended_Number& operator-=(const From& y);
+  template <typename From>
+  Extended_Number& operator*=(const From& y);
+  template <typename From>
+  Extended_Number& operator/=(const From& y);
+  template <typename From>
+  Extended_Number& operator%=(const From& y);
+  Extended_Number& operator++();
+  Extended_Number  operator++(int);
+  Extended_Number& operator--();
+  Extended_Number  operator--(int);
+
+  //! Swaps \p *this with \p y.
+  void swap(Extended_Number& y);
+
+  static void bad_result(Result r);
+  static void check_result(Result r);
+
+private:
+  T v;
+};
+
+extern Minus_Infinity MINUS_INFINITY;
+extern Plus_Infinity PLUS_INFINITY;
+extern Not_A_Number NOT_A_NUMBER;
+
+template <typename T, typename Policy>
+const T&
+raw_value(const Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+T&
+raw_value(Extended_Number<T, Policy>& x);
+
+#if 0
+//! Returns the total size in bytes of the memory occupied by \p x.
+template <typename T, typename Policy>
+size_t
+total_memory_in_bytes(const Extended_Number<T, Policy>& x);
+
+//! Returns the size in bytes of the memory managed by \p x.
+template <typename T, typename Policy>
+size_t
+external_memory_in_bytes(const Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator+(const Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator-(const Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator+(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator-(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator*(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator/(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+Extended_Number<T, Policy>
+operator%(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+bool
+operator==(const Extended_Number<T, Policy>& x,
+	   const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+bool
+operator!=(const Extended_Number<T, Policy>& x,
+	   const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+bool
+operator>=(const Extended_Number<T, Policy>& x,
+	   const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+bool
+operator>(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+bool
+operator<=(const Extended_Number<T, Policy>& x,
+	   const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+bool
+operator<(const Extended_Number<T, Policy>& x,
+	  const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+int
+sgn(const Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+int
+cmp(const Extended_Number<T, Policy>& x, const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+void
+negate(Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+void
+add_mul_assign(Extended_Number<T, Policy>& x,
+	       const Extended_Number<T, Policy>& y,
+	       const Extended_Number<T, Policy>& z);
+
+template <typename T, typename Policy>
+void
+sub_mul_assign(Extended_Number<T, Policy>& x,
+	       const Extended_Number<T, Policy>& y,
+	       const Extended_Number<T, Policy>& z);
+
+template <typename T, typename Policy>
+void
+gcd_assign(Extended_Number<T, Policy>& x, const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+void
+gcd_assign(Extended_Number<T, Policy>& x,
+	   const Extended_Number<T, Policy>& y,
+	   const Extended_Number<T, Policy>& z);
+
+template <typename T, typename Policy>
+void
+lcm_assign(Extended_Number<T, Policy>& x, const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+void
+lcm_assign(Extended_Number<T, Policy>& x,
+	   const Extended_Number<T, Policy>& y,
+	   const Extended_Number<T, Policy>& z);
+
+template <typename T, typename Policy>
+void
+exact_div_assign(Extended_Number<T, Policy>& x,
+		 const Extended_Number<T, Policy>& y);
+
+template <typename T, typename Policy>
+void
+exact_div_assign(Extended_Number<T, Policy>& x,
+		 const Extended_Number<T, Policy>& y,
+		 const Extended_Number<T, Policy>& z);
+
+template <typename T, typename Policy>
+void sqrt_assign(Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+void sqrt_assign(Extended_Number<T, Policy>& x,
+		 const Extended_Number<T, Policy>& y);
+
+#endif
+
+template <typename T, typename Policy>
+std::ostream&
+operator<<(std::ostream& os, const Extended_Number<T, Policy>& x);
+
+template <typename T, typename Policy>
+std::istream&
+operator>>(std::istream& is, Extended_Number<T, Policy>& x);
+
+} // namespace Parma_Polyhedra_Library
+
+#include "Extended_Number.inlines.hh"
+
+#endif // !defined(PPL_Extended_Number_defs_hh)
---
+++ src/Extended_Number.inlines.hh
@@ -0,0 +1,304 @@
+/* Extended_Number class implementation: inline functions.
+   Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Extended_Number_inlines_hh
+#define PPL_Extended_Number_inlines_hh 1
+
+#include <stdexcept>
+
+namespace Parma_Polyhedra_Library {
+
+template <typename T, typename Policy>
+inline void
+Extended_Number<T, Policy>::check_result(Result r) {
+// Reserved for future implementation according to the policy.
+}
+
+template <typename T, typename Policy>
+inline T&
+Extended_Number<T, Policy>::raw_value() {
+  return v;
+}
+
+template <typename T, typename Policy>
+inline const T&
+Extended_Number<T, Policy>::raw_value() const {
+  return v;
+}
+
+template <typename T, typename Policy>
+inline Result
+Extended_Number<T, Policy>::classify(bool nan, bool inf, bool sign) {
+  return Checked::classify<Policy>(v, nan, inf, sign);
+}
+
+template <typename T, typename Policy>
+inline
+Extended_Number<T, Policy>::Extended_Number() {
+}
+
+template <typename T, typename Policy>
+inline Result
+Extended_Number<T, Policy>::assign(const Minus_Infinity&, const Rounding&) {
+  return Checked::set_special<Policy>(v, V_MINUS_INFINITY);
+}
+
+template <typename T, typename Policy>
+inline Result
+Extended_Number<T, Policy>::assign(const Plus_Infinity&, const Rounding&) {
+  return Checked::set_special<Policy>(v, V_PLUS_INFINITY);
+}
+
+template <typename T, typename Policy>
+inline Result
+Extended_Number<T, Policy>::assign(const Not_A_Number&, const Rounding&) {
+  return Checked::set_special<Policy>(v, V_UNKNOWN);
+}
+
+#define FUNC1(name, func) \
+template <typename T, typename Policy> \
+template <typename From> \
+inline Result \
+Extended_Number<T, Policy>::name(const From& x, const Rounding& mode) { \
+  return Checked::func<Policy, Checked::Transparent_Policy>(v, x, mode); \
+} \
+template <typename T, typename Policy> \
+template <typename From, typename From_Policy> \
+inline Result \
+Extended_Number<T, Policy>::name(const Extended_Number<From, From_Policy>& x, const Rounding& mode) { \
+  return Checked::func<Policy, From_Policy>(v, x.raw_value(), mode); \
+}
+
+FUNC1(assign, assign_ext)
+FUNC1(assign_neg, neg_ext)
+FUNC1(assign_abs, abs_ext)
+FUNC1(assign_sqrt, sqrt_ext)
+
+#undef FUNC1
+
+#define FUNC2(name, func) \
+template <typename T, typename Policy> \
+template <typename From1, \
+	  typename From2> \
+inline Result \
+Extended_Number<T, Policy>::name(const From1& x, const From2& y, const Rounding& mode) { \
+  return Checked::func<Policy, Checked::Transparent_Policy, Checked::Transparent_Policy>(v, x, y, mode); \
+} \
+template <typename T, typename Policy> \
+template <typename From1, \
+	  typename From2, typename From2_Policy> \
+inline Result \
+Extended_Number<T, Policy>::name(const From1& x, const Extended_Number<From2, From2_Policy>& y, const Rounding& mode) { \
+  return Checked::func<Policy, Checked::Transparent_Policy, From2_Policy>(v, x, y.raw_value(), mode); \
+} \
+template <typename T, typename Policy> \
+template <typename From1, typename From1_Policy, \
+	  typename From2> \
+inline Result \
+Extended_Number<T, Policy>::name(const Extended_Number<From1, From1_Policy>& x, const From2& y, const Rounding& mode) { \
+  return Checked::func<Policy, From1_Policy, Checked::Transparent_Policy>(v, x.raw_value(), y, mode); \
+} \
+template <typename T, typename Policy> \
+template <typename From1, typename From1_Policy, \
+	  typename From2, typename From2_Policy> \
+inline Result \
+Extended_Number<T, Policy>::name(const Extended_Number<From1, From1_Policy>& x, const Extended_Number<From2, From2_Policy>& y, const Rounding& mode) { \
+  return Checked::func<Policy, From1_Policy, From2_Policy>(v, x.raw_value(), y.raw_value(), mode); \
+}
+
+  FUNC2(assign_add, add_ext)
+  FUNC2(assign_sub, sub_ext)
+  FUNC2(assign_mul, mul_ext)
+  FUNC2(assign_div, div_ext)
+  FUNC2(assign_mod, mod_ext)
+
+#undef FUNC2
+
+template <typename From1, typename From1_Policy,
+	  typename From2, typename From2_Policy>
+inline Result
+compare(const Extended_Number<From1, From1_Policy>& x, const Extended_Number<From2, From2_Policy>& y) { \
+  return Checked::cmp_ext<From1_Policy, From2_Policy>(x.raw_value(), y.raw_value()); \
+}
+
+template <typename T, typename Policy>
+inline const T&
+raw_value(const Extended_Number<T, Policy>& x) {
+  return x.raw_value();
+}
+
+template <typename T, typename Policy>
+inline T&
+raw_value(Extended_Number<T, Policy>& x) {
+  return x.raw_value();
+}
+
+template <typename T, typename Policy>
+template <typename From>
+Extended_Number<T, Policy>::Extended_Number(const From& y) {
+  check_result(assign(y, Rounding::CURRENT));
+}
+
+#define DEF_INCREMENT(f, fun) \
+template <typename T, typename Policy> \
+inline Extended_Number<T, Policy>& \
+Extended_Number<T, Policy>::f() { \
+  check_result(fun(*this, T(1), Rounding(Rounding::CURRENT))); \
+  return *this; \
+}\
+template <typename T, typename Policy> \
+inline Extended_Number<T, Policy> \
+Extended_Number<T, Policy>::f(int) {\
+  T r = v;\
+  check_result(fun(*this, T(1), Rounding(Rounding::CURRENT)));\
+  return r;\
+}
+
+DEF_INCREMENT(operator ++, assign_add)
+DEF_INCREMENT(operator --, sssign_add)
+
+template <typename T, typename Policy>
+inline void
+Extended_Number<T, Policy>::swap(Extended_Number<T, Policy>& y) {
+  std::swap(v, y.v);
+}
+
+#define DEF_BINARY_ASSIGN(f, fun) \
+template <typename T, typename Policy> \
+template <typename From> \
+inline Extended_Number<T, Policy>& \
+Extended_Number<T, Policy>::f(const From& y) { \
+  check_result(fun(*this, y, Rounding(Rounding::CURRENT))); \
+  return *this; \
+}
+
+DEF_BINARY_ASSIGN(operator =, assign)
+DEF_BINARY_ASSIGN(operator +=, assign_add)
+DEF_BINARY_ASSIGN(operator -=, assign_sub)
+DEF_BINARY_ASSIGN(operator *=, assign_mul)
+DEF_BINARY_ASSIGN(operator /=, assign_div)
+DEF_BINARY_ASSIGN(operator %=, assign_mod)
+
+#if 0
+#define DEF_BINARY(f, fun) \
+template <typename T, typename Policy, typename From1, typename From2> \
+inline Extended_Number<T, Policy> \
+f(const From1& x, const From2& y) { \
+  Extended_Number<T, Policy> r; \
+  Extended_Number<T, Policy>::check_result(r.fun(x, y, Rounding(Rounding::CURRENT))); \
+  return r; \
+}
+
+DEF_BINARY(operator +, assign_add)
+DEF_BINARY(operator -, assign_sub)
+DEF_BINARY(operator *, assign_mul)
+DEF_BINARY(operator /, assign_div)
+DEF_BINARY(operator %, assign_mod)
+#endif
+
+#if 0
+#define DEF_COMPARE(f, op) \
+template <typename T, typename Policy> \
+inline bool \
+f(const Extended_Number<T, Policy>& x, const Extended_Number<T, Policy>& y) { \
+  return x.raw_value() op y.raw_value(); \
+}
+
+DEF_COMPARE(operator ==, ==)
+DEF_COMPARE(operator !=, !=)
+DEF_COMPARE(operator >=, >=)
+DEF_COMPARE(operator >, >)
+DEF_COMPARE(operator <=, <=)
+DEF_COMPARE(operator <, <)
+#endif
+
+/*! \relates Extended_Number */
+template <typename T, typename Policy>
+inline Extended_Number<T, Policy>
+operator+(const Extended_Number<T, Policy>& x) {
+  return x;
+}
+
+/*! \relates Extended_Number */
+template <typename T, typename Policy>
+inline Extended_Number<T, Policy>
+operator-(const Extended_Number<T, Policy>& x) {
+  Extended_Number<T, Policy> r;
+  Extended_Number<T, Policy>::check_result(r.assign_neg(x, Rounding(Rounding::CURRENT)));
+  return r;
+}
+
+/*! \relates Extended_Number */
+template <typename T, typename Policy>
+inline int
+sgn(const Extended_Number<T, Policy>& x) {
+  Result r = x.classify(false, false, true);
+  switch (r) {
+  case V_LT:
+    return -1;
+  case V_EQ:
+    return 0;
+  case V_GT:
+    return 1;
+  default:
+    throw(0);
+  }
+}
+
+/*! \relates Extended_Number */
+template <typename From1, typename From2>
+inline int
+cmp(const From1& x, const From2& y) {
+  Result r = compare(x, y);
+  switch (r) {
+  case V_LT:
+    return -1;
+  case V_EQ:
+    return 0;
+  case V_GT:
+    return 1;
+  default:
+    throw(0);
+  }
+}
+
+/*! \relates Extended_Number */
+template <typename T, typename Policy>
+inline std::ostream&
+operator<<(std::ostream& os, const Extended_Number<T, Policy>& x) {
+  Extended_Number<T, Policy>::check_result(Checked::print_ext<Policy>(os, x.raw_value(), Numeric_Format(), Rounding(Rounding::CURRENT)));
+  return os;
+}
+
+#if 0
+/*! \relates Extended_Number */
+template <typename T, typename Policy>
+inline std::istream& operator>>(std::istream& is, Extended_Number<T, Policy>& x) {
+  Extended_Number<T, Policy>::check_result(Checked::input_ext<Policy>(is, x.raw_value(), Rounding(Rounding::CURRENT)));
+  return is;
+}
+#endif
+
+} // namespace Parma_Polyhedra_Library
+
+#endif // !defined(PPL_Extended_Number_inlines_hh)
