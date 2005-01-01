13b9bd9218a89d5d69802a7a84575e2bc1813792
Roberto Bagnara <bagnara@cs.unipr.it>
2005-01-01 22:54:33 +0000

    Another program to test the allocation error recovery facility of the library.

---
+++ tests/Polyhedron/memory2.cc
@@ -0,0 +1,121 @@
+/* Test the allocation error recovery facility of the library.
+   Copyright (C) 2001-2004 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+#include <new>
+
+using namespace std;
+using namespace Parma_Polyhedra_Library;
+
+#ifndef NOISY
+#define NOISY 0
+#endif
+
+// If GMP does not support exceptions the test is pointless.
+#if !GMP_SUPPORTS_EXCEPTIONS
+
+int
+main() TRY {
+  return 0;
+}
+CATCH
+
+#else // GMP_SUPPORTS_EXCEPTIONS
+
+namespace {
+
+unsigned allocated = 0;
+unsigned deallocated = 0;
+
+extern "C" void*
+cxx_malloc(size_t size) {
+  static int k = 0;
+  if (k++ > 7) {
+#if NOISY
+    cout << "std::bad_alloc thrown" << endl;
+#endif
+    throw std::bad_alloc();
+  }
+  void* p = ::operator new(size);
+#if NOISY
+  cout << "allocated " << size << " @ " << p << endl;
+#endif
+  ++allocated;
+  return p;
+}
+
+extern "C" void*
+cxx_realloc(void* p, size_t old_size, size_t new_size) {
+  if (new_size <= old_size) {
+#if NOISY
+  cout << "reallocated " << old_size << " @ " << p
+       << " to " << new_size << " @ " << p
+       << endl;
+#endif
+    return p;
+  }
+  else {
+    void* new_p = ::operator new(new_size);
+    memcpy(new_p, p, old_size);
+    ::operator delete(p);
+#if NOISY
+    cout << "reallocated " << old_size << " @ " << p
+	 << " to " << new_size << " @ " << new_p
+	 << endl;
+#endif
+    return new_p;
+  }
+}
+
+extern "C" void
+cxx_free(void* p, size_t) {
+#if NOISY
+  cout << "deallocated " << p << endl;
+#endif
+  ::operator delete(p);
+  ++deallocated;
+}
+
+} // namespace
+
+int
+main() {
+  mp_set_memory_functions(cxx_malloc, cxx_realloc, cxx_free);
+
+  try {
+    Matrix* matrix = new Matrix(2, 5);
+    delete matrix;
+
+    // We must not get here.
+    return 1;
+  }
+  catch (const std::bad_alloc&) {
+#if NOISY
+    cout << "std::bad_alloc caught" << endl;
+#endif
+  }
+
+  return (allocated == deallocated) ? 0 : 1;
+}
+
+#endif // GMP_SUPPORTS_EXCEPTIONS
