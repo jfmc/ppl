681dc6bafbcfecc3d64be222dc3c02b266608117
Roberto Bagnara <bagnara@cs.unipr.it>
2005-02-17 17:49:10 +0000

    BD_Shape<T>::CC76_narrowing_assign() improved.

--- src/BD_Shape.inlines.hh
+++ src/BD_Shape.inlines.hh
@@ -2046,12 +2046,13 @@ BD_Shape<T>::limited_CH78_extrapolation_assign(const BD_Shape& y,
   assert(OK());
 }
 
-
 template <typename T>
 inline void
 BD_Shape<T>::CC76_narrowing_assign(const BD_Shape& y) {
+  dimension_type space_dim = space_dimension();
+
   // Dimension-compatibility check.
-  if (space_dimension() != y.space_dimension())
+  if (space_dim != y.space_dimension())
     throw_dimension_incompatible("CC76_narrowing_assign(y)", y);
 
 #ifndef NDEBUG
@@ -2063,10 +2064,9 @@ BD_Shape<T>::CC76_narrowing_assign(const BD_Shape& y) {
   }
 #endif
 
-  dimension_type k = space_dimension();
   // If both systems of bounded differences are zero-dimensional,
   // since `*this' contains `y', we simply return `*this'.
-  if (k == 0)
+  if (space_dim == 0)
     return;
 
   closure_assign();
@@ -2131,10 +2131,10 @@ BD_Shape<T>::CC76_narrowing_assign(const BD_Shape& y) {
   // `plus_infinity', we replace it with the value the corresponding
   // constraint of `y'.
   bool changed = false;
-  for (dimension_type i = 0; i <= k; ++i) {
+  for (dimension_type i = 0; i <= space_dim; ++i) {
     DB_Row<T>& dbm_i = dbm[i];
     const DB_Row<T>& y_dbm_i = y.dbm[i];
-    for (dimension_type j = 0; j <= k; ++j) {
+    for (dimension_type j = 0; j <= space_dim; ++j) {
       T& dbm_ij = dbm_i[j];
       const T& y_dbm_ij = y_dbm_i[j];
       if (is_plus_infinity(dbm_ij)) {
