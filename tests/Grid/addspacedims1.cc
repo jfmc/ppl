03659436d495750afd7cb4b29096f32e405a04f9
Matthew Mundell <mundell@noemail.com>
2005-05-09 10:39:13 +0000

    Initial revision.

---
+++ tests/Grid/addspacedims1.cc
@@ -0,0 +1,193 @@
+/* Test Grid::add_space_dimensions_*.
+   Copyright (C) 2005 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace Parma_Polyhedra_Library::IO_Operators;
+
+#define find_variation find_variation_template<Grid>
+
+// add_space_dimensions_and_embed, from congruences.
+
+void
+test1() {
+  nout << "test1:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable D(3);
+  Variable E(4);
+
+  Congruence_System cgs;
+  cgs.insert((A + 0*C %= 0) / 2);
+
+  Grid gr(cgs);
+
+  if (find_variation(gr))
+    exit(1);
+
+  gr.add_space_dimensions_and_embed(2);
+
+  if (find_variation(gr))
+    exit(1);
+
+  Congruence_System known_cgs;
+  known_cgs.insert((A + 0*E %= 0) / 2);
+
+  Grid known_gr(known_cgs);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// add_space_dimensions_and_embed, an empty grid.
+
+void
+test2() {
+  nout << "test2:" << endl;
+
+  Grid gr(2, Grid::EMPTY);
+
+  if (find_variation(gr))
+    exit(1);
+
+  gr.add_space_dimensions_and_embed(3);
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(5, Grid::EMPTY);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// add_space_dimensions_and_embed, the universe grid.
+
+void
+test3() {
+  nout << "test3:" << endl;
+
+  Grid gr(1, Grid::UNIVERSE);
+
+  if (find_variation(gr))
+    exit(1);
+
+  gr.add_space_dimensions_and_embed(4);
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(5, Grid::UNIVERSE);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// add_space_dimensions_and_embed, from generators.
+
+void
+test4() {
+  nout << "test4:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable E(4);
+
+  Generator_System gs;
+  gs.insert(point(A));
+  gs.insert(point(A + C));
+
+  Grid gr(gs);
+
+  if (find_variation(gr))
+    exit(1);
+
+  gr.add_space_dimensions_and_embed(2);
+
+  if (find_variation(gr))
+    exit(1);
+
+  Congruence_System known_cgs;
+  known_cgs.insert((A == 1) / 0);
+  known_cgs.insert((C + 0*E %= 0) / 1);
+  known_cgs.insert((B == 0) / 0);
+
+  Grid known_gr(known_cgs);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+int
+main() TRY {
+  set_handlers();
+
+  nout << "addspacedims1:" << endl;
+
+  test1();
+  test2();
+  test3();
+  test4();
+
+  return 0;
+}
+CATCH
