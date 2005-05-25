8066b6e81dbd87581fad4d050b9fcb649609968f
Matthew Mundell <mundell@noemail.com>
2005-05-25 17:27:52 +0000

    Add tests 2 to 5.

--- tests/Grid/addgenerator1.cc
+++ tests/Grid/addgenerator1.cc
@@ -27,22 +27,165 @@ using namespace Parma_Polyhedra_Library::IO_Operators;
 
 #define find_variation find_variation_template<Grid>
 
+// One dimension.
+
 void
 test1() {
   nout << "test1:" << endl;
 
   Variable A(0);
-  Variable B(1);
 
-  Grid gr(2, Grid::EMPTY);
+  Grid gr(1, Grid::EMPTY);
   gr.add_generator(point(-A));
 
   if (find_variation(gr))
     exit(1);
 
-  Grid known_gr(2);
+  Grid known_gr(1);
   known_gr.add_congruence((A == -1) / 0);
-  known_gr.add_congruence((B == 0) / 0);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// Two dimensions.
+
+void
+test2() {
+  nout << "test2:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  Grid gr(2, Grid::EMPTY);
+  gr.add_generator(point(A + B));
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(2);
+  known_gr.add_congruence((A == 1) / 0);
+  known_gr.add_congruence((B == 1) / 0);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// Add many generators to two dimensions.
+
+void
+test3() {
+  nout << "test3:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  Grid gr(2, Grid::EMPTY);
+  gr.add_generator(point());
+  gr.add_generator(point(A + 2*B));
+  gr.add_generator(point(A + B));
+  gr.add_generator(point(2*A + 2*B));
+  gr.add_generator(line(A));
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(2);
+  known_gr.add_congruence(A %= 0);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// Add NNC generators.
+
+void
+test4() {
+  nout << "test4:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  Generator_System gs;
+  gs.insert(closure_point(3*A, 4));
+  gs.insert(point(7*A, 4));
+  gs.insert(line(A - B));
+
+  Grid gr(2, Grid::EMPTY);
+
+  for (Generator_System::const_iterator i = gs.begin(),
+	 gs_end = gs.end(); i != gs_end; ++i)
+    if ((*i).is_closure_point() == false)
+      gr.add_generator(*i);
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(2);
+  known_gr.add_congruence((4*A + 4*B == 7) / 0);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+// Add generators to a grid of a higher space dimension.
+
+void
+test5() {
+  nout << "test5:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable D(3);
+
+  Grid gr(4, Grid::EMPTY);
+  gr.add_generator(point(7*A, 3));
+  gr.add_generator(line(A - B));
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(4);
+  known_gr.add_congruence((3*A + 3*B == 7) / 0);
+  known_gr.add_congruence((C == 0) / 0);
+  known_gr.add_congruence((D == 0) / 0);
 
   if (find_variation(known_gr))
     exit(1);
@@ -64,6 +207,10 @@ main() TRY {
   nout << "addgenerator1:" << endl;
 
   test1();
+  test2();
+  test3();
+  test4();
+  test5();
 
   return 0;
 }
