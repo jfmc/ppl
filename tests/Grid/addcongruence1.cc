44413a794a380f40d564f548273bb4a3989212c5
Matthew Mundell <mundell@noemail.com>
2005-05-25 18:47:11 +0000

    Initial revision.

---
+++ tests/Grid/addcongruence1.cc
@@ -0,0 +1,76 @@
+/* Test Grid::add_congruence*().
+   Copyright (C) 2001-2005 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
+USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace Parma_Polyhedra_Library::IO_Operators;
+
+#define find_variation find_variation_template<Grid>
+
+// Many cases are covered in addgenerator1, in which the known grid is
+// always created with Grid::add_congruence.
+
+// add_congruence_and_minimize
+
+void
+test1() {
+  nout << "test1:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  Grid gr(2);
+
+  gr.add_congruence((A + B %= 0) / 6);
+  gr.add_congruence_and_minimize((A + B %= 0) / 3);
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(2);
+  known_gr.add_congruence((A + B %= 0) / 6);
+
+  if (find_variation(known_gr))
+    exit(1);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
+int
+main() TRY {
+  set_handlers();
+
+  nout << "addcongruence1:" << endl;
+
+  test1();
+
+  return 0;
+}
+CATCH
