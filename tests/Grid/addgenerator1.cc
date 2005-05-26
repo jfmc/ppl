ee37ef66e0b8f75b4d8d187ee21f4470059d4111
Matthew Mundell <mundell@noemail.com>
2005-05-26 15:01:05 +0000

    Add test7, which adds a generator to a universe grid.

--- tests/Grid/addgenerator1.cc
+++ tests/Grid/addgenerator1.cc
@@ -27,6 +27,8 @@ using namespace Parma_Polyhedra_Library::IO_Operators;
 
 #define find_variation find_variation_template<Grid>
 
+// grid1.cc also tests add_generator_and_minimize.
+
 // One dimension.
 
 void
@@ -235,6 +237,35 @@ test6() {
   exit(1);
 }
 
+// Add a generator to a universe grid.
+
+void
+test7() {
+  nout << "test7:" << endl;
+
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable D(3);
+
+  Grid gr(4);
+  gr.add_generator(point(12*A + 7*D));
+
+  if (find_variation(gr))
+    exit(1);
+
+  Grid known_gr(4);
+
+  if (gr == known_gr)
+    return;
+
+  nout << "Grid should equal known grid." << endl
+       << " grid:" << endl << gr << endl
+       << "known:" << endl << known_gr << endl;
+
+  exit(1);
+}
+
 int
 main() TRY {
   set_handlers();
@@ -247,6 +278,7 @@ main() TRY {
   test4();
   test5();
   test6();
+  test7();
 
   return 0;
 }
