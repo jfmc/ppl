41a0b4a2f79f0012f713ee6655d8f32ff1fbe046
Roberto Bagnara <bagnara@cs.unipr.it>
2005-08-21 22:40:49 +0000

    Address of the Free Software Foundation updated.

--- interfaces/Prolog/tests/clpq.pl
+++ interfaces/Prolog/tests/clpq.pl
@@ -16,9 +16,8 @@
 % for more details.
 %
 % You should have received a copy of the GNU General Public License
-% along with this program; if not, write to the Free Software
-% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-% USA.
+% along with this program; if not, write to the Free Software Foundation,
+% Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
 %
 % For the most up-to-date information see the Parma Polyhedra Library
 % site: http://www.cs.unipr.it/ppl/ .
@@ -486,7 +485,7 @@ show_copying :-
                        Version 2, June 1991\n\
 \n\
  Copyright (C) 1989, 1991 Free Software Foundation, Inc.\n\
-                       59 Temple Place, Suite 330, Boston, MA  02111-1307  USA\n\
+                       51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA\n\
  Everyone is permitted to copy and distribute verbatim copies\n\
  of this license document, but changing it is not allowed.\n\
 \n\
