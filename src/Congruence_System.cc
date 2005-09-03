3745c21dfc4ba69f2ddf01d5b679ddbdc6d25b57
Matthew Mundell <mundell@noemail.com>
2005-09-03 09:53:22 +0000

    Update FSF address.

--- src/Congruence_System.cc
+++ src/Congruence_System.cc
@@ -14,9 +14,8 @@ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307,
-USA.
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
 
 For the most up-to-date information see the Parma Polyhedra Library
 site: http://www.cs.unipr.it/ppl/ . */
@@ -133,6 +132,7 @@ PPL::Congruence_System::insert(const Congruence& cg) {
 
 void
 PPL::Congruence_System::insert(const Constraint& c) {
+  // FIX insert makes a copy of this copy (cg)
   Congruence cg(c);
   insert(cg);
 }
