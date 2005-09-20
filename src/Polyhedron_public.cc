e6c1ce7c2b852632be2d5b6dd24bf78dc2f3bac7
Roberto Bagnara <bagnara@cs.unipr.it>
2005-09-20 07:39:54 +0000

    Unwanted inclusion removed.

--- src/Polyhedron_public.cc
+++ src/Polyhedron_public.cc
@@ -23,7 +23,6 @@ site: http://www.cs.unipr.it/ppl/ . */
 #include <config.h>
 
 #include "Polyhedron.defs.hh"
-//#include "../tests/PFunction.hh"
 #include "scalar_products.defs.hh"
 
 #include <cassert>
