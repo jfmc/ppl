44bddd13a4ddb55012909c84bfe6c67c46524be2
Roberto Bagnara <bagnara@cs.unipr.it>
2005-09-23 13:04:04 +0000

    Test program renamed.  The matrix1.cc (and, possibly, matrix2.cc and so forth) will contain tests of some functionalities of the class Matrix (especially those that are not exercised much in an indirect way by the clients of Matrix).

--- tests/Polyhedron/matrix1.cc
+++ tests/Polyhedron/matrix1.cc
@@ -1,4 +1,4 @@
-/* Test Matrix::ascii_dump() and Matrix::ascii_load().
+/* Test some functionalities of class Matrix.
    Copyright (C) 2001-2005 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
@@ -35,22 +35,16 @@ using namespace Parma_Polyhedra_Library;
 
 namespace {
 
-const char* data_file = "matasciidumpload1.dat";
+const char* data_file = "matrix1.dat";
 
-} // namespace
-
-int
-main() TRY {
-  set_handlers();
+void
+ascii_dump_load() {
   Variable A(0);
   Variable B(1);
 
   gmp_randclass r(gmp_randinit_default);
   // Seed the random number generator.
   r.seed((unsigned long) time(0));
-
-#define ROWS 3
-#define COLS 4
 
   mpz_class cmin = 0;
   mpz_class range = 0;
@@ -59,29 +53,50 @@ main() TRY {
     range = raw_value(std::numeric_limits<COEFFICIENT_TYPE>::max()) - cmin + 1;
   }
 
-  Matrix m1(ROWS, COLS);
-  mpz_class n;
-  for (dimension_type row = 0; row < ROWS; ++row)
-    for (dimension_type col = 0; col < COLS; ++col) {
-      if (std::numeric_limits<COEFFICIENT_TYPE>::is_bounded)
-	n = cmin + r.get_z_range(range);
-      else
-	n = r.get_z_bits(512);
-      m1[row][col] = n;
+  for (dimension_type num_rows = 0; num_rows <= 3; ++num_rows)
+    for (dimension_type num_cols = 0; num_cols <= 3; ++num_cols) {
+      Matrix m1(num_rows, num_cols);
+      for (dimension_type row = 0; row < num_rows; ++row)
+	for (dimension_type col = 0; col < num_cols; ++col) {
+	  mpz_class n;
+	  if (std::numeric_limits<COEFFICIENT_TYPE>::is_bounded)
+	    n = cmin + r.get_z_range(range);
+	  else
+	    n = r.get_z_bits(512);
+	  m1[row][col] = n;
+	}
+
+      fstream f;
+      open(f, data_file, ios_base::out);
+      m1.ascii_dump(f);
+      close(f);
+
+      open(f, data_file, ios_base::in);
+      Matrix m2;
+      m2.ascii_load(f);
+      close(f);
+
+      if (m1 != m2) {
+#if NOISY
+	cout << "Matrix::ascii_dump/load test failed." << endl
+	     << "m1.ascii_dump() gives" << endl;
+	m1.ascii_dump(cout);
+	cout << "m2.ascii_dump() gives" << endl;
+	m2.ascii_dump(cout);
+#endif 
+	exit(1);
+      }
     }
+}
 
-  fstream f;
-  open(f, data_file, ios_base::out);
-  m1.ascii_dump(f);
-  close(f);
+} // namespace
 
-  open(f, data_file, ios_base::in);
-  Matrix m2;
-  m2.ascii_load(f);
-  close(f);
+int
+main() TRY {
+  set_handlers();
 
-  int retval = (m1 == m2) ? 0 : 1;
+  ascii_dump_load();
 
-  return retval;
+  return 0;
 }
 CATCH
