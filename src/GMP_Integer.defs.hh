500e6e2998c26ec1dd3bd1c1dc8cb9eff55f0d9b
Matthew Mundell <mundell@noemail.com>
2005-10-06 09:55:23 +0000

    Take `inline' out of gcdext declarations.

--- src/GMP_Integer.defs.hh
+++ src/GMP_Integer.defs.hh
@@ -77,16 +77,17 @@ void gcd_assign(GMP_Integer& x,
   \p s and \p t the values such that
   \p x (before) * \p s + \p y * \p t = \p x (after).
 */
-inline void gcdext_assign(GMP_Integer& x, const GMP_Integer& y,
-			  GMP_Integer& s, GMP_Integer& t);
+void gcdext_assign(GMP_Integer& x, const GMP_Integer& y,
+		   GMP_Integer& s, GMP_Integer& t);
+
 //! Extended GCD.
 /*!
   Assigns to \p x the greatest common divisor of \p y and \p z, and to
   \p s and \p t the values such that \p y * \p s + \p z * \p t = \p x.
 */
-inline void gcdext_assign(GMP_Integer& x,
-			  const GMP_Integer& y, const GMP_Integer& z,
-			  GMP_Integer& s, GMP_Integer& t);
+void gcdext_assign(GMP_Integer& x,
+		   const GMP_Integer& y, const GMP_Integer& z,
+		   GMP_Integer& s, GMP_Integer& t);
 
 //! Assigns to \p x the least common multiple of \p x and \p y.
 /*! \relates GMP_Integer */
