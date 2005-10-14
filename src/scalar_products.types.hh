d9bbddcf073b58032637722263acb6120799a3ef
Enea Zaffanella <zaffanella@cs.unipr.it>
2005-10-14 07:10:05 +0000

    Put under CVS control.

---
+++ src/scalar_products.types.hh
@@ -0,0 +1,22 @@
+/* Copyright (C) 2001-2005 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is free software; as a special exception the author gives
+unlimited permission to copy and/or distribute it, with or without
+modifications, as long as this notice is preserved.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY, to the extent permitted by law; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. */
+
+#ifndef PPL_scalar_products_types_hh
+#define PPL_scalar_products_types_hh 1
+
+namespace Parma_Polyhedra_Library {
+
+class Scalar_Products;
+class Topology_Adjusted_Scalar_Product_Sign;
+
+} // namespace Parma_Polyhedra_Library
+
+#endif // !defined(PPL_scalar_products_types_hh)
