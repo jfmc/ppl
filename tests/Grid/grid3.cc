056bd4ab54656a03618e9ccbae3c0dcf4d563dd3
Matthew Mundell <mundell@noemail.com>
2005-11-08 13:08:29 +0000

    Align copyright line.

--- tests/Grid/grid3.cc
+++ tests/Grid/grid3.cc
@@ -1,5 +1,5 @@
 /* Test construction of grids from constraints.
-Copyright (C) 2005 Roberto Bagnara <bagnara@cs.unipr.it>
+   Copyright (C) 2005 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
 
