98c9caa3925a61e01a43582998c6d259399a8bd4
Matthew Mundell <mundell@noemail.com>
2005-11-15 11:38:31 +0000

    Add generators1 and mincongruences1, which test Grid::generators() and Grid::minimized_congruences().

---
+++ tests/Grid/generators1.cc
@@ -0,0 +1,277 @@
+/* Test Grid::generators().
+   Copyright (C) 2001-2005 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace Parma_Polyhedra_Library::IO_Operators;
+
+namespace {
+
+Variable A(0);
+Variable B(1);
+Variable C(2);
+Variable D(3);
+
+// Empty grid.
+
+void
+test1() {
+  nout << "test1:" << endl;
+
+  Grid gr1(7, EMPTY);
+
+  Grid known_gr = gr1;
+
+  Generator_System gs = gr1.generators();
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Universe grid.
+
+void
+test2() {
+  nout << "test2:" << endl;
+
+  Grid gr1(7);
+
+  Grid known_gr = gr1;
+
+  Generator_System gs = gr1.generators();
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Zero dimension empty grid.
+
+void
+test3() {
+  nout << "test3:" << endl;
+
+  Grid gr1(0, EMPTY);
+
+  Grid known_gr = gr1;
+
+  Generator_System gs = gr1.generators();
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Zero dimension universe grid.
+
+void
+test4() {
+  nout << "test4:" << endl;
+
+  Grid gr1(0);
+
+  Grid known_gr = gr1;
+
+  Generator_System gs = gr1.generators();
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Skew grid in 3D.
+
+void
+test5() {
+  nout << "test5:" << endl;
+
+  Grid gr1(3, EMPTY);
+  gr1.add_generator(point(3*B));
+  gr1.add_generator(point(5*A + 2*B));
+  gr1.add_generator(point(7*B));
+  gr1.add_generator(line(C));
+
+  Grid known_gr = gr1;
+
+  Generator_System gs = gr1.generators();
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// 3D rectilinear grid defined by congruences.
+
+void
+test6() {
+  nout << "test6:" << endl;
+
+  Grid gr1(3);
+  gr1.add_congruence((A %= 0) / 10);
+  gr1.add_congruence((B %= 10) / 0);
+
+  Grid known_gr = gr1;
+
+  Generator_System gs = gr1.generators();
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Get a reference to the empty generators, add a point, use the
+// reference to create a new grid.
+
+void
+test7() {
+  nout << "test7:" << endl;
+
+  Grid gr1(3, EMPTY);
+
+  const Generator_System& gs = gr1.generators();
+
+  // Add a point.  The idea is to check that `gs' still refers to a
+  // generator system that matches the grid.
+  gr1.add_generator(point());
+
+  Grid known_gr = gr1;
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// In zero dimensions get a reference to the universe generators,
+// empty the grid, and then use the reference to create a new grid.
+
+void
+test8() {
+  nout << "test8:" << endl;
+
+  Grid gr1(0);
+
+  const Generator_System& gs = gr1.generators();
+
+  // Empty the grid.  The idea is to check that `gs' still refers to a
+  // generator system that matches the grid.
+  gr1.add_congruence_and_minimize(Congruence::zero_dim_false());
+
+  Grid known_gr = gr1;
+
+  Grid gr2(gs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+} // namespace
+
+int
+main() TRY {
+  set_handlers();
+
+  nout << "generators1:" << endl;
+
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+  test8();
+
+  return 0;
+}
+CATCH
---
+++ tests/Grid/mincongruences1.cc
@@ -0,0 +1,250 @@
+/* Test Grid::minimized_congruences().
+   Copyright (C) 2001-2005 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include "ppl_test.hh"
+
+using namespace Parma_Polyhedra_Library::IO_Operators;
+
+namespace {
+
+Variable A(0);
+Variable B(1);
+Variable C(2);
+Variable D(3);
+
+// Empty grid.
+
+void
+test1() {
+  nout << "test1:" << endl;
+
+  Grid gr1(4, EMPTY);
+
+  Grid known_gr = gr1;
+
+  Congruence_System cgs = gr1.minimized_congruences();
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Universe grid.
+
+void
+test2() {
+  nout << "test2:" << endl;
+
+  Grid gr1(6);
+
+  Grid known_gr = gr1;
+
+  Congruence_System cgs = gr1.minimized_congruences();
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Zero dimension empty grid.
+
+void
+test3() {
+  nout << "test3:" << endl;
+
+  Grid gr1(0, EMPTY);
+
+  Grid known_gr = gr1;
+
+  Congruence_System cgs = gr1.minimized_congruences();
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Zero dimension universe grid.
+
+void
+test4() {
+  nout << "test4:" << endl;
+
+  Grid gr1(0);
+
+  Grid known_gr = gr1;
+
+  Congruence_System cgs = gr1.minimized_congruences();
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Skew grid in 3D.
+
+void
+test5() {
+  nout << "test5:" << endl;
+
+  Grid gr1(3);
+  gr1.add_congruence((A - B %= 3) / 7);
+  gr1.add_congruence((A - B %= 3) / 14);
+  gr1.add_congruence((A %= 0) / 5);
+  gr1.add_congruence((A %= 0) / 10);
+
+  Grid known_gr = gr1;
+
+  Congruence_System cgs = gr1.minimized_congruences();
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// Get a reference to the minimized_congruences, empty the grid, use the
+// reference to create a new grid.
+
+void
+test6() {
+  nout << "test6:" << endl;
+
+  Grid gr1(3);
+  gr1.add_congruence(Congruence::zero_dim_integrality());
+
+  const Congruence_System& cgs = gr1.minimized_congruences();
+
+  // Empty the grid.  The idea is to check that `cgs' still refers to
+  // a congruence system that matches the grid.
+  gr1.add_congruence(Congruence::zero_dim_false());
+
+  Grid known_gr = gr1;
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+// In zero dimensions get a reference to the universe minimized_congruences,
+// empty the grid, use the reference to create a new grid.
+
+void
+test7() {
+  nout << "test7:" << endl;
+
+  Grid gr1(0);
+  gr1.add_congruence(Congruence::zero_dim_integrality());
+
+  const Congruence_System& cgs = gr1.minimized_congruences();
+
+  // Empty the grid.  The idea is to check that `cgs' still refers to
+  // a congruence system that matches the grid.
+  gr1.add_congruence(Congruence::zero_dim_false());
+
+  Grid known_gr = gr1;
+
+  Grid gr2(cgs);
+
+  if (known_gr == gr2)
+    return;
+
+  nout << "Reproduced grid should equal known grid." << endl
+       << "grid:" << endl << gr2 << endl
+       << "known grid:" << endl << known_gr << endl;
+
+  dump_grids(gr2, known_gr);
+
+  exit(1);
+}
+
+} // namespace
+
+int
+main() TRY {
+  set_handlers();
+
+  nout << "mincongruences1:" << endl;
+
+  test1();
+  test2();
+  test3();
+  test4();
+  test5();
+  test6();
+  test7();
+
+  return 0;
+}
+CATCH
