1cceef5e4b478cc92ea31c9d58704759609ec828
Matthew Mundell <mundell@noemail.com>
2005-11-17 13:14:58 +0000

    Add a local namespace.  Fit fulfils to satisfies_all_congruence.  Take out the closure point tests.  Add tests where the divisor is greater than one.

--- tests/Grid/congruencesystem2.cc
+++ tests/Grid/congruencesystem2.cc
@@ -1,4 +1,4 @@
-/* Test Congruence_System saturate and satisfy member functions.
+/* Test Congruence_System::satisfies_all_congruences().
    Copyright (C) 2001-2005 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
@@ -26,6 +26,8 @@ using namespace Parma_Polyhedra_Library::IO_Operators;
 
 #define find_variation find_variation_template<Congruence_System>
 
+namespace {
+
 class Test_Congruence_System : public Congruence_System {
 public:
   Test_Congruence_System() : Congruence_System() {}
@@ -38,42 +40,33 @@ public:
   }
 };
 
-// FIX Fulfils was for testing saturate and satisfies with one
-//     function; now there is only satisfies.
-
 /* If PASS_EXPECTED is true:
-     If the first generator in GS saturates/satifies (whichever is
-     passed via MEMBER) CGS, then return true, else print an error
-     message and return false.
+     If the first generator in GS satifies CGS, then return true, else
+     print an error message and return false.
 
    And, symmetrically, if PASS_EXPECTED is false:
-     If the first generator in GS fails to saturates/satifies
-     (whichever is passed via MEMBER) CGS, then return false, else
-     print an error message and return true.
+     If the first generator in GS fails to satify CGS, then return
+     false, else print an error message and return true.
 
-   FIX If Generator::ascii_dump was public this could take a
-   Generator.
+   In both cases DIVISOR is passed to satisfies_all_congruences.
 
-   FIX mention DIVISOR
+   FIXME: If Generator::ascii_dump was public this could take a
+          Generator.
 */
 
 bool
 fulfils(const Generator_System& gs,
 	const Test_Congruence_System& cgs,
 	Coefficient_traits::const_reference divisor,
-	bool pass_expected = false,
-	bool(Test_Congruence_System::*member)(const Generator& g,
-					      Coefficient_traits::const_reference d) const
-	= &Test_Congruence_System::satisfies_all_congruences,
-	string verb = "satisfy") {
+	bool pass_expected = false) {
   Generator_System::const_iterator gi = gs.begin();
 
-  if ((cgs.*member)(*gi, divisor) == pass_expected)
+  if (cgs.satisfies_all_congruences(*gi, divisor) == pass_expected)
     return pass_expected;
 
-  nout << *gi << " should ";
-  pass_expected || nout << "fail to ";
-  nout << verb << " " << cgs << "." << endl
+  nout << *gi << " should";
+  pass_expected || nout << "fail to";
+  nout << " satisfy " << cgs << "." << endl
        << "ASCII dump of system holding " << *gi << ":" << endl;
   gs.ascii_dump(nout);
   nout << "ASCII dump of " << cgs << ":" << endl;
@@ -88,20 +81,19 @@ inline bool
 fails_to_satisfy(const Generator_System& gs,
 		 const Congruence_System& cgs,
 		 Coefficient_traits::const_reference divisor) {
-  return false == fulfils(gs, cgs, divisor, true,
-			  &Test_Congruence_System::satisfies_all_congruences,
-			  "satisfy");
+  if (fulfils(gs, cgs, divisor, true))
+    return false;
+  return true;
 }
 
-// satisfies_all_congruences
-// FIX test with other divisors
+Variable A(0);
+Variable B(1);
+
+// Divisor of 1.
 
 void
 test1() {
   nout << "test1:" << endl;
-
-  Variable A(0);
-  Variable B(1);
 
   Test_Congruence_System cgs0((A - B %= 0) / 7);
   if (find_variation(cgs0))
@@ -135,25 +127,8 @@ test1() {
   if (fails_to_satisfy(gs0, cgs0, 1))
     exit(1);
 
-  // Closure points.
-
-  gs0.clear();
-  gs0.insert(closure_point(2*A + B));
-  if (satisfies(gs0, cgs0, 1))
-    exit(1);
-
-  gs0.clear();
-  gs0.insert(closure_point(7*A + 7*B));
-  if (fails_to_satisfy(gs0, cgs0, 1))
-    exit(1);
-
-  gs0.clear();
-  gs0.insert(closure_point(3*A - 4*B));
-  if (fails_to_satisfy(gs0, cgs0, 1))
-    exit(1);
-
-  // FIX Rays are converted to lines.
   // Rays.
+  // FIX Rays are converted to lines, at least for now.
 
   gs0.clear();
   gs0.insert(ray(3*A + 3*B));
@@ -166,7 +141,7 @@ test1() {
     exit(1);
 
   gs0.clear();
-  gs0.insert(ray(- A + 13*B));
+  gs0.insert(ray(-A + 13*B));
   if (fails_to_satisfy(gs0, cgs0, 1))
     exit(1);
 
@@ -197,11 +172,93 @@ test1() {
   return;
 }
 
+// Divisors greater than 1.
+
+void
+test2() {
+  nout << "test2:" << endl;
+
+  Test_Congruence_System cgs0((A - B %= 0) / 7);
+  if (find_variation(cgs0))
+    exit(1);
+
+  Generator_System gs0;
+
+  // Points.
+
+  gs0.clear();
+  gs0.insert(point(A + B, 3));
+  if (fails_to_satisfy(gs0, cgs0, 3))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(point(A + 2*B, 3));
+  if (satisfies(gs0, cgs0, 3))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(point(5*A + 2*B, 5));
+  if (satisfies(gs0, cgs0, 5))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(point(5*A - 2*B, 7));
+  if (satisfies(gs0, cgs0, 7))
+    exit(1);
+
+  // FIX Rays are converted to lines, at least for now.
+  // Rays.
+
+  gs0.clear();
+  gs0.insert(ray(3*A + 3*B));
+  if (fails_to_satisfy(gs0, cgs0, 4))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(ray(A + 14*B));
+  if (satisfies(gs0, cgs0, 5))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(ray(-A + 13*B));
+  if (fails_to_satisfy(gs0, cgs0, 2))
+    exit(1);
+
+  // Lines.
+
+  gs0.clear();
+  gs0.insert(line(13*A + 13*B));
+  if (fails_to_satisfy(gs0, cgs0, 8))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(line(18*A + 14*B));
+  if (satisfies(gs0, cgs0, 9))
+    exit(1);
+
+  gs0.clear();
+  gs0.insert(line(14*A - 21*B));
+  if (satisfies(gs0, cgs0, 10))
+    exit(1);
+
+  cgs0.clear();
+  cgs0.insert((A %= 0) / 2);
+  gs0.clear();
+  gs0.insert(line(3*A));
+  if (satisfies(gs0, cgs0, 11))
+    exit(1);
+
+  return;
+}
+
+} // namespace
+
 int
 main() TRY {
   set_handlers();
 
   test1();
+  test2();
 
   return 0;
 }
