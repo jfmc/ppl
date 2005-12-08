0c71e834653d8ea2aaf2171a5ee759af1f4a2019
Roberto Bagnara <bagnara@cs.unipr.it>
2005-12-08 06:10:59 +0000

    Added support for SWI-Prolog's unbounded numbers.  This requires Prolog version 5.5.40 or later version.

--- interfaces/Prolog/SWI/ppl_swiprolog.cc
+++ interfaces/Prolog/SWI/ppl_swiprolog.cc
@@ -22,10 +22,16 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include <config.h>
 
+// Include gmp.h before SWI-Prolog.h.  This is required in order
+// to get access to interface functions dealing with GMP numbers
+// and SWI-Prolog terms.
+#include <gmp.h>
+#include <SWI-Prolog.h>
+
 #include "Coefficient.defs.hh"
 #include "Checked_Number.defs.hh"
-#include <SWI-Prolog.h>
 #include <cassert>
+#include <stdint.h>
 
 typedef term_t Prolog_term_ref;
 typedef atom_t Prolog_atom;
@@ -68,9 +74,7 @@ long Prolog_max_integer;
 */
 void
 ppl_Prolog_sysdep_init() {
-  Prolog_has_unbounded_integers = false;
-  Prolog_min_integer = PL_query(PL_QUERY_MIN_INTEGER);
-  Prolog_max_integer = PL_query(PL_QUERY_MAX_INTEGER);
+  Prolog_has_unbounded_integers = true;
 }
 
 /*!
@@ -103,8 +107,6 @@ Prolog_put_term(Prolog_term_ref t, Prolog_term_ref u) {
 */
 inline int
 Prolog_put_long(Prolog_term_ref t, long l) {
-  if (l < Prolog_min_integer || l > Prolog_max_integer)
-    throw PPL_integer_out_of_range(l);
   PL_put_integer(t, l);
   return 1;
 }
@@ -114,9 +116,15 @@ Prolog_put_long(Prolog_term_ref t, long l) {
 */
 inline int
 Prolog_put_ulong(Prolog_term_ref t, unsigned long ul) {
-  if (ul > static_cast<unsigned long>(Prolog_max_integer))
-    throw PPL_integer_out_of_range(ul);
-  PL_put_integer(t, ul);
+  if (ul <= LONG_MAX)
+    PL_put_integer(t, ul);
+  else if (ul <= std::numeric_limits<int64_t>::max())
+    PL_put_int64(t, static_cast<int64_t>(ul));
+  else {
+    mpz_class z;
+    PPL::assign(z, ul, PPL::ROUND_NOT_NEEDED);
+    PL_unify_mpz(t, z.get_mpz_t());
+  }
   return 1;
 }
 
@@ -349,18 +357,19 @@ Prolog_unify(Prolog_term_ref t, Prolog_term_ref u) {
 PPL::Coefficient
 integer_term_to_Coefficient(Prolog_term_ref t) {
   assert(Prolog_is_integer(t));
-  long v;
-  Prolog_get_long(t, &v);
-  return PPL::Coefficient(v);
+  mpz_class v;
+  PL_get_mpz(t, v.get_mpz_t());
+  PPL::Coefficient r;
+  PPL::assign(r, v, PPL::ROUND_NOT_NEEDED);
+  return r;
 }
 
 Prolog_term_ref
 Coefficient_to_integer_term(const PPL::Coefficient& n) {
-  long v;
-  if (PPL::assign(v, PPL::raw_value(n), PPL::ROUND_NOT_NEEDED) != PPL::V_EQ)
-    throw PPL_integer_out_of_range(n);
+  mpz_class v;
+  PPL::assign(v, PPL::raw_value(n), PPL::ROUND_NOT_NEEDED);
   Prolog_term_ref t = Prolog_new_term_ref();
-  Prolog_put_long(t, v);
+  PL_unify_mpz(t, v.get_mpz_t());
   return t;
 }
 
