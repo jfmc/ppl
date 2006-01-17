fc469482b6d8281917f9407b1f1f27fa8fd51829
Enea Zaffanella <zaffanella@cs.unipr.it>
2006-01-17 08:42:48 +0000

    Hiding initializers of enumeration Result. try to uniformize the documentation of enumeration values.

--- src/Result.defs.hh
+++ src/Result.defs.hh
@@ -26,83 +26,84 @@ site: http://www.cs.unipr.it/ppl/ . */
 namespace Parma_Polyhedra_Library {
 
 //! Possible outcomes of a checked arithmetic computation.
+/*! \ingroup PPL_CXX_interface */
 enum Result {
 
   VC_MASK = 48,
 
-  //! Ordinary result class.
+  //! \hideinitializer Ordinary result class.
   VC_NORMAL = 0,
 
-  //! The computed result is inexact and rounded up.
+  //! \hideinitializer The computed result is inexact and rounded up.
   V_LT = 1,
 
-  //! The computed result is inexact and rounded down.
+  //! \hideinitializer The computed result is inexact and rounded down.
   V_GT = 2,
 
-  //! The computed result is exact.
+  //! \hideinitializer The computed result is exact.
   V_EQ = 4,
 
-  //! The computed result is inexact.
+  //! \hideinitializer The computed result is inexact.
   V_NE = V_LT | V_GT,
 
-  //! The computed result may be inexact and rounded up.
+  //! \hideinitializer The computed result may be inexact and rounded up.
   V_LE = V_EQ | V_LT,
 
-  //! The computed result may be inexact and rounded down.
+  //! \hideinitializer The computed result may be inexact and rounded down.
   V_GE = V_EQ | V_GT,
 
-  //! The computed result may be inexact.
+  //! \hideinitializer The computed result may be inexact.
   V_LGE = V_LT | V_EQ | V_GT,
 
-  //! Negative infinity unrepresentable result class.
+  //! \hideinitializer Negative infinity unrepresentable result class.
   VC_MINUS_INFINITY = 16,
 
-  //! A negative overflow occurred.
+  //! \hideinitializer A negative overflow occurred.
   V_NEG_OVERFLOW = VC_MINUS_INFINITY | V_GT,
 
-  //! Positive infinity unrepresentable result class.
+  //! \hideinitializer Positive infinity unrepresentable result class.
   VC_PLUS_INFINITY = 32,
 
-  //! A positive overflow occurred.
+  //! \hideinitializer A positive overflow occurred.
   V_POS_OVERFLOW = VC_PLUS_INFINITY | V_LT,
 
-  //! Not a number result class.
+  //! \hideinitializer Not a number result class.
   VC_NAN = 48,
 
-  //! Conversion from unknown string.
+  //! \hideinitializer Converting from unknown string.
   V_CVT_STR_UNK = 49,
 
-  //! x / 0.
+  //! \hideinitializer Dividing by zero.
   V_DIV_ZERO = 50,
 
-  //! +/-inf + -/+inf.
+  //! \hideinitializer Adding two infinities having opposite signs.
   V_INF_ADD_INF = 51,
 
-  //! +/-inf / -/+inf.
+  //! \hideinitializer Dividing two infinities.
   V_INF_DIV_INF = 52,
 
-  //! +/-inf mod x.
+  //! \hideinitializer Taking the modulus of an infinity.
   V_INF_MOD = 53,
 
-  //! +/-inf * 0.
+  //! \hideinitializer Multiplying an infinity by zero.
   V_INF_MUL_ZERO = 54,
 
-  //! +/-inf - +/-inf.
+  //! \hideinitializer Subtracting two infinities having the same sign.
   V_INF_SUB_INF = 55,
 
-  //! x mod 0.
+  //! \hideinitializer Computing a remainder modulo zero.
   V_MOD_ZERO = 56,
 
-  //! sqrt(-x).
+  //! \hideinitializer Taking the square root of a negative number.
   V_SQRT_NEG = 57,
 
-  //! Unknown result due to intermediate negative overflow.
+  //! \hideinitializer Unknown result due to intermediate negative overflow.
   V_UNKNOWN_NEG_OVERFLOW = 58,
 
-  //! Unknown result due to intermediate positive overflow.
+  //! \hideinitializer Unknown result due to intermediate positive overflow.
   V_UNKNOWN_POS_OVERFLOW = 59,
 
-  //! Unordered comparison.
+  //! \hideinitializer Unordered comparison.
   V_UNORD_COMP = 60
 };
 
