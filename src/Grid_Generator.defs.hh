bab932071cf951074451dc7f51af2fd3e3c89436
Matthew Mundell <mundell@noemail.com>
2006-01-30 17:04:56 +0000

    Correct the Grid class friend declaration comment.

--- src/Grid_Generator.defs.hh
+++ src/Grid_Generator.defs.hh
@@ -424,9 +424,8 @@ private:
 
   friend std::ostream&
   IO_Operators::operator<<(std::ostream& s, const Grid_Generator& g);
-  // FIXME: The following friend declaration is for operator[],
-  //        divisor() and negate() access in Grid::conversion and
-  //        Grid::simplify.
+  // FIXME: The following friend declaration is for operator[] and
+  //        divisor() access in Grid::conversion and Grid::simplify.
   friend class Grid;
 
   friend class Grid_Generator_System;
