63023ed61061f87c547388112e8eb5eca04409cf
Roberto Bagnara <bagnara@cs.unipr.it>
2006-02-06 20:51:05 +0000

    SPARC floating point unit related functions.

---
+++ src/fpu-sparc.inlines.hh
@@ -0,0 +1,73 @@
+/* SPARC floating point unit related functions.
+   Copyright (C) 2001-2006 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifdef HAVE_IEEEFP_H
+#include <ieeefp.h>
+
+#define FPU_TONEAREST FP_RN
+#define FPU_UPWARD FP_RM
+#define FPU_DOWNWARD FP_RP
+#define FPU_TOWARDZERO FP_RZ
+
+namespace Parma_Polyhedra_Library {
+
+inline int
+fpu_get_rounding_direction() {
+  return fpgetround();
+}
+
+inline void
+fpu_set_rounding_direction(int dir) {
+  fpsetround(dir);
+}
+
+inline int
+fpu_save_rounding_direction(int dir) {
+  return fpsetround(dir);
+}
+
+inline void
+fpu_reset_inexact() {
+  fp_except except = fpgetmask();
+  except &= ~FP_X_IMP;
+  fpsetmask(except);
+}
+
+inline int
+fpu_save_rounding_direction_reset_inexact(int dir) {
+  fpu_reset_inexact();
+  return fpu_save_rounding_direction(dir);
+}
+
+inline void
+fpu_restore_rounding_direction(int dir) {
+  fpsetround(dir);
+}
+
+inline int
+fpu_check_inexact() {
+  return (fpgetmask() & FP_X_IMP) ? 1 : 0;
+}
+
+} // namespace Parma_Polyhedra_Library
+
+#endif // !defined(HAVE_IEEEFP_H)
