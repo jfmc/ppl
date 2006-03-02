c0b6588b5cadd90a14f974114c586406a9224973
Patricia M. Hill <hill@comp.leeds.ac.uk>
2006-03-02 07:56:42 +0000

    Revised tests using new guidelines

--- tests/Grid/join1.cc
+++ tests/Grid/join1.cc
@@ -22,16 +22,14 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-using namespace Parma_Polyhedra_Library::IO_Operators;
-
 namespace {
 
-Variable A(0);
-Variable B(1);
-Variable C(2);
+// Two grids each with one point and two lines
+bool
+test01() {
+  Variable B(1);
+  Variable C(2);
 
-void
-test1() {
   Grid_Generator_System gs1;
   gs1.insert(grid_point(C));
 
@@ -39,12 +37,11 @@ test1() {
   gs2.insert(grid_point(B + 0*C));
 
   Grid gr1(gs1);
+  print_generators(gr1, "*** gr1 ***");
   Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.join_assign(gr2);
-
-  if (find_variation(gr1))
-    exit(1);
 
   Grid_Generator_System known_gs;
   known_gs.insert(grid_point(C));
@@ -52,45 +49,41 @@ test1() {
 
   Grid known_gr(known_gs);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
 // Two universe grids.
-
-void
-test2() {
+bool
+test02() {
   Grid gr1(3);
+  print_generators(gr1, "*** gr1 ***");
   Grid gr2(3);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.upper_bound_assign(gr2);
 
-  if (find_variation(gr1))
-    exit(1);
-
   Grid known_gr(3);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.upper_bound_assign(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
 // Second grid universe.
+bool
+test03() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test3() {
   Grid gr1(3);
+  print_generators(gr1, "*** gr1 ***");
 
   Grid_Generator_System gs;
   gs.insert(grid_point());
@@ -99,28 +92,26 @@ test3() {
   gs.insert(grid_line(-C));
 
   Grid gr2(gs);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.join_assign(gr2);
 
-  if (find_variation(gr1))
-    exit(1);
-
   Grid known_gr(3);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
 // Inserting a parameter.
+bool
+test04() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test4() {
   Grid_Generator_System gs1;
   gs1.insert(grid_point(0*C));
   gs1.insert(grid_line(A));
@@ -129,85 +120,82 @@ test4() {
   Grid gr1(gs1);
 
   gr1.add_generator(parameter(-C));
+  print_generators(gr1, "*** gr1 ***");
 
   Grid_Generator_System gs2;
   gs2.insert(grid_point(0*C));
 
   Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.upper_bound_assign(gr2);
-
-  if (find_variation(gr1))
-    exit(1);
 
   Grid known_gr(3);
   known_gr.add_congruence(C %= 0);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
-// Divisor normalization.
+// join_assign_and_minimize - Divisor normalization.
+bool
+test05() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test5() {
   Grid_Generator_System gs1;
   gs1.insert(grid_point(0*C));
   gs1.insert(grid_line(A));
   gs1.insert(grid_line(B));
 
   Grid gr1(gs1);
+  print_generators(gr1, "*** gr1 ***");
 
   Grid_Generator_System gs2;
   gs2.insert(grid_point());
   gs2.insert(grid_point(C, 3));
 
   Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.join_assign_and_minimize(gr2);
-
-  if (find_variation(gr1))
-    exit(1);
 
   Congruence_System known_cgs;
   known_cgs.insert((3*C %= 0) / 1);
 
   Grid known_gr(known_cgs);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign_and_minimize(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
 // Out-of-date generators in the first grid.
+bool
+test06() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test6() {
   Grid gr1(3);
   gr1.add_congruence(A == 0);
   gr1.add_congruence(B == 0);
   gr1.add_congruence(C == 0);
+  print_generators(gr1, "*** gr1 ***");
 
   Grid_Generator_System gs2;
   gs2.insert(grid_point(B + 0*C));
 
   Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.join_assign(gr2);
-
-  if (find_variation(gr1))
-    exit(1);
 
   Grid_Generator_System known_gs;
   known_gs.insert(grid_point());
@@ -215,34 +203,33 @@ test6() {
 
   Grid known_gr(known_gs);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
 // Out-of-date generators in the second grid.
+bool
+test07() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test7() {
   Grid_Generator_System gs;
   gs.insert(grid_point(B + 0*C));
 
   Grid gr1(gs);
+  print_generators(gr1, "*** gr1 ***");
 
   Grid gr2(3);
   gr2.add_congruence(A == 0);
   gr2.add_congruence(B == 0);
   gr2.add_congruence(C == 0);
+  print_congruences(gr2, "*** gr2 ***");
 
   gr1.upper_bound_assign(gr2);
-
-  if (find_variation(gr1))
-    exit(1);
 
   Grid_Generator_System known_gs;
   known_gs.insert(grid_point());
@@ -250,20 +237,20 @@ test7() {
 
   Grid known_gr(known_gs);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.upper_bound_assign(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
 // Space dimension exception.
+bool
+test08() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test8() {
   Grid_Generator_System gs;
   gs.insert(grid_point(B + 0*C));
 
@@ -276,60 +263,114 @@ test8() {
 
   try {
     gr1.upper_bound_assign(gr2);
-    nout << "Exception expected." << endl;
-    exit(1);
   }
-  catch (const std::invalid_argument& e) {}
+  catch (const std::invalid_argument& e) {
+    nout << "invalid_argument: " << e.what() << endl;
+  }
+  catch (...) {
+    return false;
+  }
+  return true;
 }
 
 // Out-of-date generators in the first grid, which is empty.
+bool
+test09() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test9() {
   Grid gr1(3);
   gr1.add_congruence(A == 0);
   gr1.add_congruence(A == 1);
+  print_congruences(gr1, "*** gr1 ***");
 
   Grid_Generator_System gs2;
   gs2.insert(grid_point(B + 0*C));
 
   Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
 
   Grid known_gr = gr2;
 
   gr1.join_assign(gr2);
 
-  if (find_variation(gr1))
-    exit(1);
+  bool ok = (gr1 == known_gr);
 
-  if (gr1 == known_gr)
-    return;
+  print_congruences(gr1, "*** gr1.join_assign(gr2) ***");
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  return ok;
+}
 
-  exit(1);
+// Based on an example in a paper by Muller-Olm and Seidl in SAS 2005
+// Here there is an input and output version of each variable
+// A, B being input and A1, B1 the output.
+bool
+test10() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable D(3);
+
+  Grid gr0(4); // initial point
+  gr0.add_congruence(A == 2);
+  gr0.add_congruence(B == 0);
+
+  Grid gr1(4); // a pass through the procedure may do nothing
+  gr1.add_congruence(A == C);
+  gr1.add_congruence(B == D);
+
+  gr1.intersection_assign(gr0); // add the inital point
+  print_congruences(gr1, "*** gr1 ***");
+
+  Grid gr2(4); // one non-trivial pass through procedure
+  gr2.add_congruence(15 * A == C);
+  gr2.add_congruence(18 * A + B == D);
+
+  gr2.intersection_assign(gr0); // add the inital point
+  print_congruences(gr2, "*** gr2 ***");
+
+  Grid gr3(4); // two non-trivial passes through procedure
+  gr3.add_congruence(225 * A == C);
+  gr3.add_congruence(282 * A + B == D);
+
+  gr3.intersection_assign(gr0); // add the inital point
+  print_congruences(gr3, "*** gr3 ***");
+
+  gr1.join_assign(gr2); // combine alternative paths 1 and 2
+  print_generators(gr1, "*** gr1.join_assign(gr2) ***");
+  gr1.join_assign(gr3); // combine alternative paths 1, 2 and 3
+  print_generators(gr1, "*** gr1.join_assign(gr3) ***");
+
+  Variables_Set vars;
+  vars.insert(A);
+  vars.insert(B);
+
+  gr1.remove_space_dimensions(vars);
+
+  Grid known_gr(2); // as in paper
+
+  known_gr.add_congruence((A %= 2) / 28);
+  known_gr.add_congruence((B %= 0) / 12);
+
+  bool ok = (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.remove_space_dimensions(vars) ***");
+
+  return ok;
 }
 
 } // namespace
 
-int
-main() TRY {
-  set_handlers();
-
-  nout << "join1:" << endl;
-
-  DO_TEST(test1);
-  DO_TEST(test2);
-  DO_TEST(test3);
-  DO_TEST(test4);
-  DO_TEST(test5);
-  DO_TEST(test6);
-  DO_TEST(test7);
-  DO_TEST(test8);
-  DO_TEST(test9);
-
-  return 0;
-}
-CATCH
+BEGIN_MAIN
+  NEW_TEST(test01);
+  NEW_TEST(test02);
+  NEW_TEST(test03);
+  NEW_TEST(test04);
+  NEW_TEST(test05);
+  NEW_TEST(test06);
+  NEW_TEST(test07);
+  NEW_TEST(test08);
+  NEW_TEST(test09);
+  NEW_TEST_F8(test10);
+END_MAIN
--- tests/Grid/join2.cc
+++ tests/Grid/join2.cc
@@ -22,16 +22,16 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-using namespace Parma_Polyhedra_Library::IO_Operators;
-
 namespace {
 
-Variable A(0);
-Variable B(1);
-Variable C(2);
+// join_assign_and_minimize - two grids in 3D
+// each defined by a single point.
+bool
+test01() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
 
-void
-test1() {
   Grid_Generator_System gs1;
   gs1.insert(grid_point(A + 0*C));
 
@@ -39,12 +39,11 @@ test1() {
   gs2.insert(grid_point(2*A + 0*C));
 
   Grid gr1(gs1);
+  print_generators(gr1, "*** gr1 ***");
   Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.join_assign_and_minimize(gr2);
-
-  if (find_variation(gr1))
-    exit(1);
 
   Grid_Generator_System known_gs;
   known_gs.insert(grid_point(A + 0*C));
@@ -52,51 +51,275 @@ test1() {
 
   Grid known_gr(known_gs);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign_and_minimize(gr2) ***");
 
-  exit(1);
+  return ok;
 }
 
-// Two universe grids.
-
-void
-test2() {
+// join_assign_and_minimize - Two universe grids.
+bool
+test02() {
   Grid gr1(3);
+  print_generators(gr1, "*** gr1 ***");
   Grid gr2(3);
+  print_generators(gr2, "*** gr2 ***");
 
   gr1.join_assign_and_minimize(gr2);
 
-  if (find_variation(gr1))
-    exit(1);
-
   Grid known_gr(3);
 
-  if (gr1 == known_gr)
-    return;
+  bool ok = (gr1 == known_gr);
 
-  nout << "Grid should equal known grid." << endl
-       << " grid:" << endl << gr1 << endl
-       << "known:" << endl << known_gr << endl;
+  print_congruences(gr1, "*** gr1.join_assign_and_minimize(gr2) ***");
 
-  exit(1);
+  return ok;
+}
+
+// join_assign and join_assign_if_exact - Two empty grids.
+bool
+test03() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  Grid gr1(4, EMPTY);
+  print_generators(gr1, "*** gr1 ***");
+  Grid gr2(4, EMPTY);
+  print_generators(gr2, "*** gr2 ***");
+
+  Grid known_gr(4, EMPTY);
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - First grid empty.
+bool
+test04() {
+  Variable A(0);
+
+  Grid gr1(4, EMPTY);
+  print_generators(gr1, "*** gr1 ***");
+
+  Grid gr2(4, EMPTY);
+  gr2.add_generator(grid_point(2*A));
+  print_generators(gr2, "*** gr2 ***");
+
+  Grid known_gr = gr2;
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - Second grid empty.
+bool
+test05() {
+  Variable B(1);
+
+  Grid gr1(4, EMPTY);
+  gr1.add_generator(grid_point());
+  gr1.add_generator(grid_line(B));
+  print_generators(gr1, "*** gr1 ***");
+
+  Grid gr2(4, EMPTY);
+  print_generators(gr2, "*** gr2 ***");
+
+  Grid known_gr = gr1;
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - Zero dimension universes.
+bool
+test06() {
+  Grid gr1(0);
+  print_generators(gr1, "*** gr1 ***");
+  Grid gr2(0);
+  print_generators(gr2, "*** gr2 ***");
+
+  Grid known_gr = gr1;
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - First included in second.
+bool
+test07() {
+  Variable A(0);
+
+  Grid gr1(1);
+  gr1.add_congruence((A %= 0) / 2);
+  print_congruences(gr1, "*** gr1 ***");
+
+  Grid gr2(1);
+  gr2.add_congruence(A %= 0);
+  print_congruences(gr2, "*** gr2 ***");
+
+  Grid known_gr = gr2;
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - Second included in first.
+bool
+test08() {
+  Variable A(0);
+  Variable B(1);
+
+  Grid gr1(2);
+  gr1.add_congruence(A - B %= 0);
+  print_congruences(gr1, "*** gr1 ***");
+
+  Grid gr2(2);
+  gr2.add_congruence(A - B == 0);
+  print_congruences(gr2, "*** gr2 ***");
+
+  Grid known_gr = gr1;
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - Exact join.
+bool
+test09() {
+  Variable A(0);
+  Variable B(1);
+
+  Grid gr1(2);
+  gr1.add_congruence((A %= 0) / 2);
+  gr1.add_congruence((B %= 0) / 2);
+  print_congruences(gr1, "*** gr1 ***");
+
+  Grid gr2(2);
+  gr2.add_congruence((A %= 1) / 2);
+  gr2.add_congruence((B %= 1) / 2);
+  print_congruences(gr2, "*** gr2 ***");
+
+  Grid known_gr(2);
+  known_gr.add_congruence((A - B %= 0) / 2);
+  known_gr.add_congruence(A %= 0);
+
+  bool ok = (gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// join_assign_if_exact - Two points (join adds more points).
+bool
+test10() {
+  Variable A(0);
+
+  Grid_Generator_System gs1;
+  gs1.insert(grid_point(A));
+
+  Grid_Generator_System gs2;
+  gs2.insert(grid_point(3*A, 5));
+
+  Grid gr1(gs1);
+  print_generators(gr1, "*** gr1 ***");
+  Grid gr2(gs2);
+  print_generators(gr2, "*** gr2 ***");
+
+  Grid known_gr = gr1;
+
+  bool ok = (!gr1.join_assign_if_exact(gr2));
+
+  if (ok)
+    ok &= (gr1 == known_gr);
+
+  print_congruences(gr1, "*** gr1.join_assign_if_exact(gr2) ***");
+
+  return ok;
+}
+
+// upper_bound_assign_if_exact - Space dimension exception.
+bool
+test11() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  Grid_Generator_System gs;
+  gs.insert(grid_point(B + 0*C));
+
+  Grid gr1(gs);
+
+  Grid gr2(4);
+  gr2.add_congruence(A == 0);
+  gr2.add_congruence(B == 0);
+  gr2.add_congruence(C == 0);
+
+  try {
+    gr1.upper_bound_assign_if_exact(gr2);
+  }
+  catch (const std::invalid_argument& e) {
+    nout << "invalid_argument: " << e.what() << endl;
+  }
+  catch (...) {
+    return false;
+  }
+  return true;
 }
 
 } // namespace
 
-int
-main() TRY {
-  set_handlers();
-
-  nout << "join2:" << endl;
-
-  DO_TEST(test1);
-  DO_TEST(test2);
-
-  return 0;
-}
-CATCH
+BEGIN_MAIN
+  NEW_TEST(test01);
+  NEW_TEST(test02);
+  NEW_TEST(test03);
+  NEW_TEST(test04);
+  NEW_TEST(test05);
+  NEW_TEST(test06);
+  NEW_TEST(test07);
+  NEW_TEST(test08);
+  NEW_TEST(test09);
+  NEW_TEST(test10);
+  NEW_TEST(test11);
+END_MAIN
