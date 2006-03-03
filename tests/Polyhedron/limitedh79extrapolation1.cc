077edde51e759cf55c42acbe01efeeeed0c32388
Enea Zaffanella <zaffanella@cs.unipr.it>
2006-03-03 07:34:16 +0000

    Adapted to new standards for tests.

--- tests/Polyhedron/limitedh79extrapolation1.cc
+++ tests/Polyhedron/limitedh79extrapolation1.cc
@@ -22,8 +22,10 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-int
-main() TRY {
+namespace {
+
+bool
+test01() {
   Variable x(0);
   Variable y(1);
 
@@ -66,6 +68,206 @@ main() TRY {
   print_constraints(computed_result,
 		    "*** After limited_H79_extrapolation_assign ****");
 
-  return (computed_result == known_result) ? 0 : 1;
+  return computed_result == known_result;
 }
-CATCH
+
+bool
+test02() {
+  Variable x(0);
+  Variable y(1);
+
+  Constraint_System cs1;
+  cs1.insert(x >= 0);
+  cs1.insert(x <= 1);
+  cs1.insert(y == 0);
+
+  C_Polyhedron ph1(cs1);
+
+  print_constraints(ph1, "*** ph1 ****");
+
+  Constraint_System cs2;
+  cs2.insert(x <= 2);
+  cs2.insert(y >= 0);
+  cs2.insert(y <= x);
+
+  C_Polyhedron ph2(cs2);
+
+  print_constraints(ph2, "*** ph2 ****");
+
+  Constraint_System cs;
+  cs.insert(y <= -1);
+  cs.insert(x <= 5);
+
+  print_constraints(cs, "*** cs ****");
+
+  C_Polyhedron computed_result = ph2;
+  computed_result.limited_H79_extrapolation_assign(ph1, cs);
+
+  C_Polyhedron known_result = ph2;
+  known_result.add_generator(point(5*x));
+  known_result.add_generator(point(5*x + 5*y));
+
+  print_constraints(computed_result,
+		    "*** After limited_H79_extrapolation_assign ****");
+
+  return computed_result == known_result;
+}
+
+bool
+test03() {
+  Variable x(0);
+
+  Constraint_System cs1;
+  cs1.insert(x >= 0);
+  cs1.insert(x <= 1);
+  C_Polyhedron ph1(cs1);
+
+  print_constraints(ph1, "*** ph1 ****");
+
+  Constraint_System cs2;
+  cs2.insert(x == 0);
+  C_Polyhedron ph2(cs2);
+
+  print_constraints(ph2, "*** ph2 ****");
+
+  Constraint_System cs;
+  cs.insert(x >= 0);
+
+  print_constraints(cs, "*** cs ****");
+
+  C_Polyhedron computed_result = ph1;
+  computed_result.limited_H79_extrapolation_assign(ph2, cs);
+
+  C_Polyhedron known_result(cs);
+
+  print_constraints(computed_result,
+		    "*** After limited_H79_extrapolation_assign ****");
+
+  return computed_result == known_result;
+}
+
+bool
+test04() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph1(2, EMPTY);
+
+  C_Polyhedron ph2(2);
+  ph2.add_constraint(B >= 0);
+  ph2.add_constraint(A - B >= 0);
+  ph2.add_constraint(A <= 2);
+
+  Constraint_System cs;
+  cs.insert(B <= 4);
+
+  C_Polyhedron known_result(ph2);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+  print_constraints(cs, "*** cs ***");
+
+  ph2.limited_H79_extrapolation_assign(ph1, cs);
+
+  bool ok = (ph2 == known_result);
+
+  print_constraints(ph2,
+		    "*** After ph2.limited_H79_extrapolation_assign(ph1, cs)"
+		    " ***");
+  return ok;
+}
+
+bool
+test05() {
+  Variable B(1);
+
+  C_Polyhedron ph1(2, EMPTY);
+
+  C_Polyhedron ph2(2, EMPTY);
+
+  Constraint_System cs;
+  cs.insert(B <= 4);
+
+  C_Polyhedron known_result(ph2);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+  print_constraints(cs, "*** cs ***");
+
+  ph2.limited_H79_extrapolation_assign(ph1, cs);
+
+  bool ok = (ph2 == known_result);
+
+  print_constraints(ph2,
+		    "*** After ph2.limited_H79_extrapolation_assign(ph1, cs)"
+		    " ***");
+  return ok;
+}
+
+bool
+test06() {
+  C_Polyhedron ph1;
+
+  C_Polyhedron ph2;
+
+  Constraint_System cs;
+  cs.insert(Linear_Expression(2) <= 4);
+
+  C_Polyhedron known_result(ph2);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+  print_constraints(cs, "*** cs ***");
+
+  ph2.limited_H79_extrapolation_assign(ph1, cs);
+
+  bool ok = (ph2 == known_result);
+
+  print_constraints(ph2,
+		    "*** After ph2.limited_H79_extrapolation_assign(ph1, cs)"
+		    " ***");
+  return ok;
+}
+
+bool
+test07() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph1(2);
+  ph1.add_constraint(A >= 2);
+  ph1.add_constraint(A <= -2);
+  ph1.add_constraint(B == 0);
+  C_Polyhedron ph2(2);
+  ph1.add_constraint(A >= 2);
+
+  Constraint_System cs;
+  cs.insert(B <= 4);
+
+  C_Polyhedron known_result(ph2);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+  print_constraints(cs, "*** cs ***");
+
+  ph2.limited_H79_extrapolation_assign(ph1, cs);
+
+  bool ok = (ph2 == known_result);
+
+  print_constraints(ph2,
+		    "*** After ph2.limited_H79_extrapolation_assign(ph1, cs)"
+		    " ***");
+  return ok;
+}
+
+} // namespace
+
+BEGIN_MAIN
+  NEW_TEST(test01);
+  NEW_TEST(test02);
+  NEW_TEST(test03);
+  NEW_TEST(test04);
+  NEW_TEST(test05);
+  NEW_TEST(test06);
+  NEW_TEST(test07);
+END_MAIN
