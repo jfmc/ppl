4dae26e84ce830c3ec014ae0d8bd25422e10fb38
Enea Zaffanella <zaffanella@cs.unipr.it>
2006-03-04 19:50:59 +0000

    Adapted polyhull*.cc to the new standards for tests.

--- tests/Polyhedron/polyhull1.cc
+++ tests/Polyhedron/polyhull1.cc
@@ -22,10 +22,10 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-int
-main() TRY {
-  set_handlers();
+namespace {
 
+bool
+test01() {
   Variable x(0);
   Variable y(1);
 
@@ -61,6 +61,344 @@ main() TRY {
 
   print_generators(computed_result, "*** ph1.poly_hull_assign(ph2) ***");
 
-  return (computed_result == known_result) ? 0 : 1;
+  return (computed_result == known_result);
 }
-CATCH
+
+bool
+test02() {
+  Variable x(0);
+  Variable y(1);
+
+  C_Polyhedron ph1(2, EMPTY);
+
+  Generator_System gs;
+  gs.insert(point());
+  gs.insert(ray(x + y));
+  C_Polyhedron ph2(gs);
+
+  print_generators(ph1, "*** ph1 ***");
+  print_generators(ph2, "*** ph2 ***");
+
+  C_Polyhedron computed_result1(ph1);
+
+  computed_result1.poly_hull_assign_and_minimize(ph2);
+
+  C_Polyhedron computed_result2(ph1);
+  computed_result2.poly_hull_assign(ph2);
+
+  C_Polyhedron known_result(ph2);
+
+  bool ok = (computed_result1 == known_result
+	     && computed_result2 == known_result);
+
+  print_generators(computed_result1,
+		   "*** After poly_hull_assign_and_minimize ***");
+  print_generators(computed_result2, "*** After poly_hull_assign ***");
+
+  return ok;
+}
+
+bool
+test03() {
+  Variable x(0);
+  Variable y(1);
+
+  C_Polyhedron ph1(2);
+  ph1.add_constraint(x >= 0);
+  ph1.add_constraint(y >= 0);
+  ph1.add_constraint(x <= 2);
+  ph1.add_constraint(y <= 2);
+
+  C_Polyhedron ph2(2);
+  ph2.add_constraint(y >= 2);
+  ph2.add_constraint(y <= 4);
+  ph2.add_constraint(x >= 0);
+  ph2.add_constraint(x <= 2);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+
+  ph1.poly_hull_assign(ph2);
+
+  print_generators(ph1, "*** After poly_hull_assign ***");
+
+  C_Polyhedron known_result(2, EMPTY);
+  known_result.add_generator(point());
+  known_result.add_generator(point(2*x));
+  known_result.add_generator(point(4*y));
+  known_result.add_generator(point(2*x + 4*y));
+
+  bool ok = (ph1 == known_result);
+
+  return ok;
+}
+
+bool
+aux_test04(C_Polyhedron& ph1, const C_Polyhedron& ph2,
+	   // Note intentional call-by-value!
+	   C_Polyhedron known_result) {
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+
+  ph1.poly_hull_assign_and_minimize(ph2);
+
+  print_generators(ph1, "*** After poly_hull_assign ***");
+
+  return ph1 == known_result;
+}
+
+bool
+test04() {
+  Variable x(0);
+  Variable y(1);
+
+  C_Polyhedron ph1_1(2);
+  ph1_1.add_constraint(x >= 0);
+  ph1_1.add_constraint(y >= 0);
+  ph1_1.add_constraint(x <= 2);
+  ph1_1.add_constraint(y <= 2);
+  C_Polyhedron ph1_2(ph1_1);
+
+  C_Polyhedron ph2_1(2);
+  ph2_1.add_constraint(x+y <= 0);
+  ph2_1.add_constraint(x+y >= 2);
+  C_Polyhedron ph2_2(ph2_1);
+  C_Polyhedron ph2_3(ph2_1);
+  C_Polyhedron ph2_4(ph2_1);
+
+  bool ok = aux_test04(ph1_1, ph2_1, ph1_1)
+    && aux_test04(ph2_2, ph1_2, ph1_2)
+    && aux_test04(ph2_3, ph2_4, ph2_3);
+
+  return ok;
+}
+
+bool
+test05() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph1(2, EMPTY);
+  ph1.add_generator(point(A));
+  ph1.add_generator(ray(A));
+  ph1.add_generator(ray(B));
+  C_Polyhedron ph2(2, EMPTY);
+
+  print_generators(ph1, "*** ph1 ***");
+  print_generators(ph2, "*** ph2 ***");
+
+  C_Polyhedron known_result(ph1);
+
+  ph1.poly_hull_assign_and_minimize(ph2);
+
+  bool ok = (ph1 == known_result);
+
+  print_generators(ph1,
+		   "*** After ph1.poly_hull_assign_and_minimize(ph2) ***");
+
+  return ok;
+}
+
+bool
+test06() {
+  C_Polyhedron ph1;
+  C_Polyhedron ph2;
+
+  print_generators(ph1, "*** ph1 ***");
+  print_generators(ph2, "*** ph2 ***");
+
+  C_Polyhedron known_result(ph1);
+
+  ph1.poly_hull_assign_and_minimize(ph2);
+
+  bool ok = (ph1 == known_result);
+
+  print_generators(ph1,
+		   "*** After ph1.poly_hull_assign_and_minimize(ph2) ***");
+
+  return ok;
+}
+
+bool
+test07() {
+  Variable A(0);
+  Variable B(1);
+
+  Generator_System gs1;
+  gs1.insert(point());
+  gs1.insert(ray(A));
+  gs1.insert(point(B));
+  C_Polyhedron ph1(gs1);
+  ph1.generators();
+  ph1.constraints();
+
+  Generator_System gs2;
+  gs2.insert(point());
+  gs2.insert(ray(B));
+  C_Polyhedron ph2(gs2);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_generators(ph1, "*** ph1 ***");
+  print_generators(ph2, "*** ph2 ***");
+
+  ph1.poly_hull_assign_and_minimize(ph2);
+
+  C_Polyhedron known_result(2);
+  known_result.add_constraint(A >= 0);
+  known_result.add_constraint(B >= 0);
+
+  bool ok = (ph1 == known_result);
+
+  print_generators(ph1,
+		   "*** After ph1.poly_hull_assugn_and_minimize(ph2) ***");
+
+  return ok;
+}
+
+bool
+test08() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph1(2);
+  ph1.add_constraint(A - B >= 0);
+  C_Polyhedron ph2(2, EMPTY);
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+
+  C_Polyhedron known_result(ph1);
+
+  ph1.poly_hull_assign(ph2);
+
+  bool ok = (ph1 == known_result);
+
+  print_constraints(ph1, "*** After ph1.poly_hull_assign(ph2) ***");
+
+  return ok;
+}
+
+bool
+test09() {
+  C_Polyhedron ph1;
+  C_Polyhedron ph2;
+
+  print_constraints(ph1, "*** ph1 ***");
+  print_constraints(ph2, "*** ph2 ***");
+
+  C_Polyhedron known_result(ph1);
+
+  ph1.poly_hull_assign(ph2);
+
+  bool ok = (ph1 == known_result);
+
+  print_constraints(ph1, "*** After ph1.poly_hull_assign(ph2) ***");
+
+  return ok;
+}
+
+bool
+test10() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph1(2);
+  ph1.generators();
+  ph1.add_constraint(A == B);
+  C_Polyhedron copy_ph1 = ph1;
+
+  C_Polyhedron ph2(2);
+  ph2.generators();
+  ph2.add_constraint(A >= B + 1);
+  C_Polyhedron copy_ph2 = ph2;
+
+  print_generators(ph1, "*** ph1 ***");
+  print_generators(ph2, "*** ph2 ***");
+
+  ph1.poly_hull_assign(ph2);
+  copy_ph1.poly_hull_assign(copy_ph2);
+
+  bool ok = (ph1 == copy_ph1);
+
+  print_generators(ph1, "*** After poly_hull_assign ***");
+  print_generators(copy_ph1,
+		    "*** After poly_hull_assign_and_minimize ***");
+
+  return ok;
+}
+
+bool
+test11() {
+  Variable A(0);
+  Variable B(1);
+
+  C_Polyhedron ph1(2, EMPTY);
+  ph1.add_generator(point());
+  ph1.constraints();
+  ph1.add_generator(line(A + B));
+  C_Polyhedron copy_ph1 = ph1;
+
+  C_Polyhedron ph2(2, EMPTY);
+  ph2.add_generator(point());
+  ph2.constraints();
+  ph2.add_generator(ray(A));
+  ph2.add_generator(ray(B));
+
+  C_Polyhedron copy_ph2 = ph2;
+
+  print_generators(ph1, "*** ph1 ***");
+  print_generators(ph2, "*** ph2 ***");
+
+  ph1.poly_hull_assign(ph2);
+  copy_ph1.poly_hull_assign(copy_ph2);
+
+  bool ok = (ph1 == copy_ph1);
+
+  print_generators(ph1, "*** After poly_hull_assign ***");
+  print_generators(copy_ph1,
+		    "*** After poly_hull_assign_and_minimize ***");
+
+  return ok;
+}
+
+bool
+test12() {
+  // Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  C_Polyhedron p(3);
+  p.add_constraint(B >= 0);
+  p.add_constraint(C >= 0);
+
+  C_Polyhedron q(3);
+  q.add_constraint(C >= 0);
+
+  print_constraints(p, "*** p ***");
+  print_constraints(q, "*** q ***");
+
+  p.poly_hull_assign_and_minimize(q);
+
+  bool ok = (p == q);
+
+  print_constraints(p, "*** p.poly_hull_assign_and_minimize(q) ***");
+
+  return ok;
+}
+
+} // namespace
+
+BEGIN_MAIN
+  NEW_TEST(test01);
+  NEW_TEST(test02);
+  NEW_TEST(test03);
+  NEW_TEST(test04);
+  NEW_TEST(test05);
+  NEW_TEST(test06);
+  NEW_TEST(test07);
+  NEW_TEST(test08);
+  NEW_TEST(test09);
+  NEW_TEST(test10);
+  NEW_TEST(test11);
+  NEW_TEST(test12);
+END_MAIN
