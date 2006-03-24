bd7fab4fc1603586860d09305e93837e90db82aa
Roberto Bagnara <bagnara@cs.unipr.it>
2006-03-24 21:43:08 +0000

    Spurious semicolons removed.

--- src/Polyhedra_Powerset.templates.hh
+++ src/Polyhedra_Powerset.templates.hh
@@ -570,7 +570,7 @@ Polyhedra_Powerset<PH>::ascii_dump(std::ostream& s) const {
     xi->element().ascii_dump(s);
 }
 
-PPL_OUTPUT_TEMPLATE_DEFINITIONS(PH, Polyhedra_Powerset<PH>);
+PPL_OUTPUT_TEMPLATE_DEFINITIONS(PH, Polyhedra_Powerset<PH>)
 
 template <typename PH>
 bool
