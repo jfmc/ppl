2260b195d1b0387d6aed48c4ce3b2d02a46fcefe
Roberto Bagnara <bagnara@cs.unipr.it>
2006-04-07 06:45:58 +0000

    Tests for Octagon::concatenate_assign() rewritten according to the new guidelines.

--- tests/Octagon/concatenate1.cc
+++ tests/Octagon/concatenate1.cc
@@ -22,19 +22,10 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-using namespace std;
-using namespace Parma_Polyhedra_Library;
+namespace {
 
-#ifndef NOISY
-#define NOISY 0
-#endif
-
-#ifndef NOISY
-#define NOIY 0
-#endif
-
-int
-main() TRY {
+bool
+test01() {
   Variable x1(0);
   Variable x2(1);
   Variable x3(2);
@@ -45,28 +36,23 @@ main() TRY {
   Variable x8(7);
   Variable x9(8);
 
-
   TOctagon oc1(6);
-  TOctagon oc2(3);
-
-
   oc1.add_constraint(x2 - x3 <= 0);
   oc1.add_constraint(x3 <= 2);
   oc1.add_constraint(x6 - x5 <= 2);
   oc1.add_constraint(x5 <= 3);
 
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(3);
   oc2.add_constraint(x2 - x3 <= 2);
   oc2.add_constraint(x3 <= 7);
 
-#if NOISY
-  print_constraints(oc1, "*** oc1 ***");
   print_constraints(oc2, "*** oc2 ***");
-#endif
 
   oc1.concatenate_assign(oc2);
 
-  TOctagon known_result(9);
-
+  Octagon<mpq_class> known_result(9);
   known_result.add_constraint(x2 - x3 <= 0);
   known_result.add_constraint(x3 <= 2);
   known_result.add_constraint(x6 - x5 <= 2);
@@ -74,13 +60,223 @@ main() TRY {
   known_result.add_constraint(x8 - x9 <= 2);
   known_result.add_constraint(x9 <= 7);
 
+  bool ok = (Octagon<mpq_class>(oc1) == known_result);
 
-#if NOISY
   print_constraints(oc1, "*** oc1.concatenate_assign(oc2) ***");
-#endif
 
-  int retval = (oc1 == known_result) ? 0 : 1;
-
-  return retval;
+  return ok;
 }
-CATCH
+
+bool
+test02() {
+  Variable x(0);
+  Variable y(1);
+
+  TOctagon oc1(2);
+  oc1.add_constraint(x <= 3);
+  oc1.add_constraint(x - y <= 4);
+
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(0, EMPTY);
+
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc2.concatenate_assign(oc1);
+
+  Octagon<mpq_class> known_result(2, EMPTY);
+
+  bool ok = (Octagon<mpq_class>(oc2) == known_result);
+
+  print_constraints(oc2, "*** oc2.concatenate_assign(oc1) ***");
+
+  return ok;
+}
+
+bool
+test03() {
+  Variable x(0);
+  Variable y(1);
+
+  TOctagon oc1(0, EMPTY);
+
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(2);
+  oc2.add_constraint(x <= 3);
+  oc2.add_constraint(x - y <= 4);
+
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc2.concatenate_assign(oc1);
+
+  Octagon<mpq_class> known_result(2, EMPTY);
+
+  bool ok = (Octagon<mpq_class>(oc2) == known_result);
+
+  print_constraints(oc2, "*** oc2.concatenate_assign(oc1) ***");
+
+  return ok;
+}
+
+bool
+test04() {
+  Variable x1(0);
+  Variable x2(1);
+  Variable x3(2);
+  Variable x4(3);
+  Variable x5(4);
+  Variable x6(5);
+  Variable x7(6);
+  Variable x8(7);
+  Variable x9(8);
+
+  TOctagon oc1(6);
+  oc1.add_constraint(x2 + x3 <= 1);
+  oc1.add_constraint(x3 <= 2);
+  oc1.add_constraint(x6 - x5 <= 2);
+  oc1.add_constraint(x5 <= 3);
+
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(3);
+  oc2.add_constraint(x2 + x3 <= 77);
+  oc2.add_constraint(x3 <= 7);
+
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc1.concatenate_assign(oc2);
+
+  Octagon<mpq_class> known_result(9);
+  known_result.add_constraint(x2 + x3 <= 1);
+  known_result.add_constraint(x3 <= 2);
+  known_result.add_constraint(x6 - x5 <= 2);
+  known_result.add_constraint(x5 <= 3);
+  known_result.add_constraint(x8 + x9 <= 77);
+  known_result.add_constraint(x9 <= 7);
+
+  bool ok = (Octagon<mpq_class>(oc1) == known_result);
+
+  print_constraints(oc1, "*** oc1.concatenate_assign(oc2) ***");
+
+  return ok;
+}
+
+bool
+test05() {
+  TOctagon oc1(4);
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(0);
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc1.concatenate_assign(oc2);
+
+  Octagon<mpq_class> known_result(4);
+
+  bool ok = (Octagon<mpq_class>(oc1) == known_result);
+
+  print_constraints(oc1, "*** oc1.concatenate_assign(oc2) ***");
+
+  return ok;
+}
+
+bool
+test06() {
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oc1(4);
+  oc1.add_constraint(A - B <= 0);
+
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(0);
+
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc2.concatenate_assign(oc1);
+
+  Octagon<mpq_class> known_result(oc1);
+
+  bool ok = (Octagon<mpq_class>(oc2) == known_result);
+
+  print_constraints(oc2, "*** oc2.concatenate_assign(oc1) ***");
+
+  return ok;
+}
+
+bool
+test07() {
+  TOctagon oc1(0, EMPTY);
+
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(0, EMPTY);
+
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc1.concatenate_assign(oc2);
+
+  Octagon<mpq_class> known_result(0, EMPTY);
+
+  bool ok = (Octagon<mpq_class>(oc1) == known_result);
+
+  return ok;
+}
+
+bool
+test08() {
+  Variable x1(0);
+  Variable x2(1);
+  Variable x3(2);
+  Variable x4(3);
+  Variable x5(4);
+  Variable x6(5);
+  Variable x7(6);
+  Variable x8(7);
+  Variable x9(8);
+
+  TOctagon oc1(6);
+  oc1.add_constraint(x2 - x3 <= 1);
+  oc1.add_constraint(x3 <= 12);
+  oc1.add_constraint(x6 - x5 <= 2);
+  oc1.add_constraint(x5 <= 23);
+
+  print_constraints(oc1, "*** oc1 ***");
+
+  TOctagon oc2(3);
+  oc2.add_constraint(x2 - x3 <= 2);
+  oc2.add_constraint(x3 <= 8);
+
+  print_constraints(oc2, "*** oc2 ***");
+
+  oc1.concatenate_assign(oc2);
+
+  Octagon<mpq_class> known_result(9);
+  known_result.add_constraint(x2 - x3 <= 1);
+  known_result.add_constraint(x3 <= 12);
+  known_result.add_constraint(x6 - x5 <= 2);
+  known_result.add_constraint(x5 <= 23);
+  known_result.add_constraint(x8 - x9 <= 2);
+  known_result.add_constraint(x9 <= 8);
+
+  bool ok = (Octagon<mpq_class>(oc1) == known_result);
+
+  print_constraints(oc1, "*** oc1.concatenate_assign(oc2) ***");
+
+  return ok;
+}
+
+} // namespace
+
+BEGIN_MAIN
+  DO_TEST(test01);
+  DO_TEST(test02);
+  DO_TEST(test03);
+  DO_TEST(test04);
+  DO_TEST(test05);
+  DO_TEST(test06);
+  DO_TEST(test07);
+  DO_TEST(test08);
+END_MAIN
