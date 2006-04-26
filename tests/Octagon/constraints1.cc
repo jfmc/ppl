455743a174a59aec3fa2112b34ffcfbd9664035a
Elena Mazzi <mazzi@no.email>
2006-04-26 10:44:20 +0000

    Tests for constraints()	rewritten according to the new guidelines.

--- tests/Octagon/constraints1.cc
+++ tests/Octagon/constraints1.cc
@@ -24,35 +24,253 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-using namespace std;
-using namespace Parma_Polyhedra_Library;
+namespace {
 
-#ifndef NOISY
-#define NOISY 0
-#endif
-
-int
-main() TRY {
+bool
+test01() {
   Variable A(0);
 
   TOctagon oct1(2);
   oct1.add_constraint(0*A == 1);
 
-#if NOISY
-  print_constraints(oct1, "*** oct constraints ***");
-#endif
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result = oct1;
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);  
+  
+  print_constraints(oct2, "*** oct2 ***");
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test02() {
+  TOctagon oct1(3, EMPTY);
+
+  print_constraints(oct1, "*** oct1 ***");
 
   Octagon<mpq_class> known_result = oct1;
 
   Constraint_System cs = oct1.constraints();
   TOctagon oct2(cs);
 
-  int retval = (oct2 == known_result) ? 0 : 1;
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
 
-#if NOISY
   print_constraints(cs, "*** cs ***");
-#endif
 
-  return retval;
+  return ok;
 }
-CATCH
+
+bool
+test03() {
+  Variable x(0);
+  Variable y(1);
+  Variable z(2);
+
+  TOctagon oct1(3);
+  oct1.add_constraint(x == 1);
+  oct1.add_constraint(y <= 3);
+  oct1.add_constraint(y - x >= 5);
+  oct1.add_constraint(z + x == 2);
+  oct1.add_constraint(z - y <= 7);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result = oct1;
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test04() {
+  TOctagon oct1(0, EMPTY);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result(oct1);
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs.space_dimension(), EMPTY);
+
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test05() {
+  TOctagon oct1(0);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result(oct1);
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test06() {
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oct1(2);
+
+  oct1.add_constraint(A >= 0);
+  oct1.add_constraint(B >= 0);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result(oct1);
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test07() {
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oct1(2);
+
+  oct1.add_constraint(A == 0);
+  oct1.add_constraint(B >= 1);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result(oct1);
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test08() {
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oct1(2);
+
+  oct1.add_constraint(A + B == 0);
+  oct1.add_constraint(B <= 1);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result(oct1);
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test09() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TOctagon oct1(3);
+
+  oct1.add_constraint(A - B == 0);
+  oct1.add_constraint(B <= 1);
+  oct1.add_constraint(C + A <= 2);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result(oct1);
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+bool
+test10() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable D(3);
+  Variable E(4);
+
+  TOctagon oct1(5);
+  oct1.add_constraint(A == 1);
+  oct1.add_constraint(B + C == 1);
+  oct1.add_constraint(E - D == 1);
+  oct1.add_constraint(A + C <= 1);
+  oct1.add_constraint(B - D <= 1);
+  oct1.add_constraint(E + C >= -1);
+  oct1.add_constraint(A - D >= -1);
+
+  print_constraints(oct1, "*** oct1 ***");
+
+  Octagon<mpq_class> known_result = oct1;
+
+  Constraint_System cs = oct1.constraints();
+  TOctagon oct2(cs);
+
+  bool ok = (Octagon<mpq_class>(oct2) == known_result);
+
+  print_constraints(cs, "*** cs ***");
+
+  return ok;
+}
+
+} // namespace
+
+BEGIN_MAIN
+  DO_TEST(test01);
+  DO_TEST(test02);
+  DO_TEST(test03);
+  DO_TEST(test04);
+  DO_TEST(test05);
+  DO_TEST(test06);
+  DO_TEST(test07);
+  DO_TEST(test08);
+  DO_TEST(test09);
+END_MAIN
+
