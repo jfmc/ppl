d5772f0526efe9522e500a993fc7fb4d88b3fb77
Barbara Quartieri <quartier@no.email>
2006-04-27 10:53:07 +0000

    Tests for Octagon::affine_image() rewritten according to the new guidelines.

--- tests/Octagon/ascii_dump_load1.cc
+++ tests/Octagon/ascii_dump_load1.cc
@@ -22,19 +22,16 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-using namespace std;
-using namespace Parma_Polyhedra_Library;
-
-#ifndef NOISY
-#define NOISY 0
-#endif
 #include "files.hh"
+#include <string>
 #include <fstream>
 
-static const char* my_file = "ascii_dump_load1.dat";
+namespace {
 
-int
-main() TRY {
+const char* my_file = "ascii_dump_load1.dat";
+
+bool
+test01() {
   Variable A(0);
   Variable B(1);
 
@@ -53,8 +50,239 @@ main() TRY {
   oc2.ascii_load(f);
   close(f);
 
-  int retval = (oc1 == oc2) ? 0 : 1;
+  print_constraints(oc1, "*** oc1 ***");
+  print_constraints(oc2, "*** oc2 ***");
 
-  return retval;
+  bool ok = (oc1 == oc2);
+
+  return ok;
 }
-CATCH
+
+bool
+test02() {
+  Variable A(0);
+  Variable B(1);
+  
+  TOctagon oc1(3);
+  oc1.add_constraint(A - B >= 0);
+
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc1.ascii_dump(f);
+  f.seekp(0);
+  f << "A";
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  bool ok = !oc2.ascii_load(f);
+  close(f);
+
+  return ok;
+}
+
+bool
+test03() {
+
+  nout << "test03()" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oc(2);
+  oc.add_constraint(A >= 0);
+  oc.add_constraint(B >= 0);
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in | ios_base::out);
+  string str;
+  do
+    f >> str;
+  while (str != "-EM");
+  f.seekp(0, ios_base::cur);
+  f << " A";
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  bool ok = !oc2.ascii_load(f);
+  close(f);
+
+  return ok;
+}
+
+bool
+test04() {
+
+  cout << "test04()" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oc(2);
+  oc.add_constraint(A >= 0);
+  oc.add_constraint(B >= 1);
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in | ios_base::out);
+  string str;
+  do
+    f >> str;
+  while(str != "+ZE");
+  f.seekp(0, ios_base::cur);
+  f << "A";
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  bool ok = !oc2.ascii_load(f);
+  close(f);
+
+  return ok;
+}
+
+bool
+test05() {
+
+  nout << "test05()" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oc(2);
+  oc.add_constraint(A >= 0);
+  oc.add_constraint(B >= 2);
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in | ios_base::out);
+  string str;
+  do
+    f >> str;
+  while(str != "-SC");
+  f.seekp(0, ios_base::cur);
+  f << "A";
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  bool ok = !oc2.ascii_load(f);
+  close(f);
+
+  return ok;
+}
+
+bool
+test06() {
+
+  nout << "test06()" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oc(2);
+  oc.add_constraint(A >= 0);
+  oc.add_constraint(B >= 3);
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in | ios_base::out);
+  string str;
+  do
+    f >> str;
+  while(str != "+inf");
+  f.seekp(0, ios_base::cur);
+  f << "A";
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  bool ok = !oc2.ascii_load(f);
+  close(f);
+
+  return ok;
+}
+
+bool
+test07() {
+
+  cout << "test07()" << endl;
+
+  Variable A(0);
+  Variable B(1);
+
+  TOctagon oc(2);
+  oc.add_constraint(A >= 0);
+  oc.add_constraint(B >= 3);
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in | ios_base::out);
+  string str;
+  do
+    f >> str;
+  while(str != "+inf");
+  do
+    f >> str;
+  while(str != "+inf");
+  f.seekp(0, ios_base::cur);
+  f << " 3 ";
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  bool ok = !oc2.ascii_load(f);
+  close(f);
+
+  return ok;
+}
+
+bool
+test08() {
+  TOctagon oc1(0, EMPTY);
+
+  fstream f;
+  open(f, my_file, ios_base::out);
+  oc1.ascii_dump(f);
+  close(f);
+
+  open(f, my_file, ios_base::in);
+  TOctagon oc2;
+  oc2.ascii_load(f);
+  close(f);
+
+  bool ok = (oc1 == oc2);
+
+  return ok;
+}
+
+} // namespace
+
+BEGIN_MAIN
+  DO_TEST(test01);
+  DO_TEST(test02);
+  DO_TEST(test03);
+  DO_TEST(test04);
+  DO_TEST(test05);
+  DO_TEST(test06);
+  DO_TEST(test07);
+  DO_TEST(test08);
+END_MAIN
