c8c754f1b3f383c2f3efaa0f3654545d4a3526ad
Enea Zaffanella <zaffanella@cs.unipr.it>
2006-04-30 13:16:23 +0000

    Avoid useless inclusions of iostream, sstream and stdexcept. Avoid a few useless braces. Deleted useless function strong_coherence_local_step(). Deleted functions get_matrix_element() (use OR_Matrix<T>::at() instead). Some improvements (in particular, in assertion checking) in methods add_octagonal_constraint() and forget_octagonal_constraints(). A few documentation improvements.

--- src/Octagonal_Shape.inlines.hh
+++ src/Octagonal_Shape.inlines.hh
@@ -28,9 +28,6 @@ site: http://www.cs.unipr.it/ppl/ . */
 #include "Poly_Con_Relation.defs.hh"
 #include "Poly_Gen_Relation.defs.hh"
 #include <cassert>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
 #include <algorithm>
 
 // FIXME: this is only to get access to
@@ -52,10 +49,9 @@ Octagonal_Shape<T>::Octagonal_Shape(const dimension_type num_dimensions,
   : matrix(num_dimensions), space_dim(num_dimensions), status() {
   if (kind == EMPTY)
     status.set_empty();
-  else if (num_dimensions > 0) {
+  else if (num_dimensions > 0)
     // A (non zero-dim) universe octagon is strongly closed.
     status.set_strongly_closed();
-  }
   assert(OK());
 }
 
@@ -69,7 +65,8 @@ template <typename T>
 inline
 Octagonal_Shape<T>::Octagonal_Shape(const Constraint_System& cs)
   : matrix(cs.space_dimension()),
-    space_dim(cs.space_dimension()) {
+    space_dim(cs.space_dimension()),
+    status() {
   if (cs.space_dimension() > 0)
     // A (non zero-dim) universe octagon is strong closed.
     status.set_strongly_closed();
@@ -121,17 +118,15 @@ template <typename T>
 inline bool
 operator==(const Octagonal_Shape<T>& x, const Octagonal_Shape<T>& y) {
   if (x.space_dim != y.space_dim)
-    // If the two octagons are dimension-incompatible, then they cannot be
-    // the same octagon.
+    // Dimension-incompatible OSs are different.
     return false;
 
-  // Zero-dim octagons are equal if and only if they are both empty or universe.
-  if (x.space_dim == 0) {
+  // Zero-dim OSs are equal if and only if they are both empty or universe.
+  if (x.space_dim == 0)
     if (x.marked_empty())
       return y.marked_empty();
     else
       return !y.marked_empty();
-  }
 
   x.strong_closure_assign();
   y.strong_closure_assign();
@@ -141,7 +136,7 @@ operator==(const Octagonal_Shape<T>& x, const Octagonal_Shape<T>& y) {
     return y.marked_empty();
   if (y.marked_empty())
     return false;
-
+  // Strong closure is a canonical form.
   return x.matrix == y.matrix;
 }
 
@@ -166,47 +161,6 @@ Octagonal_Shape<T>::set_zero_dim_univ() {
 }
 
 template <typename T>
-inline void
-strong_coherence_local_step(T& result, const T& a, const T& b, const T& c) {
-  // This is an auxiliary function used in the method strong_closure_assign()
-  // to realize a step of local strong-coherence.
-  // It assigns to `result':
-  // c,                if `a' or/and `b' are plus infinity;
-  // min(c, (a+b)/2),  otherwise.
-  if (is_plus_infinity(a) ||
-      is_plus_infinity(b))
-    result = c;
-  else {
-    // Compute (a+b)/2 into `result', rounding the result towards plus infinity.
-    T sum;
-    add_assign_r(sum, a, b, ROUND_UP);
-    div2exp_assign_r(result, sum, 1, ROUND_UP);
-    Implementation::BD_Shapes::min_assign(result, c);
-  }
-}
-
-inline dimension_type
-coherent_index(const dimension_type& i) {
-  return (i%2) ? i-1 : i+1;
-}
-
-template <typename T>
-inline const T&
-get_matrix_element(const OR_Matrix<T>& mat, const dimension_type i,
-		   const dimension_type j) {
-  return (j < mat.row_size(i)) ? mat[i][j]
-    : mat[coherent_index(j)][coherent_index(i)];
-}
-
-template <typename T>
-inline T&
-get_matrix_element(OR_Matrix<T>& mat, const dimension_type i,
-		   const dimension_type j) {
-  return (j < mat.row_size(i)) ? mat[i][j]
-    : mat[coherent_index(j)][coherent_index(i)];
-}
-
-template <typename T>
 inline Constraint_System
 Octagonal_Shape<T>::minimized_constraints() const {
   strong_reduction_assign();
@@ -219,13 +173,13 @@ Octagonal_Shape<T>
 ::add_octagonal_constraint(const dimension_type i,
 			   const dimension_type j,
 			   N k) {
-  using Implementation::BD_Shapes::div_round_up;
-
   // Private method: the caller has to ensure the following.
+#ifndef NDEBUG
   assert(i < 2*space_dim && j < 2*space_dim && i != j);
-  typename OR_Matrix<N>::row_iterator i_iter = matrix.row_begin() + i;
-  typename OR_Matrix<N>::row_reference_type r_i = *i_iter;
-  N& r_i_j = r_i[j];
+  typename OR_Matrix<N>::row_iterator m_i = matrix.row_begin() + i;
+  assert(j < m_i.row_size());
+#endif
+  N& r_i_j = matrix[i][j];
   if (r_i_j > k) {
     r_i_j = k;
     if (marked_strongly_closed())
@@ -242,10 +196,13 @@ Octagonal_Shape<T>
 			   Coefficient_traits::const_reference num,
 			   Coefficient_traits::const_reference den) {
   using Implementation::BD_Shapes::div_round_up;
-
+#ifndef NDEBUG
   // Private method: the caller has to ensure the following.
   assert(i < 2*space_dim && j < 2*space_dim && i != j);
+  typename OR_Matrix<N>::row_iterator m_i = matrix.row_begin() + i;
+  assert(j < m_i.row_size());
   assert(den != 0);
+#endif
   N k;
   div_round_up(k, num, den);
   add_octagonal_constraint(i, j, k);
@@ -255,20 +212,21 @@ template <typename T>
 inline void
 Octagonal_Shape<T>
 ::forget_all_octagonal_constraints(const dimension_type v) {
-  assert(v < 2*space_dim);
-  typename OR_Matrix<N>::row_iterator v_iter = matrix.row_begin() + v;;
-  typename OR_Matrix<N>::row_reference_type r_v = *v_iter;
-  typename OR_Matrix<N>::row_reference_type r_cv = *(++v_iter);
-  for (dimension_type h = v_iter.row_size(); h-- > 0; ) {
+  assert(v % 2 == 0 && v < 2*space_dim);
+  const dimension_type cv = v + 1;
+  typename OR_Matrix<N>::row_iterator m_iter = matrix.row_begin() + v;
+  typename OR_Matrix<N>::row_reference_type r_v = *m_iter;
+  typename OR_Matrix<N>::row_reference_type r_cv = *(++m_iter);
+  for (dimension_type h = m_iter.row_size(); h-- > 0; ) {
     r_v[h] = PLUS_INFINITY;
     r_cv[h] = PLUS_INFINITY;
   }
-  ++v_iter;
-  for (typename OR_Matrix<N>::row_iterator v_end = matrix.row_end();
-       v_iter != v_end; ++v_iter) {
-    typename OR_Matrix<N>::row_reference_type r = *v_iter;
+  ++m_iter;
+  for (typename OR_Matrix<N>::row_iterator m_end = matrix.row_end();
+       m_iter != m_end; ++m_iter) {
+    typename OR_Matrix<N>::row_reference_type r = *m_iter;
     r[v] = PLUS_INFINITY;
-    r[v+1] = PLUS_INFINITY;
+    r[cv] = PLUS_INFINITY;
   }
 }
 
@@ -276,30 +234,28 @@ template <typename T>
 inline void
 Octagonal_Shape<T>
 ::forget_binary_octagonal_constraints(const dimension_type v) {
-  assert(v < 2*space_dim);
-  const dimension_type h = v + 2;
-  for (typename OR_Matrix<N>::row_iterator iter_h = matrix.row_begin() + h,
-	 iter_end = matrix.row_end(); iter_h != iter_end; ++iter_h) {
-    typename OR_Matrix<N>::row_reference_type r_h = *iter_h;
-    r_h[v] = PLUS_INFINITY;
-    r_h[v+1] = PLUS_INFINITY;
-  }
-  typename OR_Matrix<N>::row_iterator iter_v = matrix.row_begin() + v;
-  typename OR_Matrix<N>::row_reference_type r_v = *iter_v;
-  typename OR_Matrix<N>::row_reference_type r_cv = *(++iter_v);
+  assert(v % 2 == 0 && v < 2*space_dim);
+  typename OR_Matrix<N>::row_iterator m_iter = matrix.row_begin() + v;
+  typename OR_Matrix<N>::row_reference_type r_v = *m_iter;
+  typename OR_Matrix<N>::row_reference_type r_cv = *(++m_iter);
   for (dimension_type k = v; k-- > 0; ) {
     r_v[k] = PLUS_INFINITY;
     r_cv[k] = PLUS_INFINITY;
+  }
+  ++m_iter;
+  for (typename OR_Matrix<N>::row_iterator m_end = matrix.row_end();
+       m_iter != m_end; ++m_iter) {
+    typename OR_Matrix<N>::row_reference_type r = *m_iter;
+    r[v] = PLUS_INFINITY;
+    r[v+1] = PLUS_INFINITY;
   }
 }
 
 template <typename T>
 inline void
 Octagonal_Shape<T>::add_constraints(const Constraint_System& cs) {
-  // This method not preserve closure.
-  // Seen add_constraint().
-  Constraint_System::const_iterator iend = cs.end();
-  for (Constraint_System::const_iterator i = cs.begin(); i != iend; ++i)
+  Constraint_System::const_iterator i_end = cs.end();
+  for (Constraint_System::const_iterator i = cs.begin(); i != i_end; ++i)
     add_constraint(*i);
   assert(OK());
 }
@@ -309,7 +265,7 @@ inline bool
 Octagonal_Shape<T>::add_constraints_and_minimize(const Constraint_System& cs) {
   add_constraints(cs);
   strong_closure_assign();
-  return !(marked_empty());
+  return !marked_empty();
 }
 
 template <typename T>
@@ -371,11 +327,11 @@ Octagonal_Shape<T>::time_elapse_assign(const Octagonal_Shape& y) {
   // Dimension-compatibility check.
   if (space_dimension() != y.space_dimension())
     throw_dimension_incompatible("time_elapse_assign(y)", y);
-  // Seen the polyhedra documentation.
+  // See the polyhedra documentation.
   C_Polyhedron px(constraints());
   C_Polyhedron py(y.constraints());
   px.time_elapse_assign(py);
-  Octagonal_Shape x(px.constraints());
+  Octagonal_Shape<T> x(px);
   swap(x);
   assert(OK());
 }
@@ -385,21 +341,16 @@ inline bool
 Octagonal_Shape<T>::add_constraint_and_minimize(const Constraint& c) {
   bool was_closed = marked_strongly_closed();
   add_constraint(c);
-  // `*this' was closed and we add an only constraint to `*this',
-  // thus it is many convenient to use the incremental-closure,
-  // instead of the closure, because it cost only O(n2) against
-  // O(n3).
+  // If the OS was strongly closed and we add a single constraint,
+  // it is convenient to use the incremental strong-closure algorithm,
+  // as we known that the constraint has affected two variables at most.
+  // (The cost is O(n^2) instead of O(n^3).)
   if (was_closed)
-    for (dimension_type i = c.space_dimension(); i-- > 0;) {
-      Variable var = Variable(i);
-      if (c.coefficient(var) != 0) {
-	// If the constraint `c' isn't a bounded difference
-	// or the constraint `c' doesn't change `*this',
-	// the incremental-closure does nothing.
-	incremental_strong_closure_assign(var);
+    for (dimension_type i = c.space_dimension(); i-- > 0;)
+      if (c.coefficient(Variable(i)) != 0) {
+	incremental_strong_closure_assign(Variable(i));
 	break;
       }
-    }
   else
     strong_closure_assign();
   return !(marked_empty());
