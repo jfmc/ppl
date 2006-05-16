470dafb6930d89ac550bf7a4cedf36a06d49339e
Barbara Quartieri <quartier@no.email>
2006-05-16 10:20:14 +0000

    Indentation fixed.

--- src/Og_Status.idefs.hh
+++ src/Og_Status.idefs.hh
@@ -84,7 +84,7 @@ private:
   //@{
   static const flags_t ZERO_DIM_UNIV       = 0U;
   static const flags_t EMPTY               = 1U << 0;
-  static const flags_t STRONGLY_CLOSED = 1U << 1;
+  static const flags_t STRONGLY_CLOSED     = 1U << 1;
   //@}
 
   //! This holds the current bitset.
