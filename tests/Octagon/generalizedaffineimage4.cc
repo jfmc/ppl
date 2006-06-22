fa2742e999dee71d9d5b79395aac58d06b7832ab
Barbara Quartieri <quartier@no.email>
2006-06-22 09:42:50 +0000

    Erased a useless empty line.

--- tests/Octagon/generalizedaffineimage4.cc
+++ tests/Octagon/generalizedaffineimage4.cc
@@ -72,7 +72,7 @@ test02() {
   oct.generalized_affine_image(e1, EQUAL, e2);
 
   bool ok = check_result(oct, known_result, "1.98e-6", "9.26e-7", "5.25e-7");
-  
+
   print_constraints(oct, "*** oct.generalized_affine_image(6, "
 		    "EQUAL, 3*x - 4) ***");
 
