79bd69468ee6c9c27d968ae8fb1b701e042f9aae
Elena Mazzi <mazzi@no.email>
2006-09-22 13:34:28 +0000

    Added new test on the method BD_Shape::bds_hull_assign_and_minimize().

--- tests/BD_Shape/bdshull1.cc
+++ tests/BD_Shape/bdshull1.cc
@@ -208,6 +208,32 @@ test06() {
   return false;
 }
 
+bool
+test07() {
+  Variable A(0);
+
+  TBD_Shape bd1(1);
+  bd1.add_constraint(A <= 0);
+  bd1.add_constraint(A >= 1);
+
+  TBD_Shape bd2(1);
+  bd2.add_constraint(A <= 3);
+
+  print_constraints(bd1, "*** bd1 ***");
+  print_constraints(bd2, "*** bd2 ***");
+
+  bd1.bds_hull_assign_and_minimize(bd2);
+
+  BD_Shape<mpq_class> known_result(1);
+  known_result.add_constraint(A <= 3);
+
+  bool ok = (BD_Shape<mpq_class>(bd1) == known_result) ;
+
+  print_constraints(bd1, "*** bd1.bds_hull_assign_and_minimize(bd2) ***");
+
+  return ok;
+}
+
 } // namespace
 
 BEGIN_MAIN
@@ -217,4 +243,5 @@ BEGIN_MAIN
   DO_TEST(test04);
   DO_TEST(test05);
   DO_TEST(test06);
+  DO_TEST(test07);
 END_MAIN
