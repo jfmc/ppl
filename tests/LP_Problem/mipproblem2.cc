5e8d97d6e53dc023c8299d04e7babd98b468b782
Roberto Bagnara <bagnara@cs.unipr.it>
2006-10-06 18:34:58 +0000

    New test added.

--- tests/LP_Problem/mipproblem2.cc
+++ tests/LP_Problem/mipproblem2.cc
@@ -1,4 +1,4 @@
-/* Test the LP_Problem class.
+/* Test the MIP_Problem class.
    Copyright (C) 2001-2006 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
@@ -22,7 +22,7 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
 
-using namespace Parma_Polyhedra_Library;
+namespace {
 
 bool
 test01() {
@@ -51,6 +51,7 @@ test01() {
   nout << "Optimizing point = ";
   using namespace Parma_Polyhedra_Library::IO_Operators;
   nout << pg << endl;
+
   return true;
 }
 
@@ -88,10 +89,54 @@ test02() {
   nout << "Optimizing point = ";
   using namespace Parma_Polyhedra_Library::IO_Operators;
   nout << pg << endl;
+
   return true;
 }
+
+bool
+test03() {
+  Variable A(0);
+  Variable B(1);
+  Constraint_System cs;
+  cs.insert(Coefficient("-3152519739159347")*A
+	    - Coefficient("4503599627370496")*B
+	    >= Coefficient("-2837267765243412480"));
+  cs.insert(Coefficient("-14411518807585588")*A
+	    - Coefficient("3602879701896397")*B
+	    >= Coefficient("-19455550390240542720"));
+  cs.insert(Coefficient("6325070415853456823515479584966165845298645305129441198653167438357198111499854590373761990669910140474596183259900372230931523043306046152094168748148078435047419508642698792639590866940413010663742739952273283392562733857021646831815729864036236135650314266011211548510419206725953204130822734645187695728365866909171712")*A
+	    >= Coefficient("134217729"));
+  cs.insert(B >= 20);
+  cs.insert(-B >= -500);
+
+  // Integer variables.
+  Variables_Set ivs(A);
+
+  // Cost function
+  Linear_Expression cost(-4*A - B);
+
+  MIP_Problem lp = MIP_Problem(cs.space_dimension(),
+			       cs.begin(), cs.end(),
+			       ivs,
+			       cost,
+			       MINIMIZATION);
+
+  Generator pg = lp.optimizing_point();
+  nout << "Optimizing point = ";
+  print_generator(pg);
+
+  Coefficient num;
+  Coefficient den;
+  lp.evaluate_objective_function(pg, num, den);
+  nout << "Optimum value = " << num << "/" << den << endl;
+
+  return true;
+}
+
+} // namespace
 
 BEGIN_MAIN
   DO_TEST(test01);
   DO_TEST(test02);
+  DO_TEST_F64(test03);
 END_MAIN
