1e1194962b1089326f71c874f92a0d4030a8abb2
Roberto Bagnara <bagnara@cs.unipr.it>
2006-10-21 11:27:42 +0000

    Fixed spelling error introduced with last commit.

--- src/Grid_public.cc
+++ src/Grid_public.cc
@@ -3,7 +3,7 @@
 
 This file is part of the Parma Polyhedra Library (PPL).
 
-The PPL is fre software; you can redistribute it and/or modify it
+The PPL is free software; you can redistribute it and/or modify it
 under the terms of the GNU General Public License as published by the
 Free Software Foundation; either version 2 of the License, or (at your
 option) any later version.
