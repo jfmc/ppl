a581e94340d1a82499a66fbf04086e703b3363ab
Enea Zaffanella <zaffanella@cs.unipr.it>
2006-11-08 14:55:23 +0000

    Tests 3 and 4 fail with 8-bit integers.

--- tests/Octagonal_Shape/max_min1.cc
+++ tests/Octagonal_Shape/max_min1.cc
@@ -602,8 +602,8 @@ test16() {
 BEGIN_MAIN
   DO_TEST(test01);
   DO_TEST(test02);
-  DO_TEST(test03);
-  DO_TEST(test04);
+  DO_TEST_F8(test03);
+  DO_TEST_F8(test04);
   DO_TEST(test05);
   DO_TEST(test06);
   DO_TEST(test07);
