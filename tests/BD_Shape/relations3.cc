3e382eba8d439037d29a0c4866805e1b2322a385
Patricia M. Hill <hill@comp.leeds.ac.uk>
2006-11-22 09:06:00 +0000

    Added 2 tests to each of these test files.
    The first of these (13 in BD_Shape/relations3.cc and 20 in
    Octagonal_Shape/relatwithgen1.cc) throw an exception but the
    call to these is commented for now.
    
    The second of these (14 in BD_Shape/relations3.cc and 21 in
    Octagonal_Shape/relatwithgen1.cc) fail indicating a possible bug.

--- tests/BD_Shape/relations3.cc
+++ tests/BD_Shape/relations3.cc
@@ -268,6 +268,49 @@ test12() {
   return rel == known_result;
 }
 
+bool
+test13() {
+  // A 1D empty BDS that is not in minimal form and the point is the origin.
+  Variable A(0);
+
+  TBD_Shape bd(1);
+  bd.add_constraint(A <= 0);
+  bd.add_constraint(A >= 1);
+
+  Generator g = point();
+  Poly_Gen_Relation rel = bd.relation_with(g);
+
+  print_constraints(bd, "--- bd ---");
+  print_generator(g, "--- g ---");
+  using namespace IO_Operators;
+  nout << "bd.relation_with(v()) == " << rel << endl;
+
+  Poly_Gen_Relation known_result = Poly_Gen_Relation::nothing();
+
+  return rel == known_result;
+}
+
+bool
+test14() {
+  // A single point does not subsume another (different) point.
+  Variable A(0);
+
+  TBD_Shape bd(1);
+  bd.add_constraint(A == 1);  bd.add_constraint(A >= 1);
+
+  Generator g = point();
+  Poly_Gen_Relation rel = bd.relation_with(g);
+
+  print_constraints(bd, "--- bd ---");
+  print_generator(g, "--- g ---");
+  using namespace IO_Operators;
+  nout << "bd.relation_with(v()) == " << rel << endl;
+
+  Poly_Gen_Relation known_result = Poly_Gen_Relation::nothing();
+
+  return rel == known_result;
+}
+
 } // namespace
 
 BEGIN_MAIN
@@ -283,4 +326,6 @@ BEGIN_MAIN
   DO_TEST(test10);
   DO_TEST(test11);
   DO_TEST(test12);
+//  DO_TEST(test13);
+  DO_TEST_F(test14);
 END_MAIN
