3d91e536b3282090196f9be72f865080fecb679d
Andrea Cimino <cimino@no.email>
2006-11-24 10:00:33 +0000

    Added a new test that relates to Partial_Function implementation in the Java interface. The written test is based on test03() of mapspacedims1.cc in the test/Polyhedron directory.

--- interfaces/Java/tests/C_Polyhedron_test1.java
+++ interfaces/Java/tests/C_Polyhedron_test1.java
@@ -31,129 +31,53 @@ public class C_Polyhedron_test1 {
 	System.loadLibrary("ppl_java");
     }
 
-    public static void main(String[] args) {
-	// Variable declarations.
+    // This code tests the method `map_space_dimension(pfunc)'.
+    public static boolean test01() {
+	Test_Partial_Function partial_function = new Test_Partial_Function();
+	partial_function.insert(0, 2);
+	partial_function.insert(2, 0);
+	partial_function.insert(1, 1);
+	long ss =  partial_function.max_in_codomain();
 	Variable A = new Variable(0);
 	Variable B = new Variable(1);
 	Variable C = new Variable(2);
+	Linear_Expression_Variable le_b = new Linear_Expression_Variable(B);
+	Linear_Expression_Variable le_c = new Linear_Expression_Variable(C);
+	Linear_Expression_Variable le_a = new Linear_Expression_Variable(A);
+	Linear_Expression_Sum le_a_plus_b = new Linear_Expression_Sum(le_a,
+								      le_b);
+	Linear_Expression_Sum le_a_plus_c = new Linear_Expression_Sum(le_a,
+								      le_c);
+	Linear_Expression_Sum le_c_plus_b = new Linear_Expression_Sum(le_c,
+								      le_b);
+	Linear_Expression_Sum le_c_plus_a = new Linear_Expression_Sum(le_c,
+								      le_a);
+	Linear_Expression_Coefficient le_two
+	    = new Linear_Expression_Coefficient(new Coefficient(2));
+	Linear_Expression_Times le_2c
+	    = new Linear_Expression_Times(le_c,
+					  new Coefficient(2));
+	Linear_Expression_Times le_2a
+	    = new Linear_Expression_Times(le_a,
+					  new Coefficient(2));
 
-	// Coefficient declaration.
-	Coefficient coeff1 = new Coefficient("5");
-
-	// Linear_Expression declarations.
-	Linear_Expression le1 = new Linear_Expression_Variable(A);
-	Linear_Expression le2 = new Linear_Expression_Variable(C);
-	Linear_Expression le3 = new Linear_Expression_Coefficient(coeff1);
-	Linear_Expression le4 = le1.times(new Coefficient(3));
-	le2 = le2.sum(le4);
-
-	// Constraint declarations.
-	Constraint c1 = new Constraint(le2, Relation_Symbol.EQUAL, le3);
-	Constraint c2 = new Constraint(le2, Relation_Symbol.EQUAL, le3);
-	Constraint c3 = new Constraint(le2,
-				       Relation_Symbol.GREATER_THAN_OR_EQUAL,
-				       le4);
-	Constraint c4 = new Constraint(le2,
-				       Relation_Symbol.GREATER_THAN_OR_EQUAL,
-				       le3);
-
-	// Constraint_System declarations.
-	Constraint_System cs1 = new Constraint_System();
-	cs1.add(c1);
-	cs1.add(c2);
-	Constraint_System cs2 = new Constraint_System();
-	cs2.add(c3);
-	cs2.add(c4);
-
-	le2 = le2.times(new Coefficient("10"));
-
-	// C_Polyhedron declarations
-	C_Polyhedron c_poly1 = new C_Polyhedron(cs1);
-	C_Polyhedron c_poly2 = new C_Polyhedron(cs2);
-
-	// Generator declarations.
-	Generator g1 = Generator.point(le2, new Coefficient(1));
-	Generator g2 = Generator.point(le2, new Coefficient(2));
-        Generator g3 = Generator.point(le2, new Coefficient(3));
-	Generator g4 = Generator.point(le1, new Coefficient(4));
-
-	// Generator_System declaration.
 	Generator_System gs = new Generator_System();
-	gs.add(g1);
-	gs.add(g2);
-	gs.add(g3);
-	gs.add(g4);
-       	C_Polyhedron poly3 = new C_Polyhedron(gs);
+	gs.add(Generator.point(le_2c, new Coefficient(1)));
+	gs.add(Generator.line(le_a_plus_b));
+	gs.add(Generator.ray(le_a_plus_c));
 
-	// Here some tests.
-	if (c_poly1.is_empty())
-	    System.out.println("c_poly1 is empty..");
-	else
-	    System.out.println("c_poly1 is not empty.");
-   	if (c_poly1.is_universe())
-	    System.out.println("c_poly1 is universe.");
-	else
-	    System.out.println("c_poly1 is not universe.");
-	if (c_poly1.is_topologically_closed())
-	    System.out.println("c_poly1 is topologically closed.");
-	else
-	    System.out.println("c_poly1 is not topologically closed.");
-	if (c_poly1.is_bounded())
-	    System.out.println("c_poly1 is bounded.");
-	else
-	    System.out.println("c_poly1 is not bounded.");
-	if (c_poly1.is_disjoint_from(c_poly2))
-	    System.out.println("c_poly1 is disjoint.");
-	else
-	    System.out.println("c_poly1 is not disjoint.");
-	if (c_poly1.bounds_from_above(le2))
-	    System.out.println("c_poly1 bounds from above le2.");
-	else
-	    System.out.println("c_poly1 not bounds from above le2.");
-        if (c_poly1.bounds_from_below(le2))
-	    System.out.println("c_poly1 bounds from below le2.");
-	else
-	    System.out.println("c_poly1 not bounds from below.");
-	if (c_poly1.contains(c_poly2))
-	    System.out.println("c_poly1 contains c_poly2.");
-	else
-	    System.out.println("c_poly1 not contains c_poly2.");
-	if (c_poly1.strictly_contains(c_poly2))
-	    System.out.println("c_poly1 strictly contains c_poly2.");
-	else
-	    System.out.println("c_poly1 not strictly contains c_poly2.");
-        if (c_poly1.contains_integer_point())
-	    System.out.println("c_poly1 contains an integer point.");
-	else
-	    System.out.println("c_poly1 does not cointain an integer point.");
-	if (c_poly1.is_discrete())
-	    System.out.println("c_poly1 is discrete.");
-	else
-	    System.out.println("c_poly1 is not discrete.");
-	Linear_Expression le5 = new Linear_Expression_Variable(A);
-	le5.sum(new Linear_Expression_Variable(B));
-	le5.sum(new Linear_Expression_Coefficient(new Coefficient(14)));
-	Constraint c5 = new Constraint(le1,
-				       Relation_Symbol.GREATER_THAN_OR_EQUAL,
-				       le5);
-	c_poly1.add_constraint(c5);
- 	c_poly1.affine_image(A,le1, new Coefficient(5));
- 	c_poly1.affine_preimage(A,le1, new Coefficient(5));
-	// These lines cause overflow if PPL is compiled with 8
-	// bit coefficients and assertions enabled.
-	// c_poly1.generalized_affine_image(le1, Relation_Symbol.EQUAL, le2);
-	// c_poly1.generalized_affine_image(A, Relation_Symbol.EQUAL, le2,
-	//			 new Coefficient(6));
-	c_poly1.bounded_affine_image(B, le2, le3, new Coefficient(10));
-	c_poly1.bounded_affine_preimage(B, le2, le4, new Coefficient(30));
-	c_poly1.time_elapse_assign(c_poly2);
-	c_poly1.difference_assign(c_poly2);
-	c_poly1.topological_closure_assign();
-	c_poly1.poly_hull_assign(c_poly2);
- 	if (c_poly1.poly_hull_assign_and_minimize(c_poly2))
-	    System.out.println("c_poly1 is minimized.");
- 	else
- 	    System.out.println("c_poly1 is not minimized.");
-	c_poly1.intersection_assign(c_poly2);
+	C_Polyhedron poly1 = new C_Polyhedron(gs);
+	poly1.map_space_dimensions(partial_function);
+
+	Generator_System known_gs = new Generator_System();
+	known_gs.add(Generator.point(le_2a, new Coefficient(1)));
+	known_gs.add(Generator.line(le_c_plus_b));
+	known_gs.add(Generator.ray(le_c_plus_a));
+
+	C_Polyhedron known_result = new C_Polyhedron(known_gs);
+	return known_result.equals(poly1);
+    }
+    public static void main(String[] args) {
+	test01();
     }
 }
