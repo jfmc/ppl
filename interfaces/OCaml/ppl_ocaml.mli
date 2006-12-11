4dc188e909ffb3b209b3ac41fa0bf660c385fa21
Andrea Cimino <cimino@no.email>
2006-12-11 15:30:18 +0000

    Separated declaration and implementation of the OCaml interface. Added some kinds of exceptions that relates to PPL. Added a piece of code in `configure.ac' that tries to detect the presence of the GMP-OCaml module.

--- interfaces/OCaml/ppl_ocaml.mli
+++ interfaces/OCaml/ppl_ocaml.mli
@@ -2,6 +2,8 @@
 
 open Gmp
 
+exception Error of string
+
 type linear_expression =
     Variable of int
   | Coefficient of Z.t
@@ -48,256 +50,224 @@ type relation_symbol = Less_Than_RS | Less_Than_Or_Equal_RS | Equal_RS
 
 type polyhedron
 
-external ppl_new_C_Polyhedron_from_space_dimension:
-  int -> polyhedron = "ppl_new_C_Polyhedron_from_space_dimension"
+val ppl_new_C_Polyhedron_from_space_dimension:
+  int -> polyhedron
 
-external ppl_new_C_Polyhedron_from_constraint_system:
-  constraint_system -> polyhedron = "ppl_new_C_Polyhedron_from_constraint_system"
-external ppl_new_C_Polyhedron_from_generator_system:
-  generator_system -> polyhedron = "ppl_new_C_Polyhedron_from_generator_system"
+val ppl_new_C_Polyhedron_from_constraint_system:
+  constraint_system -> polyhedron
 
-external ppl_new_C_Polyhedron_from_congruence_system:
-  congruence_system -> polyhedron = "ppl_new_C_Polyhedron_from_congruence_system"
-external ppl_Polyhedron_poly_con_relation:
+val ppl_new_C_Polyhedron_from_generator_system:
+  generator_system -> polyhedron
+
+val ppl_new_C_Polyhedron_from_congruence_system:
+  congruence_system -> polyhedron
+
+val ppl_Polyhedron_poly_con_relation:
       polyhedron -> linear_constraint -> relation_with_constraint list
-	  = "ppl_Polyhedron_poly_con_relation"
 
-external ppl_Polyhedron_poly_gen_relation:
+val ppl_Polyhedron_poly_gen_relation:
       polyhedron -> linear_generator -> relation_with_generator list
-	  = "ppl_Polyhedron_poly_gen_relation"
 
-external ppl_Polyhedron_space_dimension:
-  polyhedron -> int = "ppl_Polyhedron_space_dimension"
+val ppl_Polyhedron_space_dimension:
+  polyhedron -> int
 
-external ppl_Polyhedron_affine_dimension:
-  polyhedron -> int = "ppl_Polyhedron_affine_dimension"
+val ppl_Polyhedron_affine_dimension:
+  polyhedron -> int
 
-external ppl_Polyhedron_is_empty:
-  polyhedron -> bool = "ppl_Polyhedron_is_empty"
+val ppl_Polyhedron_is_empty:
+  polyhedron -> bool
 
-external ppl_Polyhedron_is_universe:
-  polyhedron -> bool = "ppl_Polyhedron_is_universe"
+val ppl_Polyhedron_is_universe:
+  polyhedron -> bool
 
-external ppl_Polyhedron_contains_interger_point:
-  polyhedron -> bool = "ppl_Polyhedron_contains_integer_point"
+val ppl_Polyhedron_contains_interger_point:
+  polyhedron -> bool
 
-external ppl_Polyhedron_is_topologically_closed:
-  polyhedron -> bool = "ppl_Polyhedron_is_topologically_closed"
+val ppl_Polyhedron_is_topologically_closed:
+  polyhedron -> bool
 
-external ppl_Polyhedron_is_bounded:
-  polyhedron -> bool = "ppl_Polyhedron_is_bounded"
+val ppl_Polyhedron_is_bounded:
+  polyhedron -> bool
 
-external ppl_Polyhedron_bounds_from_above:
-  polyhedron -> linear_expression -> bool = "ppl_Polyhedron_bounds_from_above"
+val ppl_Polyhedron_bounds_from_above:
+  polyhedron -> linear_expression -> bool
 
-external ppl_Polyhedron_bounds_from_below:
-  polyhedron -> linear_expression -> bool = "ppl_Polyhedron_bounds_from_below"
+val ppl_Polyhedron_bounds_from_below:
+  polyhedron -> linear_expression -> bool
 
-external ppl_Polyhedron_add_constraint:
-  polyhedron -> linear_constraint -> unit = "ppl_Polyhedron_add_constraint"
-
-external ppl_Polyhedron_add_constraint_and_minimize:
+val ppl_Polyhedron_add_constraint:
   polyhedron -> linear_constraint -> unit
-      = "ppl_Polyhedron_add_constraint_and_minimize"
 
-external ppl_Polyhedron_add_constraints:
-  polyhedron -> constraint_system -> unit = "ppl_Polyhedron_add_constraints"
+val ppl_Polyhedron_add_constraint_and_minimize:
+  polyhedron -> linear_constraint -> unit
 
-external ppl_Polyhedron_add_constraints_and_minimize:
+val ppl_Polyhedron_add_constraints:
   polyhedron -> constraint_system -> unit
-      = "ppl_Polyhedron_add_constraints_and_minimize"
 
-external ppl_Polyhedron_add_generator:
-  polyhedron -> linear_generator -> unit = "ppl_Polyhedron_add_generator"
+val ppl_Polyhedron_add_constraints_and_minimize:
+  polyhedron -> constraint_system -> unit
 
-external ppl_Polyhedron_add_generator_and_minimize:
+val ppl_Polyhedron_add_generator:
   polyhedron -> linear_generator -> unit
-      = "ppl_Polyhedron_add_generator_and_minimize"
 
-external ppl_Polyhedron_add_generators:
-  polyhedron -> generator_system -> unit = "ppl_Polyhedron_add_generators"
+val ppl_Polyhedron_add_generator_and_minimize:
+  polyhedron -> linear_generator -> unit
 
-external ppl_Polyhedron_add_generators_and_minimize:
+val ppl_Polyhedron_add_generators:
   polyhedron -> generator_system -> unit
-      = "ppl_Polyhedron_add_generators_and_minimize"
 
-external ppl_Polyhedron_add_congruences:
+val ppl_Polyhedron_add_generators_and_minimize:
+  polyhedron -> generator_system -> unit
+
+val ppl_Polyhedron_add_congruences:
   polyhedron -> congruence_system -> unit
-      = "ppl_Polyhedron_add_congruences"
 
-external ppl_Polyhedron_is_disjoint_from:
+val ppl_Polyhedron_is_disjoint_from:
   polyhedron -> polyhedron -> bool
-      = "ppl_Polyhedron_is_disjoint_from"
 
-external ppl_Polyhedron_contains:
+val ppl_Polyhedron_contains:
   polyhedron -> polyhedron -> bool
-      = "ppl_Polyhedron_contains"
 
-external ppl_Polyhedron_intersection_assign_and_minimize:
+val ppl_Polyhedron_intersection_assign_and_minimize:
    polyhedron -> polyhedron -> bool
-       = "ppl_Polyhedron_intersection_assign_and_minimize"
 
-external ppl_Polyhedron_intersection_assign:
+val ppl_Polyhedron_intersection_assign:
    polyhedron -> polyhedron -> unit
-       = "ppl_Polyhedron_intersection_assign"
 
-external ppl_Polyhedron_poly_hull_assign_and_minimize:
+val ppl_Polyhedron_poly_hull_assign_and_minimize:
     polyhedron -> polyhedron -> bool
-	= "ppl_Polyhedron_poly_hull_assign_and_minimize"
 
-external ppl_Polyhedron_poly_hull_assign:
+val ppl_Polyhedron_poly_hull_assign:
     polyhedron -> polyhedron -> unit
-	= "ppl_Polyhedron_poly_hull_assign"
 
-external ppl_Polyhedron_upper_bound_assign:
+val ppl_Polyhedron_upper_bound_assign:
     polyhedron -> polyhedron -> unit
-	= "ppl_Polyhedron_upper_bound_assign"
 
-external ppl_Polyhedron_poly_difference_assign:
+val ppl_Polyhedron_poly_difference_assign:
     polyhedron -> polyhedron -> unit
-	= "ppl_Polyhedron_poly_difference_assign"
 
-external ppl_Polyhedron_difference_assign:
+val ppl_Polyhedron_difference_assign:
     polyhedron -> polyhedron -> unit
-	= "ppl_Polyhedron_difference_assign"
 
-external ppl_Polyhedron_time_elapse_assign:
+val ppl_Polyhedron_time_elapse_assign:
     polyhedron -> polyhedron -> unit
-	= "ppl_Polyhedron_time_elapse_assign"
 
-external ppl_Polyhedron_concatenate_assign:
+val ppl_Polyhedron_concatenate_assign:
     polyhedron -> polyhedron -> unit
-	= "ppl_Polyhedron_concatenate_assign"
 
-external ppl_Polyhedron_add_space_dimensions_and_embed:
-  polyhedron -> int -> unit = "ppl_Polyhedron_add_space_dimensions_and_embed"
+val ppl_Polyhedron_add_space_dimensions_and_embed:
+  polyhedron -> int -> unit
 
-external ppl_Polyhedron_add_space_dimensions_and_project:
-  polyhedron -> int -> unit = "ppl_Polyhedron_add_space_dimensions_and_project"
+val ppl_Polyhedron_add_space_dimensions_and_project:
+  polyhedron -> int -> unit
 
-external ppl_Polyhedron_remove_higher_space_dimensions:
-  polyhedron -> int -> unit = "ppl_Polyhedron_remove_higher_space_dimensions"
+val ppl_Polyhedron_remove_higher_space_dimensions:
+  polyhedron -> int -> unit
 
-external ppl_Polyhedron_remove_higher_space_dimensions:
-  polyhedron -> int -> unit = "ppl_Polyhedron_remove_higher_space_dimensions"
+val ppl_Polyhedron_remove_higher_space_dimensions:
+  polyhedron -> int -> unit
 
-external ppl_Polyhedron_space_dimension:
-  polyhedron -> int = "ppl_Polyhedron_space_dimension"
+val ppl_Polyhedron_space_dimension:
+  polyhedron -> int
 
-external ppl_Polyhedron_constraints:
-   polyhedron -> constraint_system = "ppl_Polyhedron_constraints"
+val ppl_Polyhedron_constraints:
+   polyhedron -> constraint_system
 
-external ppl_Polyhedron_minimized_constraints:
-   polyhedron -> constraint_system = "ppl_Polyhedron_minimized_constraints"
+val ppl_Polyhedron_minimized_constraints:
+   polyhedron -> constraint_system
 
-external ppl_Polyhedron_generators:
-   polyhedron -> generator_system = "ppl_Polyhedron_generators"
+val ppl_Polyhedron_generators:
+   polyhedron -> generator_system
 
-external ppl_Polyhedron_minimized_generators:
-   polyhedron -> generator_system = "ppl_Polyhedron_minimized_generators"
+val ppl_Polyhedron_minimized_generators:
+   polyhedron -> generator_system
 
-external ppl_Polyhedron_congruences:
-   polyhedron -> congruence_system = "ppl_Polyhedron_congruences"
+val ppl_Polyhedron_congruences:
+   polyhedron -> congruence_system
 
-external ppl_Polyhedron_minimized_congruences:
-   polyhedron -> congruence_system = "ppl_Polyhedron_minimized_congruences"
+val ppl_Polyhedron_minimized_congruences:
+   polyhedron -> congruence_system
 
-external ppl_Polyhedron_affine_image:
+val ppl_Polyhedron_affine_image:
   polyhedron -> int -> linear_expression -> Z.t -> unit
-      = "ppl_Polyhedron_affine_image"
 
-external ppl_Polyhedron_affine_preimage:
+val ppl_Polyhedron_affine_preimage:
   polyhedron -> int -> linear_expression -> Z.t -> unit
-      = "ppl_Polyhedron_affine_preimage"
 
-external ppl_Polyhedron_generalized_affine_image:
+val ppl_Polyhedron_generalized_affine_image:
   polyhedron -> linear_expression -> relation_symbol -> linear_expression
     -> unit
-      = "ppl_Polyhedron_generalized_affine_image1"
 
-external ppl_Polyhedron_generalized_affine_image:
+val ppl_Polyhedron_generalized_affine_image:
   polyhedron -> int -> relation_symbol -> linear_expression
       -> Z.t -> unit
-      = "ppl_Polyhedron_generalized_affine_image2"
 
-external ppl_Polyhedron_generalized_affine_preimage:
+val ppl_Polyhedron_generalized_affine_preimage1:
   polyhedron -> int -> relation_symbol -> linear_expression
       -> Z.t -> unit
-      = "ppl_Polyhedron_generalized_affine_preimage2"
 
-external ppl_Polyhedron_generalized_affine_preimage:
+val ppl_Polyhedron_generalized_affine_preimage2:
   polyhedron -> linear_expression -> relation_symbol -> linear_expression
     -> unit
-      = "ppl_Polyhedron_generalized_affine_preimage1"
 
-external ppl_Polyhedron_bounded_affine_image:
+val ppl_Polyhedron_bounded_affine_image:
   polyhedron -> int -> linear_expression -> linear_expression
-      -> Z.t -> unit = "ppl_Polyhedron_bounded_affine_image"
+      -> Z.t -> unit
 
-external ppl_Polyhedron_bounded_affine_preimage:
+val ppl_Polyhedron_bounded_affine_preimage:
   polyhedron -> int -> linear_expression -> linear_expression
-      -> Z.t -> unit = "ppl_Polyhedron_bounded_affine_preimage"
+      -> Z.t -> unit
 
-external ppl_Polyhedron_BHRZ03_widening_assign:
+val ppl_Polyhedron_BHRZ03_widening_assign:
  polyhedron -> polyhedron -> int -> int
-     = "ppl_Polyhedron_BHRZ03_widening_assign"
 
-external ppl_Polyhedron_limited_BHRZ03_extrapolation_assign:
+val ppl_Polyhedron_limited_BHRZ03_extrapolation_assign:
  polyhedron -> polyhedron -> constraint_system -> int -> int
-     = "ppl_Polyhedron_limited_BHRZ03_extrapolation_assign"
 
-external ppl_Polyhedron_bounded_BHRZ03_extrapolation_assign:
+val ppl_Polyhedron_bounded_BHRZ03_extrapolation_assign:
  polyhedron -> polyhedron -> constraint_system -> int -> int
-     = "ppl_Polyhedron_bounded_BHRZ03_extrapolation_assign"
 
-external ppl_Polyhedron_limited_H79_extrapolation_assign:
+val ppl_Polyhedron_limited_H79_extrapolation_assign:
  polyhedron -> polyhedron -> constraint_system -> int -> int
-     = "ppl_Polyhedron_limited_H79_extrapolation_assign"
 
-external ppl_Polyhedron_bounded_H79_extrapolation_assign:
+val ppl_Polyhedron_bounded_H79_extrapolation_assign:
  polyhedron -> polyhedron -> constraint_system -> int -> int
-     = "ppl_Polyhedron_bounded_H79_extrapolation_assign"
 
-external ppl_Polyhedron_H79_widening_assign:
+val ppl_Polyhedron_H79_widening_assign:
  polyhedron -> polyhedron -> int -> int
-     = "ppl_Polyhedron_H79_widening_assign"
 
-external ppl_Polyhedron_maximize:
+val ppl_Polyhedron_maximize:
  polyhedron -> linear_expression -> bool * Z.t * Z.t * bool * linear_generator
-     = "ppl_Polyhedron_maximize"
 
-external ppl_Polyhedron_minimize:
+val ppl_Polyhedron_minimize:
  polyhedron -> linear_expression -> bool * Z.t * Z.t * bool * linear_generator
-     = "ppl_Polyhedron_minimize"
 
-external ppl_Polyhedron_remove_space_dimensions:
+val ppl_Polyhedron_remove_space_dimensions:
      polyhedron -> int list -> unit
-	 = "ppl_Polyhedron_remove_space_dimensions"
 
-external ppl_Polyhedron_fold_space_dimensions:
+val ppl_Polyhedron_fold_space_dimensions:
      polyhedron -> int list -> int -> unit
-	 = "ppl_Polyhedron_fold_space_dimensions"
 
-external ppl_Polyhedron_OK:
- polyhedron -> bool = "ppl_Polyhedron_OK"
+val ppl_Polyhedron_OK:
+ polyhedron -> bool
 
-external ppl_Polyhedron_map_space_dimensions:
- polyhedron -> (int*int) list -> unit = "ppl_Polyhedron_map_space_dimensions"
+val ppl_Polyhedron_map_space_dimensions:
+ polyhedron -> (int*int) list -> unit
 
-external ppl_Polyhedron_swap:
-polyhedron -> polyhedron -> unit = "ppl_Polyhedron_swap"
+val ppl_Polyhedron_swap:
+polyhedron -> polyhedron -> unit
 
-external test_linear_expression:
-  linear_expression -> unit = "test_linear_expression"
+val test_linear_expression:
+  linear_expression -> unit
 
-external test_linear_constraint:
-  linear_constraint -> unit = "test_linear_constraint"
+val test_linear_constraint:
+  linear_constraint -> unit
 
-external test_linear_generator:
-  linear_generator -> unit = "test_linear_generator"
+val test_linear_generator:
+  linear_generator -> unit
 
-external test_constraint_system:
-  constraint_system -> unit = "test_constraint_system"
+val test_constraint_system:
+  constraint_system -> unit
 
-external test_generator_system:
-  generator_system -> unit = "test_generator_system"
+val test_generator_system:
+  generator_system -> unit
