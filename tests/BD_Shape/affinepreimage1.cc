f69f1bc8278c0fb7852d91349f5f8bfd141c8c71
Roberto Bagnara <bagnara@cs.unipr.it>
2007-02-28 09:22:20 +0000

    Test program affinepreimage2.cc merged into affinepreimage1.cc.

--- tests/BD_Shape/affinepreimage1.cc
+++ tests/BD_Shape/affinepreimage1.cc
@@ -169,6 +169,247 @@ test06() {
   return false;
 }
 
+bool
+test07() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(A, 4*B + 6*C + 2, -2);
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(B <= 0);
+  known_result.add_constraint(C >= 0);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(A, 4*B + 6*C + 2, -2) ***");
+
+  return ok;
+}
+
+bool
+test08() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(A, 2*A + 3*C + 2, 2);
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(A <= -2);
+  known_result.add_constraint(B <= 0);
+  known_result.add_constraint(C >= 0);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(A, 2*A + 3*C + 2, 2) ***");
+
+  return ok;
+}
+
+bool
+test09() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(A, 2*A + 3*C + 2, 2);
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(A <= -2);
+  known_result.add_constraint(B <= 0);
+  known_result.add_constraint(C >= 0);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(A, -3*A + C - 1, 2) ***");
+
+  return ok;
+}
+
+bool
+test10() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(A, 3*A + C - 1, -2);
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(B <= 0);
+  known_result.add_constraint(C >= 0);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(A, 3*A + C - 1, -2) ***");
+
+  return ok;
+}
+
+bool
+test11() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(A, -3*A + C - 1, -2);
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(B <= 0);
+  known_result.add_constraint(C >= 0);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(A, -3*A + C - 1, -2) ***");
+
+  return ok;
+}
+
+bool
+test12() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+  Variable D(3);
+
+  TBD_Shape bd(4);
+  bd.add_constraint(A == 2);
+  bd.add_constraint(B == 0);
+  bd.add_constraint(C >= -3);
+  bd.add_constraint(D <= 5);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(D, 4*A - B + 2*C + 5*D - 1, 3);
+
+  BD_Shape<mpq_class> known_result(4);
+  known_result.add_constraint(A == 2);
+  known_result.add_constraint(B == 0);
+  known_result.add_constraint(C >= -3);
+  known_result.add_constraint(5*D <= 14);
+
+  bool ok = check_result(bd, known_result, "7.63e-7", "3.82e-7", "1.91e-7");
+
+  print_constraints(bd, "*** bd.affine_preimage"
+		        "(D, 4*A - B + 2*C + 5*D - 1, 3) ***");
+
+  return ok;
+}
+
+bool
+test13() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  bd.affine_preimage(B, -B);
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(A <= -1);
+  known_result.add_constraint(B >= 0);
+  known_result.add_constraint(C >= 0);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(B, -B) ***");
+
+  return ok;
+}
+
+bool
+test14() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  BD_Shape<mpq_class> known_result(bd);
+
+  bd.affine_preimage(B, -B, -1);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(B, -B, -1) ***");
+
+  return ok;
+}
+
+bool
+test15() {
+  Variable A(0);
+  Variable B(1);
+  Variable C(2);
+
+  TBD_Shape bd(3);
+  bd.add_constraint(A <= -1);
+  bd.add_constraint(B <= 0);
+  bd.add_constraint(C >= 0);
+
+  print_constraints(bd, "*** bd ***");
+
+  BD_Shape<mpq_class> known_result(3);
+  known_result.add_constraint(A <= -1);
+  known_result.add_constraint(B >= 2);
+  known_result.add_constraint(C >= 0);
+
+  bd.affine_preimage(B, -3*B + 6, 3);
+
+  bool ok = (BD_Shape<mpq_class>(bd) == known_result);
+
+  print_constraints(bd, "*** bd.affine_preimage(B, -3*B + 6, 3) ***");
+
+  return ok;
+}
+
 } // namespace
 
 BEGIN_MAIN
@@ -178,4 +419,13 @@ BEGIN_MAIN
   DO_TEST(test04);
   DO_TEST(test05);
   DO_TEST(test06);
+  DO_TEST(test07);
+  DO_TEST(test08);
+  DO_TEST(test09);
+  DO_TEST(test10);
+  DO_TEST(test11);
+  DO_TEST(test12);
+  DO_TEST(test13);
+  DO_TEST(test14);
+  DO_TEST(test15);
 END_MAIN
