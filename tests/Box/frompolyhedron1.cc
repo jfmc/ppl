9b319a853baffdd0006ddc636df225f21c1b21aa
Roberto Bagnara <bagnara@cs.unipr.it>
2007-03-03 17:54:33 +0000

    Drafted a more complex test.

--- tests/Box/frompolyhedron1.cc
+++ tests/Box/frompolyhedron1.cc
@@ -361,8 +361,8 @@ test12() {
   cs.insert(3*A - 5*B <= 18);
   C_Polyhedron ph(cs);
 
-  TBox box1(ph, SIMPLEX_COMPLEXITY);
-  TBox box2(ph, ANY_COMPLEXITY);
+  TBox sbox(ph, SIMPLEX_COMPLEXITY);
+  TBox nbox(ph, ANY_COMPLEXITY);
 
   Rational_Box known_result(2);
   known_result.add_constraint(A >= 0);
@@ -370,12 +370,12 @@ test12() {
   known_result.add_constraint(2*B <= 5);
   known_result.add_constraint(5*B >= -18);
 
-  bool ok = (box1 == box2
-	     && check_result(box1, known_result,
+  bool ok = (sbox == nbox
+	     && check_result(sbox, known_result,
 			     "7.50e-7", "4.89e-7", "4.34e-7"));
 
-  print_constraints(box1, "*** box1 ***");
-  print_constraints(box2, "*** box2 ***");
+  print_constraints(sbox, "*** sbox ***");
+  print_constraints(nbox, "*** nbox ***");
 
   return ok;
 }
@@ -397,6 +397,115 @@ test13() {
   return ok;
 }
 
+bool
+test14() {
+  Variable X01(0);
+  Variable X02(1);
+  Variable X03(2);
+  Variable X04(3);
+  Variable X05(4);
+  Variable X06(5);
+  Variable X07(6);
+  Variable X08(7);
+  Variable X09(8);
+  Variable X10(9);
+  Variable X11(10);
+  Variable X12(11);
+  Variable X13(12);
+  Variable X14(13);
+  Variable X15(14);
+  Variable X16(15);
+  Variable X17(16);
+  Variable X18(17);
+  Variable X19(18);
+  Variable X20(19);
+  Variable X21(20);
+  Variable X22(21);
+  Variable X23(22);
+  Variable X24(23);
+  Variable X25(24);
+  Variable X26(25);
+  Variable X27(26);
+  Variable X28(27);
+  Variable X29(28);
+  Variable X30(29);
+  Variable X31(30);
+  Variable X32(31);
+  Variable X33(32);
+  Variable X34(33);
+  Variable X35(34);
+  Variable X36(35);
+  Variable X37(36);
+  Variable X38(37);
+  Variable X39(38);
+
+  Constraint_System cs;
+  cs.insert(X01 - X02 - X03 + 0*X39 == 0);
+  cs.insert(Coefficient("2386907802506363")*X01 - X04 == 0);
+  cs.insert(-X01 >= -80);
+  cs.insert(X02 - Coefficient("3152519739159347")*X14 >= 0);
+  cs.insert(X06 + X07 + X08 + X09 - X14 - X15 == 0);
+  cs.insert(Coefficient("2386907802506363")*X06
+	    + Coefficient("2386907802506363")*X07
+	    + Coefficient("1080863910568919")*X08
+	    + Coefficient("7746191359077253")*X09
+	    - X16 == 0);
+  cs.insert(-X06 + X10 >= -80);
+  cs.insert(-X07 + X11 >= 0);
+  cs.insert(-X08 + X12 >= 0);
+  cs.insert(-X09 + X13 >= 0);
+  cs.insert(X22 - X23 - X24 - X25 == 0);
+  cs.insert(Coefficient("7746191359077253")*X22 - X26 == 0);
+  cs.insert(-X22 >= -500);
+  cs.insert(X23 - Coefficient("3152519739159347")*X36 >= 0);
+  cs.insert(Coefficient("7746191359077253")*X28
+	    + Coefficient("7746191359077253")*X29
+	    + Coefficient("3512807709348987")*X30
+	    + Coefficient("3332663724254167")*X31
+	    - X38 == 0);
+  cs.insert(X28 + X29 + X30 + X31 - X36 + X37 + X39 == 44);
+  cs.insert(-X28 + X32 >= -500);
+  cs.insert(-X29 + X33 >= 0);
+  cs.insert(-X30 + X34 >= 0);
+  cs.insert(-X31 + X35 >= 0);
+  cs.insert(Coefficient("-2661627379775963")*X10
+	    - Coefficient("2686397177726501")*X11
+	    - Coefficient("5422333951354077")*X12
+	    - Coefficient("5469621747441467")*X13
+	    + X25
+	    - Coefficient("2466846695892189")*X32
+	    - Coefficient("4996743786567565")*X33
+	    - Coefficient("5064297780978123")*X34
+	    - Coefficient("641481471923585")*X35 >= 0);
+  cs.insert(X03 - Coefficient("7854277750134145")*X22 >= 0);
+  cs.insert(X15
+	    - Coefficient("7854277750134145")*X28
+	    - Coefficient("7782220156096217")*X29
+	    - Coefficient("7782220156096217")*X30
+	    - Coefficient("7710162562058289")*X31 >= 0);
+  cs.insert(Coefficient("-5422333951354077")*X01 + X24 >= 0);
+  cs.insert(X21 >= 2);
+  cs.insert(-X16 - X38 >= -300);
+  for (dimension_type i = X01.id(); i <= X39.id(); ++i)
+    cs.insert(Variable(i) >= 0);
+
+  C_Polyhedron ph(cs);
+
+  TBox pbox(ph, POLYNOMIAL_COMPLEXITY);
+  TBox sbox(ph, SIMPLEX_COMPLEXITY);
+  TBox nbox(ph, ANY_COMPLEXITY);
+
+  Rational_Box known_result(2);
+
+  bool ok = true;
+
+  print_constraints(pbox, "*** pbox ***");
+  print_constraints(sbox, "*** sbox ***");
+  print_constraints(nbox, "*** nbox ***");
+
+  return ok;
+}
+
 } // namespace
 
 BEGIN_MAIN
@@ -413,4 +522,5 @@ BEGIN_MAIN
   DO_TEST(test11);
   DO_TEST_F8(test12);
   DO_TEST(test13);
+  DO_TEST(test14);
 END_MAIN
