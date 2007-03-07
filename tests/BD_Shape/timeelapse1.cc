caf6065b780061755d9ff8b2f0f227f5680ee788
Enea Zaffanella <zaffanella@cs.unipr.it>
2007-03-07 16:31:54 +0000

    New test shows a bug in the (just added) direct implementation of BD_Shape<T>::time_elapse_assign().

--- tests/BD_Shape/timeelapse1.cc
+++ tests/BD_Shape/timeelapse1.cc
@@ -197,6 +197,41 @@ test06() {
   return false;
 }
 
+bool
+test07() {
+  Variable x(0);
+  Variable y(1);
+
+  TBD_Shape bds1(2);
+  bds1.add_constraint(x <= 3);
+  bds1.add_constraint(x >= 1);
+  bds1.add_constraint(y <= 5);
+  bds1.add_constraint(y >= 1);
+
+  print_constraints(bds1, "*** bds1 ***");
+
+  TBD_Shape bds2(2);
+  bds2.add_constraint(x <= 2);
+  bds2.add_constraint(x >= 0);
+  bds2.add_constraint(y <= 3);
+  bds2.add_constraint(y >= 2);
+
+  print_constraints(bds2, "*** bds2 ***");
+
+  bds1.time_elapse_assign(bds2);
+
+  BD_Shape<mpq_class> known_result(2);
+  known_result.add_constraint(x >= 1);
+  known_result.add_constraint(y >= 1);
+  known_result.add_constraint(x - y <= 2);
+
+  bool ok = (BD_Shape<mpq_class>(bds1) == known_result);
+
+  print_constraints(bds1, "*** bds1.time_elapse_assign(bds2) ***");
+
+  return ok;
+}
+
 } // namespace
 
 BEGIN_MAIN
@@ -206,4 +241,5 @@ BEGIN_MAIN
   DO_TEST(test04);
   DO_TEST(test05);
   DO_TEST(test06);
+  DO_TEST(test07);
 END_MAIN
