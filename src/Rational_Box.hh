c00a93de3f27d69337284a8f972347d62893cfb9
Roberto Bagnara <bagnara@cs.unipr.it>
2007-03-08 12:02:51 +0000

    Intervals used in the Box class can be empty.

--- src/Rational_Box.hh
+++ src/Rational_Box.hh
@@ -38,7 +38,7 @@ struct Rational_Box_Interval_Info_Policy {
   const_bool_nodef(cache_singleton, true);
   const_bool_nodef(cache_normalized, false);
   const_int_nodef(next_bit, 0);
-  const_bool_nodef(may_be_empty, false);
+  const_bool_nodef(may_be_empty, true);
   const_bool_nodef(may_contain_infinity, false);
   const_bool_nodef(check_empty_result, false);
   const_bool_nodef(check_inexact, false);
