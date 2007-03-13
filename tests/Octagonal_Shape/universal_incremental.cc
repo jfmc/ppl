8167941e8afe103c95e8eec3f7621bd8a75ebde1
Barbara Quartieri <quartier@no.email>
2007-03-13 13:55:56 +0000

    Added new tests on strong_closure() and incremental_strong_closure().

---
+++ tests/Octagonal_Shape/universal_incremental.cc
@@ -0,0 +1,121 @@
+#include "ppl_test.hh"
+
+#include "timings.hh"
+#include "files.hh"
+#include "stdio.h"
+#include "string.h"
+
+using namespace std;
+
+// dim = 400 per i double; dim = 200 per gli mpz_class;
+// dim = 100 per gli mpq_class.
+const int max_rand = 1000;
+dimension_type dim = 100;
+unsigned int default_inst = 0;
+//char* fname = "closure1.dat";
+char fname[80];
+
+void set_defaults(int type) {
+  switch (type) {
+    // double
+  case 0: { 
+    dim = 400;
+    strcat(fname,".double.");
+    strcat(fname,"universal");
+    break;
+  }
+    // mpq_class
+  case 1: { 
+    dim = 100;
+    strcat(fname,".mpq_class.");
+    strcat(fname,"universal");
+    break;
+  }
+  }
+}
+
+template <typename INST>
+void
+generate_dat_file() {
+//   std::cout << "Generating DAT file with percentage " << p << " ... ";
+  Octagonal_Shape<INST> oct(dim, UNIVERSE);
+
+  fstream f;
+  open(f, fname, ios_base::out);
+  oct.ascii_dump(f);
+  close(f);
+//   std::cout << " done.\n\n";
+ }
+
+
+template <typename INST>
+int
+universal_incremental_closure() {
+  Octagonal_Shape<INST> oct(dim, UNIVERSE);
+
+  fstream f;
+  open(f, fname, ios_base::in);
+  oct.ascii_load(f);
+  close(f);
+
+  start_clock();
+  oct.add_constraint_and_minimize(Variable(0) >= 5);
+  std::cout << "Universal_incremental: ";
+  print_clock(std::cout);
+  std::cout << std::endl;
+  if (oct.is_empty())
+    std::cerr << "Oct is empty." << std::endl;
+
+  return 0;
+}
+
+
+int
+main(int argc, char* argv[]) {
+  strcpy(fname,"closure1.dat");
+  if (argc < 2 || argc > 3) {
+    std::cerr << "Usage: " << argv[0] << " numtype [generate]" 
+	      << std::endl
+	      << "Example: " << argv[0] << " double generate" << std::endl
+	      << "Example: " << argv[0] << " mpq_class" << std::endl;
+    return 1;
+  }
+  if  (strcmp(argv[1],"mpq_class")==0)
+    default_inst = 1;
+  else if  (strcmp(argv[1],"double")==0)
+    default_inst = 0;
+  else {
+    std::cerr << "Invalid numerical type " << argv[1] << std::endl;
+    return 1;
+  }
+  set_defaults(default_inst);
+  if (argc>=3 &&   strcmp(argv[2],"generate")==0) {
+    switch(default_inst) {
+    case 0: {
+      generate_dat_file<double>();
+      break;
+    }
+    case 1: {
+      generate_dat_file<mpq_class>();
+      break;
+    }
+    
+    }
+  }
+
+  switch(default_inst) {
+  case 0: {
+    //    std::cerr << "double\n";
+    universal_incremental_closure<double>();
+    break;
+  }
+  case 1: {
+    //    std::cerr << "mpq_class\n";
+    universal_incremental_closure<mpq_class>();
+    break;
+  }
+    
+  }
+
+  return 0;
+}
