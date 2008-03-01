8f065040e6206e5b911cefc197c6befe21d647ad
Enea Zaffanella <zaffanella@cs.unipr.it>
2008-03-01 18:37:34 +0000

    Test 7 overflows when using 8 bit integers with assertions turned on; tests 11 and 15 overflow with 8 bit integers independently from assertions.

--- tests/Box/generalizedaffinepreimage1.cc
+++ tests/Box/generalizedaffinepreimage1.cc
@@ -462,13 +462,13 @@ DO_TEST(test03);
 DO_TEST(test04);
 DO_TEST(test05);
 DO_TEST(test06);
-DO_TEST(test07);
+DO_TEST_F8A(test07);
 DO_TEST(test08);
 DO_TEST(test09);
 DO_TEST(test10);
-DO_TEST(test11);
+DO_TEST_F8(test11);
 DO_TEST(test12);
 DO_TEST(test13);
 DO_TEST(test14);
-DO_TEST(test15);
+DO_TEST_F8(test15);
 END_MAIN
