77714cb975ba7b8ccc9203ae6d4e7eefbf928af8
Roberto Bagnara <bagnara@cs.unipr.it>
2008-08-21 19:27:20 +0000

    FIXME added.

--- src/checked_float.inlines.hh
+++ src/checked_float.inlines.hh
@@ -854,6 +854,11 @@ output_float(std::ostream& os, const Type from, const Numeric_Format&,
     os << "nan";
   else {
     int old_precision = os.precision(10000);
+    // FIXME: here correctness depends on the behavior of the standard
+    // output operator which, in turn, may depend on the behavior
+    // of printf().  The C99 standard, 7.19.6#13, does not give
+    // enough guarantees.  We could not find something similar
+    // in the C++ standard, but perhaps there is a concrete danger here.
     os << from;
     os.precision(old_precision);
   }
