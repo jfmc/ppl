c802d0cb35b417304302dc8e78b35fbd240931c5
Enea Zaffanella <zaffanella@cs.unipr.it>
2008-10-01 19:27:57 +0000

    Due to the recent change to simplify_using_context implementation, tests 05 and 09 no longer overflow when using 8 bit coefficients.

--- tests/Powerset/simplifyusingcontext1.cc
+++ tests/Powerset/simplifyusingcontext1.cc
@@ -450,9 +450,9 @@ BEGIN_MAIN
   DO_TEST(test02);
   DO_TEST(test03);
   DO_TEST(test04);
-  DO_TEST_F8(test05);
+  DO_TEST(test05);
   DO_TEST(test06);
   DO_TEST(test07);
   DO_TEST(test08);
-  DO_TEST_F8(test09);
+  DO_TEST(test09);
 END_MAIN
