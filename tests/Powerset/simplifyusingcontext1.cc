86f9ad585d2c36a00e80dc275034a00b62d7a168
Enea Zaffanella <zaffanella@cs.unipr.it>
2008-10-02 19:03:17 +0000

    Previous commit was wrong: tests 5 and 9 will overflow on 8 bit coefficients, *if* assertions are turned on.

--- tests/Powerset/simplifyusingcontext1.cc
+++ tests/Powerset/simplifyusingcontext1.cc
@@ -450,9 +450,9 @@ BEGIN_MAIN
   DO_TEST(test02);
   DO_TEST(test03);
   DO_TEST(test04);
-  DO_TEST(test05);
+  DO_TEST_F8A(test05);
   DO_TEST(test06);
   DO_TEST(test07);
   DO_TEST(test08);
-  DO_TEST(test09);
+  DO_TEST_F8A(test09);
 END_MAIN
