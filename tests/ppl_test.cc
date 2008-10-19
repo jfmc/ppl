d82e63a3ba99b5104746cc84b320e140ab82cf9c
Roberto Bagnara <bagnara@cs.unipr.it>
2008-10-19 12:15:16 +0000

    Do not rely on the definition of optional macros of <fenv.h>.

--- tests/ppl_test.cc
+++ tests/ppl_test.cc
@@ -85,21 +85,28 @@ fpe_handler(int sig, siginfo_t* sip, void*) {
   else {
     std::cerr << "SIGFPE caught (unknown si_code " << sip->si_code << ")"
 	      << std::endl;
-    // FIXME: as of GCC 4.3.0, defined(PPL_HAVE_FENV_H) does not provide
-    // the information we need (fenv.h is present, but does not contain
-    // the required definitions).
-#if defined(PPL_HAVE_FENV_H) && !defined(__CYGWIN__)
+#if defined(PWL_HAVE_FENV_H)
     std::cerr << "Inquire with fetestexcept(): ";
+#ifdef FE_INEXACT
     if (fetestexcept(FE_INEXACT))
       std::cerr << "FE_INEXACT ";
+#endif
+#ifdef FE_DIVBYZERO
     if (fetestexcept(FE_DIVBYZERO))
       std::cerr << "FE_DIVBYZERO ";
+#endif
+#ifdef FE_UNDERFLOW
     if (fetestexcept(FE_UNDERFLOW))
       std::cerr << "FE_UNDERFLOW ";
+#endif
+#ifdef FE_OVERFLOW
     if (fetestexcept(FE_OVERFLOW))
       std::cerr << "FE_OVERFLOW ";
+#endif
+#if FE_INVALID
     if (fetestexcept(FE_INVALID))
       std::cerr << "FE_INVALID ";
+#endif
     std::cerr << std::endl;
 #endif
   }
