018e1ebb3a93ce22eb755ce9a44df208965f3b84
Enea Zaffanella <zaffanella@cs.unipr.it>
2009-01-20 09:00:37 +0000

    Added a test on hypercubes.

--- tests/BD_Shape/upperboundifexact1.cc
+++ tests/BD_Shape/upperboundifexact1.cc
@@ -323,6 +323,41 @@ test10() {
   return ok;
 }
 
+bool
+test11() {
+  // const dimension_type dim = 500;
+  const dimension_type dim = 50;
+  Constraint_System cs;
+  for (dimension_type i = 1; i < dim; ++i) {
+    Variable x(i);
+    cs.insert(x >= 0);
+    cs.insert(x <= 4);
+  }
+
+  Variable x(0);
+
+  TBD_Shape hypercube1(cs);
+  hypercube1.add_constraint(x >= 0);
+  hypercube1.add_constraint(x <= 4);
+
+  TBD_Shape hypercube2(cs);
+  hypercube2.add_constraint(x >= 2);
+  hypercube2.add_constraint(x <= 6);
+
+  TBD_Shape known_result(cs);
+  known_result.add_constraint(x >= 0);
+  known_result.add_constraint(x <= 6);
+
+  bool ok = hypercube1.upper_bound_assign_if_exact(hypercube2);
+
+  ok &= (hypercube1 == known_result);
+
+  print_constraints(hypercube1, "*** hyp1 ***");
+  print_constraints(hypercube2, "*** hyp2 ***");
+
+  return ok;
+}
+
 } // namespace
 
 BEGIN_MAIN
@@ -336,4 +371,5 @@ BEGIN_MAIN
   DO_TEST(test08);
   DO_TEST(test09);
   DO_TEST(test10);
+  DO_TEST(test11);
 END_MAIN
