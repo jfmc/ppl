b8777ff8bbf8a7dc54cfe1331f5c78eea06c4c56
Roberto Bagnara <bagnara@cs.unipr.it>
2009-03-25 22:01:12 +0100

    Depend on PPL_CAN_CONTROL_FPU.

--- src/fpu-ia32.cc
+++ src/fpu-ia32.cc
@@ -21,7 +21,10 @@ For the most up-to-date information see the Parma Polyhedra Library
 site: http://www.cs.unipr.it/ppl/ . */
 
 #include <ppl-config.h>
-#if defined(PPL_FPMATH_MAY_USE_SSE) && defined(__i386__) && (defined(__GNUC__) || defined(__INTEL_COMPILER))
+
+#if PPL_CAN_CONTROL_FPU && defined(PPL_FPMATH_MAY_USE_SSE) \
+  && defined(__i386__) \
+  && (defined(__GNUC__) || defined(__INTEL_COMPILER))
 
 #include "fpu.defs.hh"
 #include <csetjmp>
@@ -64,4 +67,4 @@ detect_sse_unit() {
 
 } // namespace Parma_Polyhedra_Library
 
-#endif // defined(PPL_FPMATH_MAY_USE_SSE) && defined(__i386__) && (defined(__GNUC__) || defined(__INTEL_COMPILER))
+#endif // PPL_CAN_CONTROL_FPU && defined(PPL_FPMATH_MAY_USE_SSE) && defined(__i386__) && (defined(__GNUC__) || defined(__INTEL_COMPILER))
