dd82590da6f24fb84b0c794d4be71468b6cc1da3
Roberto Bagnara <bagnara@cs.unipr.it>
2009-03-30 18:56:44 +0000

    Disable test19() on the Alpha.

--- tests/Box/relations4.cc
+++ tests/Box/relations4.cc
@@ -480,6 +480,9 @@ BEGIN_MAIN
   DO_TEST(test16);
   DO_TEST(test17);
   DO_TEST(test18);
+#ifndef __alpha__
+  // Exception handling is broken in GCC on the Alpha.
   DO_TEST(test19);
+#endif
   DO_TEST(test20);
 END_MAIN
