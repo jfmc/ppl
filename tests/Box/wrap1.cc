ffc6689ea022e45587d30ea28aae9626cede3616
Roberto Bagnara <bagnara@cs.unipr.it>
2009-05-14 07:10:26 +0200

    Marked tests that are expected to fail with 8-bit coefficients.

--- tests/Box/wrap1.cc
+++ tests/Box/wrap1.cc
@@ -123,7 +123,7 @@ test03() {
 } // namespace
 
 BEGIN_MAIN
-  DO_TEST(test01);
-  DO_TEST(test02);
-  DO_TEST(test03);
+  DO_TEST_F8(test01);
+  DO_TEST_F8(test02);
+  DO_TEST_F8(test03);
 END_MAIN
