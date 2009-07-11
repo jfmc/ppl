99d4c96253e1e775d022baaa67e496ccd8b14fe4
Abramo Bagnara <abramo.bagnara@gmail.com>
2009-07-11 18:14:42 +0200

    Forgotten file.

---
+++ Watchdog/src/Weightwatch.cc
@@ -0,0 +1,36 @@
+/* Weightwatch and associated classes' implementation (non-inline functions).
+   Copyright (C) 2001-2009 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Watchdog Library (PWL).
+
+The PWL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The PWL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include <pwl-config.h>
+
+#include "Weightwatch.defs.hh"
+
+namespace Parma_Watchdog_Library {
+
+#if 0
+// The ordered queue of pending thresholds.
+Weightwatch::WW_Pending_List Weightwatch::pending;
+
+void (**Weightwatch::check_hook_ptr)(void) = 0;
+#endif
+
+}
