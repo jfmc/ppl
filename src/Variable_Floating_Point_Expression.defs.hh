bc1a412d5547c459473918f13c22328758bf5e84
Fabio Bossi <bossi@cs.unipr.it>
2009-09-01 10:40:11 +0200

    Added a first version of class Variable_Floating_Point_Expression.

---
+++ src/Variable_Floating_Point_Expression.defs.hh
@@ -0,0 +1,71 @@
+/* Declarations for the Variable_Floating_Point_Expression class and
+   its constituents.
+   Copyright (C) 2001-2009 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Variable_Floating_Point_Expression_defs_hh
+#define PPL_Variable_Floating_Point_Expression_defs_hh 1
+
+#include "Floating_Point_Expression.defs.hh"
+#include "globals.defs.hh"
+#include "Variable_Floating_Point_Expression.types.hh"
+#include <map>
+
+namespace Parma_Polyhedra_Library {
+
+template <typename FP_Interval_Type, typename FP_Format>
+class Variable_Floating_Point_Expression
+: public virtual Floating_Point_Expression<FP_Interval_Type, FP_Format> {
+
+public:
+
+  typedef typename
+  Floating_Point_Expression<FP_Interval_Type, FP_Format>::
+  FP_Linear_Form FP_Linear_Form;
+
+  typedef typename
+  Floating_Point_Expression<FP_Interval_Type, FP_Format>::
+  FP_Interval_Abstract_Store FP_Interval_Abstract_Store;
+
+  typedef typename
+  Floating_Point_Expression<FP_Interval_Type, FP_Format>::boundary_type
+  boundary_type;
+
+  typedef typename
+  Floating_Point_Expression<FP_Interval_Type, FP_Format>::info_type info_type;
+
+  Variable_Floating_Point_Expression(const dimension_type v_index);
+
+  ~Variable_Floating_Point_Expression();
+
+  FP_Linear_Form linearize(const FP_Interval_Abstract_Store& store) const;
+
+private:
+
+  dimension_type variable_index;
+
+}; // class Variable_Floating_Point_Expression
+
+} // namespace Parma_Polyhedra_Library
+
+#include "Variable_Floating_Point_Expression.inlines.hh"
+
+#endif // !defined(PPL_Variable_Floating_Point_Expression_defs_hh)
---
+++ src/Variable_Floating_Point_Expression.inlines.hh
@@ -0,0 +1,53 @@
+/* Variable_Floating_Point_Expression class implementation: inline functions.
+   Copyright (C) 2001-2009 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Variable_Floating_Point_Expression_inlines_hh
+#define PPL_Variable_Floating_Point_Expression_inlines_hh 1
+
+#include "globals.defs.hh"
+
+namespace Parma_Polyhedra_Library {
+
+template <typename FP_Interval_Type, typename FP_Format>
+inline
+Variable_Floating_Point_Expression<FP_Interval_Type, FP_Format>::
+Variable_Floating_Point_Expression(const dimension_type v_index) {
+  variable_index = v_index;
+}
+
+template <typename FP_Interval_Type, typename FP_Format>
+inline
+Variable_Floating_Point_Expression<FP_Interval_Type, FP_Format>::
+~Variable_Floating_Point_Expression() {}
+
+template <typename FP_Interval_Type, typename FP_Format>
+inline typename Variable_Floating_Point_Expression<FP_Interval_Type,
+                                                   FP_Format>::FP_Linear_Form
+Variable_Floating_Point_Expression<FP_Interval_Type, FP_Format>::linearize(
+const FP_Interval_Abstract_Store& store) const {
+  FP_Linear_Form result = FP_Linear_Form(Variable(variable_index));
+  return result;
+}
+
+} // namespace Parma_Polyhedra_Library
+
+#endif // !defined(PPL_Variable_Floating_Point_Expression_inlines_hh)
