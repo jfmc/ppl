1ce1a9746e940039f68b71e9da8248b3d6145104
Fabio Bossi <bossi@cs.unipr.it>
2009-09-25 11:40:55 +0200

    Added a trivial implementation of generalized_refine_with_linear_form_inequality.

--- src/Octagonal_Shape.inlines.hh
+++ src/Octagonal_Shape.inlines.hh
@@ -615,6 +615,33 @@ Octagonal_Shape<T>::strictly_contains(const Octagonal_Shape& y) const {
 template <typename T>
 template <typename Interval_Info>
 inline void
+Octagonal_Shape<T>::generalized_refine_with_linear_form_inequality(
+		    const Linear_Form< Interval<T, Interval_Info> >& left,
+		    const Linear_Form< Interval<T, Interval_Info> >& right,
+                    Relation_Symbol relsym) {
+  switch (relsym) {
+  case EQUAL:
+    refine_with_linear_form_inequality(left, right);
+    refine_with_linear_form_inequality(right, left);
+    break;
+  case LESS_THAN:
+  case LESS_OR_EQUAL:
+    refine_with_linear_form_inequality(left, right);
+    break;
+  case GREATER_THAN:
+  case GREATER_OR_EQUAL:
+    refine_with_linear_form_inequality(right, left);
+    break;
+  case NOT_EQUAL:
+    break;
+  default:
+    throw std::runtime_error("PPL internal error");
+  }
+}
+
+template <typename T>
+template <typename Interval_Info>
+inline void
 Octagonal_Shape<T>::
 refine_fp_interval_abstract_store(
 	  Box< Interval<T, Interval_Info> >& store) const {
