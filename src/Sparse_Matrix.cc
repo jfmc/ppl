3621844ea66fadeda55bd2828ebe6a8136f1a66a
Marco Poletti <marco@desktop.site>
2010-02-09 12:48:34 +0100

    Added Sparse_Matrix class

---
+++ src/Sparse_Matrix.cc
@@ -0,0 +1,183 @@
+/* Sparse_Matrix class implementation (non-inline functions).
+   Copyright (C) 2001-2010 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#include <ppl-config.h>
+
+#include "Sparse_Matrix.defs.hh"
+
+namespace PPL = Parma_Polyhedra_Library;
+
+PPL::Sparse_Matrix::Sparse_Matrix(dimension_type n)
+  : rows(n), width_(n) {
+  PPL_ASSERT(OK());
+}
+
+PPL::Sparse_Matrix::Sparse_Matrix(dimension_type height, dimension_type width)
+  : rows(height), width_(width) {
+  PPL_ASSERT(OK());
+}
+
+PPL::Sparse_Matrix::iterator
+PPL::Sparse_Matrix::begin() {
+  return iterator(rows.begin(),width_);
+}
+
+PPL::Sparse_Matrix::iterator
+PPL::Sparse_Matrix::end() {
+  return iterator(rows.end(),width_);
+}
+
+PPL::Sparse_Matrix::const_iterator
+PPL::Sparse_Matrix::begin() const {
+  return rows.begin();
+}
+
+PPL::Sparse_Matrix::const_iterator
+PPL::Sparse_Matrix::end() const {
+  return rows.end();
+}
+
+bool
+PPL::Sparse_Matrix::OK() const {
+  for (const_iterator i=begin(),i_end=end(); i!=i_end; ++i) {
+    Sparse_Row row(*i,width_);
+    if (static_cast<Unlimited_Sparse_Row>(row) != *i)
+      return false;
+  }
+  return true;
+}
+
+
+PPL::Sparse_Matrix::iterator::iterator(const iterator& x)
+  : itr(x.itr), size_(x.size_) {
+}
+
+PPL::Sparse_Matrix_Row
+PPL::Sparse_Matrix::iterator::operator*() {
+  return Sparse_Matrix_Row(*itr,size_);
+}
+
+PPL::Sparse_Matrix::iterator&
+PPL::Sparse_Matrix::iterator::operator++() {
+  ++itr;
+  return *this;
+}
+
+PPL::Sparse_Matrix::iterator
+PPL::Sparse_Matrix::iterator::operator++(int) {
+  iterator x(*this);
+  ++(*this);
+  return x;
+}
+
+PPL::Sparse_Matrix::iterator::iterator(
+  std::vector<Unlimited_Sparse_Row>::iterator i,
+  const dimension_type &size)
+  : itr(i), size_(size) {
+}
+
+
+PPL::Sparse_Matrix_Row::Sparse_Matrix_Row(Unlimited_Sparse_Row& row,
+                                          const dimension_type& size)
+  : row_(row), size_(size) {
+  PPL_ASSERT(OK());
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::reset(iterator i) {
+  iterator res = row_.reset(i);
+  PPL_ASSERT(OK());
+  return res;
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::reset(iterator first,iterator last) {
+  iterator res = row_.reset(first,last);
+  PPL_ASSERT(OK());
+  return res;
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::begin() {
+  return row_.begin();
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::end() {
+  return row_.end();
+}
+
+PPL::Sparse_Matrix_Row::const_iterator
+PPL::Sparse_Matrix_Row::begin() const {
+  return row_.begin();
+}
+
+PPL::Sparse_Matrix_Row::const_iterator
+PPL::Sparse_Matrix_Row::end() const {
+  return row_.end();
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::find(const key_type &c) {
+  return row_.find(c);
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::lower_bound(const key_type &c) {
+  return row_.lower_bound(c);
+}
+
+PPL::Sparse_Matrix_Row::iterator
+PPL::Sparse_Matrix_Row::upper_bound(const key_type &c) {
+  return row_.upper_bound(c);
+}
+
+PPL::Sparse_Matrix_Row::const_iterator
+PPL::Sparse_Matrix_Row::find(const key_type &c) const {
+  return row_.find(c);
+}
+
+PPL::Sparse_Matrix_Row::const_iterator
+PPL::Sparse_Matrix_Row::lower_bound(const key_type &c) const {
+  return row_.lower_bound(c);
+}
+
+PPL::Sparse_Matrix_Row::const_iterator
+PPL::Sparse_Matrix_Row::upper_bound(const key_type &c) const {
+  return row_.upper_bound(c);
+}
+
+PPL::Sparse_Matrix_Row::operator const Unlimited_Sparse_Row&() const {
+  return row_;
+}
+
+PPL::Sparse_Matrix_Row::operator Sparse_Row() const {
+  return Sparse_Row(row_,size_);
+}
+
+bool
+PPL::Sparse_Matrix_Row::OK() const {
+  Sparse_Row row(row_,size_);
+  if (static_cast<Unlimited_Sparse_Row>(row) != row_)
+    return false;
+  return true;
+}
---
+++ src/Sparse_Matrix.defs.hh
@@ -0,0 +1,118 @@
+/* Sparse_Matrix class declaration.
+   Copyright (C) 2001-2010 Roberto Bagnara <bagnara@cs.unipr.it>
+
+This file is part of the Parma Polyhedra Library (PPL).
+
+The PPL is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 3 of the License, or (at your
+option) any later version.
+
+The PPL is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
+
+For the most up-to-date information see the Parma Polyhedra Library
+site: http://www.cs.unipr.it/ppl/ . */
+
+#ifndef PPL_Sparse_Matrix_defs_hh
+#define PPL_Sparse_Matrix_defs_hh 1
+
+#include "Sparse_Matrix.types.hh"
+#include "Sparse_Row.defs.hh"
+#include <vector>
+
+class Parma_Polyhedra_Library::Sparse_Matrix {
+
+public:
+  class iterator;
+  typedef std::vector<Unlimited_Sparse_Row>::const_iterator const_iterator;
+
+  //! Constructs a square matrix filled with zeroes with the given size.
+  Sparse_Matrix(dimension_type n=0);
+
+  //! Constructs a matrix filled with zeroes with the given dimensions.
+  Sparse_Matrix(dimension_type height,dimension_type width);
+
+  iterator begin();
+  iterator end();
+  const_iterator begin() const;
+  const_iterator end() const;
+
+  //! Checks if all the invariants are satisfied.
+  bool OK() const;
+
+private:
+  std::vector<Unlimited_Sparse_Row> rows;
+  dimension_type width_;
+};
+
+class Parma_Polyhedra_Library::Sparse_Matrix::iterator {
+
+public:
+  iterator(const iterator&);
+
+  Sparse_Matrix_Row operator*();
+
+  iterator& operator++();
+  iterator operator++(int);
+
+private:
+  iterator(std::vector<Unlimited_Sparse_Row>::iterator,
+           const dimension_type &size);
+
+  std::vector<Unlimited_Sparse_Row>::iterator itr;
+  const dimension_type size_;
+
+  friend class Parma_Polyhedra_Library::Sparse_Matrix;
+};
+
+class Parma_Polyhedra_Library::Sparse_Matrix_Row {
+
+public:
+  typedef Unlimited_Sparse_Row::key_type key_type;
+
+  Sparse_Matrix_Row(Unlimited_Sparse_Row& row,const dimension_type& size);
+
+  //! A const iterator that may skip some zeros in the row.
+  typedef Unlimited_Sparse_Row::const_iterator const_iterator;
+
+  //! An iterator that may skip some zeros in the row.
+  typedef Unlimited_Sparse_Row::iterator iterator;
+
+  //! Resets to zero the value pointed by i.
+  iterator reset(iterator i);
+
+  //! Resets to zero the values in the range [first,last).
+  iterator reset(iterator first,iterator last);
+
+  iterator begin();
+  iterator end();
+  const_iterator begin() const;
+  const_iterator end() const;
+
+  iterator find(const key_type &c);
+  iterator lower_bound(const key_type &c);
+  iterator upper_bound(const key_type &c);
+  const_iterator find(const key_type &c) const;
+  const_iterator lower_bound(const key_type &c) const;
+  const_iterator upper_bound(const key_type &c) const;
+
+  operator const Unlimited_Sparse_Row&() const;
+  operator Sparse_Row() const;
+
+  //! Checks the invariant.
+  bool OK() const;
+  
+private:
+  Unlimited_Sparse_Row& row_;
+  const dimension_type size_;
+};
+
+
+#endif // !defined(PPL_Sparse_Matrix_defs_hh)
