e9bc08e3a715da5aab9ac0e24f9221a52332ba1d
Marco Poletti <poletti.marco@gmail.com>
2010-03-14 19:39:34 +0100

    PIP_Solution_Node: prepare solve() method for optimizations.

--- src/PIP_Tree.cc
+++ src/PIP_Tree.cc
@@ -2682,44 +2682,44 @@ PIP_Solution_Node::solve(const PIP_Problem& pip,
 
       // Compute columns t[*][j] :
       // t[i][j] -= s[i][pj] * t_pivot[j] / s_pivot_pj;
-      for (dimension_type j = num_params; j-- > 0; ) {
-        const Coefficient& t_pivot_j = t_pivot[j];
-        // Do nothing if the j-th pivot element is zero.
-        if (t_pivot_j == 0)
-          continue;
-        for (dimension_type i = num_rows; i-- > 0; ) {
-          matrix_row_reference_type s_i = tableau.s[i];
-          product = t_pivot_j * s_i[pj];
-          if (product % s_pivot_pj != 0) {
-            // Must scale matrix to stay in integer case.
-            gcd_assign(gcd, product, s_pivot_pj);
-            exact_div_assign(scale_factor, s_pivot_pj, gcd);
-            tableau.scale(scale_factor);
-            product *= scale_factor;
-          }
-          PPL_ASSERT(product % s_pivot_pj == 0);
-          exact_div_assign(product, product, s_pivot_pj);
-          tableau.t[i][j] -= product;
+      for (dimension_type i = num_rows; i-- > 0; ) {
+        matrix_row_reference_type s_i = tableau.s[i];
+        for (dimension_type j = num_params; j-- > 0; ) {
+          const Coefficient& t_pivot_j = t_pivot[j];
+          // Do nothing if the j-th pivot element is zero.
+          if (t_pivot_j != 0) {
+            product = t_pivot_j * s_i[pj];
+            if (product % s_pivot_pj != 0) {
+              // Must scale matrix to stay in integer case.
+              gcd_assign(gcd, product, s_pivot_pj);
+              exact_div_assign(scale_factor, s_pivot_pj, gcd);
+              tableau.scale(scale_factor);
+              product *= scale_factor;
+            }
+            PPL_ASSERT(product % s_pivot_pj == 0);
+            exact_div_assign(product, product, s_pivot_pj);
+            tableau.t[i][j] -= product;
 
-          // Update row sign.
-          Row_Sign& sign_i = sign[i];
-          switch (sign_i) {
-          case ZERO:
-            if (product > 0)
-              sign_i = NEGATIVE;
-            else if (product < 0)
-              sign_i = POSITIVE;
-            break;
-          case POSITIVE:
-            if (product > 0)
-              sign_i = MIXED;
-            break;
-          case NEGATIVE:
-            if (product < 0)
-              sign_i = MIXED;
-            break;
-          default:
-            break;
+            // Update row sign.
+            Row_Sign& sign_i = sign[i];
+            switch (sign_i) {
+            case ZERO:
+              if (product > 0)
+                sign_i = NEGATIVE;
+              else if (product < 0)
+                sign_i = POSITIVE;
+              break;
+            case POSITIVE:
+              if (product > 0)
+                sign_i = MIXED;
+              break;
+            case NEGATIVE:
+              if (product < 0)
+                sign_i = MIXED;
+              break;
+            default:
+              break;
+            }
           }
         }
       }
