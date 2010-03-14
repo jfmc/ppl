cab3898e8bc20b03d2fb3bcfa3ef71aba5a9876b
Marco Poletti <poletti.marco@gmail.com>
2010-03-14 19:09:23 +0100

    PIP_Tree_Node: add typedefs for matrix_row_copy_iterator and matrix_row_copy_const_iterator.

--- src/PIP_Tree.defs.hh
+++ src/PIP_Tree.defs.hh
@@ -64,6 +64,8 @@ public:
   typedef Dense_Row::iterator matrix_row_iterator;
   typedef Dense_Row::const_iterator matrix_row_const_iterator;
   typedef Dense_Row matrix_row_copy_type;
+  typedef Dense_Row::iterator matrix_row_copy_iterator;
+  typedef Dense_Row::const_iterator matrix_row_copy_const_iterator;
 #else
   typedef Sparse_Matrix matrix_type;
   typedef Sparse_Row_Reference matrix_row_reference_type;
@@ -73,6 +75,8 @@ public:
   typedef Sparse_Row_Reference::iterator matrix_row_iterator;
   typedef Sparse_Row_Reference::const_iterator matrix_row_const_iterator;
   typedef Sparse_Row matrix_row_copy_type;
+  typedef Sparse_Row::iterator matrix_row_copy_iterator;
+  typedef Sparse_Row::const_iterator matrix_row_copy_const_iterator;
 #endif
 
 protected:
