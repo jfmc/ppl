d67331d400fb5c8696c4ccddf61c9f5f09bacb02
Marco Poletti <poletti.marco@gmail.com>
2010-03-14 15:16:14 +0100

    PIP_Tree.cc: cleaned up compatibility_check_find_pivot_in_set().

--- src/PIP_Tree.cc
+++ src/PIP_Tree.cc
@@ -709,6 +709,8 @@ compatibility_check_find_pivot_in_set(std::set<std::pair<dimension_type,
       }
     } else {
       // Not in base.
+      const Coefficient* row_value;
+      PIP_Tree_Node::matrix_row_const_reference_type row = s[row_index];
       for (++i; i!=i_end; ++i) {
         const dimension_type challenger_i = i->second.row_index;
         const dimension_type challenger_j = i->first;
@@ -725,12 +727,10 @@ compatibility_check_find_pivot_in_set(std::set<std::pair<dimension_type,
 
         PPL_DIRTY_TEMP_COEFFICIENT(lhs);
         PPL_DIRTY_TEMP_COEFFICIENT(rhs);
-        PIP_Tree_Node::matrix_row_const_reference_type row = s[row_index];
-        const Coefficient* t_mk_ja;
-        const Coefficient* t_mk_jb;
-        row.get2(pj,challenger_j,t_mk_ja,t_mk_jb);
-        lhs = lhs_coeff * *t_mk_ja;
-        rhs = rhs_coeff * *t_mk_jb;
+        const Coefficient* row_challenger_value;
+        row.get2(pj,challenger_j,row_value,row_challenger_value);
+        lhs = lhs_coeff * *row_value;
+        rhs = rhs_coeff * *row_challenger_value;
         if (lhs == rhs)
           new_candidates.insert(*i);
         else {
