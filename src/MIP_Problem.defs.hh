e0c200993bcf3c0165657f9365b51b04293a8020
Marco Poletti <poletti.marco@gmail.com>
2010-03-17 22:32:39 +0100

    MIP_Problem: add typedefs for matrix_row_copy_type, matrix_row_copy_iterator and matrix_row_copy_const_iterator.

--- src/MIP_Problem.defs.hh
+++ src/MIP_Problem.defs.hh
@@ -100,6 +100,9 @@ public:
   typedef Dense_Row::const_iterator matrix_const_row_const_iterator;
   typedef Dense_Row::iterator matrix_row_iterator;
   typedef Dense_Row::const_iterator matrix_row_const_iterator;
+  typedef Dense_Row matrix_row_copy_type;
+  typedef Dense_Row::iterator matrix_row_copy_iterator;
+  typedef Dense_Row::const_iterator matrix_row_copy_const_iterator;
 #else
   typedef Sparse_Matrix matrix_type;
   typedef Sparse_Row_Reference matrix_row_reference_type;
@@ -109,6 +112,9 @@ public:
     matrix_const_row_const_iterator;
   typedef Sparse_Row_Reference::iterator matrix_row_iterator;
   typedef Sparse_Row_Reference::const_iterator matrix_row_const_iterator;
+  typedef Sparse_Row matrix_row_copy_type;
+  typedef Sparse_Row::iterator matrix_row_copy_iterator;
+  typedef Sparse_Row::const_iterator matrix_row_copy_const_iterator;
 #endif
 
   //! Builds a trivial MIP problem.
