b31687efa4e87fa9c234114bf957798e2248d22c
Marco Poletti <poletti.marco@gmail.com>
2010-03-23 22:22:56 +0100

    Unlimited_Sparse_Row: add combine_needs_first() and combine_needs_second() methods.

--- src/Unlimited_Sparse_Row.templates.hh
+++ src/Unlimited_Sparse_Row.templates.hh
@@ -26,6 +26,215 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 namespace Parma_Polyhedra_Library {
 
+template <typename Func1, typename Func2>
+void
+Unlimited_Sparse_Row::combine_needs_first(const Unlimited_Sparse_Row& y,
+                                          const Func1& f, const Func2& g) {
+  iterator i = begin();
+  iterator last_i = begin();
+  iterator i_end = end();
+  const_iterator j = y.begin();
+  const_iterator j_end = y.end();
+  if (i == i_end)
+    return;
+  if (j != j_end) {
+    if ((*i).first == (*j).first) {
+      g((*i).second, (*j).second);
+      last_i = i;
+      ++i;
+      ++j;
+    } else
+      if ((*i).first < (*j).first) {
+        f((*i).second);
+        last_i = i;
+        ++i;
+      } else {
+        ++j;
+      }
+  } else {
+    f((*i).second);
+    last_i = i;
+    ++i;
+  }
+  PPL_ASSERT(last_i != i_end);
+  while (i != i_end && j != j_end)
+    if ((*i).first == (*j).first) {
+      g((*i).second, (*j).second);
+      last_i = i;
+      ++i;
+      ++j;
+    } else
+      if ((*i).first < (*j).first) {
+        f((*i).second);
+        last_i = i;
+        ++i;
+      } else
+        j = y.lower_bound((*i).first, j);
+  while (i != i_end) {
+    f((*i).second);
+    ++i;
+  }
+}
+
+#ifndef PPL_SPARSE_BACKEND_SLOW_INSERTIONS
+
+template <typename Func1, typename Func2>
+void
+Unlimited_Sparse_Row::combine_needs_second(const Unlimited_Sparse_Row& y,
+                                           const Func1& g, const Func2& h) {
+  iterator i = begin();
+  iterator last_i = begin();
+  iterator i_end = end();
+  const_iterator j = y.begin();
+  const_iterator j_end = y.end();
+  if (i != i_end) {
+    if (j != j_end) {
+      if ((*i).first == (*j).first) {
+        g((*i).second, (*j).second);
+        last_i = i;
+        ++i;
+        ++j;
+      } else
+        if ((*i).first < (*j).first) {
+          last_i = i;
+          ++i;
+        } else {
+          last_i = find_create((*j).first);
+          h((*last_i).second, (*j).second);
+#ifdef PPL_SPARSE_BACKEND_INVALIDATES_REFERENCES
+          i = last_i;
+          ++i;
+          i_end = x.end();
+          if (& static_cast<PIP_Tree_Node::
+                            matrix_row_const_reference_type>(x) == &y) {
+            j = last_i;
+            j_end = y.end();
+          }
+#endif
+          ++j;
+        }
+    } else {
+      last_i = i;
+      ++i;
+    }
+  } else {
+    if (j != j_end) {
+      last_i = find_create((*j).first);
+      h((*last_i).second, (*j).second);
+#ifdef PPL_SPARSE_BACKEND_INVALIDATES_REFERENCES
+      i = last_i;
+      ++i;
+      i_end = x.end();
+      if (& static_cast<PIP_Tree_Node::
+                        matrix_row_const_reference_type>(x) == &y) {
+        j = last_i;
+        j_end = y.end();
+      }
+#endif
+      ++j;
+    } else {
+      PPL_ASSERT(i == i_end);
+      PPL_ASSERT(j == j_end);
+
+      return;
+    }
+  }
+  PPL_ASSERT(last_i != i_end);
+  while (i != i_end && j != j_end)
+    if ((*i).first == (*j).first) {
+      g((*i).second, (*j).second);
+      last_i = i;
+      ++i;
+      ++j;
+    } else
+      if ((*i).first < (*j).first) {
+        last_i = i;
+        i = lower_bound((*j).first, i);
+      } else {
+        last_i = find_create((*j).first, last_i);
+        h((*last_i).second, (*j).second);
+#ifdef PPL_SPARSE_BACKEND_INVALIDATES_REFERENCES
+        i = last_i;
+        ++i;
+        i_end = x.end();
+        if (& static_cast<PIP_Tree_Node::
+                          matrix_row_const_reference_type>(x) == &y) {
+          j = last_i;
+          j_end = y.end();
+        }
+#endif
+        ++j;
+      }
+  while (j != j_end) {
+    last_i = find_create((*j).first, last_i);
+    h((*last_i).second, (*j).second);
+    ++j;
+  }
+}
+
+#else
+
+template <typename Func1, typename Func2>
+void
+Unlimited_Sparse_Row::combine_needs_second(const Unlimited_Sparse_Row& y,
+                                           const Func1& g, const Func2& h) {
+  Unlimited_Sparse_Row row;
+  iterator itr = row.end();
+  const_iterator i = begin();
+  const_iterator i_end = end();
+  const_iterator j = y.begin();
+  const_iterator j_end = y.end();
+  if (i == i_end && j == j_end)
+    return;
+  if (j == j_end
+      || (i != i_end && (*i).first < (*j).first)) {
+    itr = row.find_create(*i);
+    ++i;
+  } else
+      if (i == i_end
+          || (j != j_end && (*i).first > (*j).first)) {
+        itr = row.find_create((*j).first);
+        h((*itr).second, (*j).second);
+        ++j;
+      } else {
+        PPL_ASSERT(i != i_end);
+        PPL_ASSERT(j != j_end);
+        PPL_ASSERT((*i).first == (*j).first);
+        itr = row.find_create(*i);
+        g((*itr).second, (*j).second);
+        ++i;
+        ++j;
+      }
+  PPL_ASSERT(itr != row.end());
+  while (i != i_end && j != j_end) {
+    if ((*i).first < (*j).first) {
+      itr = row.find_create(*i, itr);
+      ++i;
+    } else {
+      if ((*i).first > (*j).first) {
+        itr = row.find_create(*j, itr);
+        h((*itr).second, (*j).second);
+        ++j;
+      } else {
+        PPL_ASSERT((*i).first == (*j).first);
+        itr = row.find_create(*i, itr);
+        g((*itr).second, (*j).second);
+        ++i;
+        ++j;
+      }
+    }
+  }
+  while (j != j_end) {
+    itr = row.find_create((*j).first, itr);
+    h((*itr).second, (*j).second);
+    ++j;
+  }
+  PPL_ASSERT(j == j_end);
+  std::swap(row,*this);
+}
+
+#endif // !defined(PPL_SPARSE_BACKEND_SLOW_INSERTIONS)
+
 #ifndef PPL_SPARSE_BACKEND_SLOW_INSERTIONS
 
 template <typename Func1, typename Func2, typename Func3>
