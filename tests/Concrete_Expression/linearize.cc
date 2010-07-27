0701ca66ee0a9d2b462bfe8174d8e4d2cf98ed0f
Fabio Bossi <bossi@cs.unipr.it>
2010-07-27 13:59:48 +0200

    Added test file for the new linearization algorithm.

--- tests/Concrete_Expression/linearize.cc
+++ tests/Concrete_Expression/linearize.cc
@@ -1,9 +1,9 @@
-/* Test function compute_absolute_error.
+/* Testing linearization algorithm ad its related functions.
    Copyright (C) 2001-2010 Roberto Bagnara <bagnara@cs.unipr.it>
 
 This file is part of the Parma Polyhedra Library (PPL).
 
-The PPL is free software; you can redistribute it and/or modify it
+The PPL is free software; you can redistribute it and/or moDif_FP_Expressiony it
 under the terms of the GNU General Public License as published by the
 Free Software Foundation; either version 3 of the License, or (at your
 option) any later version.
@@ -21,23 +21,28 @@ For the most up-to-date information see the Parma Polyhedra Library
 site: http://www.cs.unipr.it/ppl/ . */
 
 #include "ppl_test.hh"
+#include "C_Expr.defs.hh"
 
 namespace {
 
 using namespace Parma_Polyhedra_Library::IO_Operators;
 
+// Tests division by zero.
 bool
 test01() {
-  std::cout << compute_absolute_error<FP_Interval>(ANALYZED_FP_FORMAT) <<
-  std::endl;
-
-  std::cout << compute_absolute_error<FP_Interval>(ANALYZED_FP_FORMAT) <<
-  std::endl;
-
-  return 1;
+  Floating_Point_Constant<C_Expr> num("3", 2);
+  Floating_Point_Constant<C_Expr> den("0", 2);
+  Binary_Operator<C_Expr> div(Binary_Operator<C_Expr>::DIV, num, den);
+  FP_Linear_Form result;
+  if (!linearize(div, FP_Interval_Abstract_Store(),
+                 FP_Linear_Form_Abstract_Store(), result)) {
+    nout << "*** Linearization failed due to division by zero. ***" << endl;
+    return true;
+  }
+  return false;
 }
 
-}
+} // namespace
 
 BEGIN_MAIN
   DO_TEST(test01);
