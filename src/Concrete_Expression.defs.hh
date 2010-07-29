253cc5c29243856d96d5d73b9d5acfc18e4d5a39
Fabio Bossi <bossi@cs.unipr.it>
2010-07-29 15:34:08 +0200

    Corrected the policy values. Make check now works again.

--- src/Concrete_Expression.defs.hh
+++ src/Concrete_Expression.defs.hh
@@ -117,13 +117,13 @@ private:
 };
 
 struct Integer_Interval_Info_Policy {
-  const_bool_nodef(store_special, false);
-  const_bool_nodef(store_open, true);
+  const_bool_nodef(store_special, true);
+  const_bool_nodef(store_open, false);
   const_bool_nodef(cache_empty, true);
   const_bool_nodef(cache_singleton, true);
   const_bool_nodef(cache_normalized, false);
   const_int_nodef(next_bit, 0);
-  const_bool_nodef(may_be_empty, false);
+  const_bool_nodef(may_be_empty, true);
   const_bool_nodef(may_contain_infinity, false);
   const_bool_nodef(check_empty_result, false);
   const_bool_nodef(check_inexact, false);
