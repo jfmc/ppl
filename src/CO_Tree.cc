472f096006af04c7e29a776e8684ae9b7cc07fe6
Marco Poletti <poletti.marco@gmail.com>
2010-08-11 12:20:57 +0200

    CO_Tree: improve code readability and adjust stack size, in the constructor from an std::vector.

--- src/CO_Tree.cc
+++ src/CO_Tree.cc
@@ -50,7 +50,13 @@ PPL::CO_Tree::CO_Tree(const std::vector<data_type>& v) {
   }
 
   // This is static and with static allocation, to improve performance.
-  static std::pair<dimension_type,char> stack[5*CHAR_BIT*sizeof(dimension_type)];
+  // CHAR_BIT*sizeof(dimension_type) is the maximum k such that 2^k-1 is a
+  // dimension_type, so it is the maximum tree height.
+  // For each node level, the stack may contain up to 4 elements: two elements
+  // with operation 0, one element with operation 2 and one element
+  // with operation 3. An additional element with operation 1 can be at the
+  // top of the tree.
+  static std::pair<dimension_type,char> stack[4*CHAR_BIT*sizeof(dimension_type)+1];
   dimension_type stack_first_empty = 0;
 
   // A pair (n, operation) in the stack means:
@@ -60,10 +66,10 @@ PPL::CO_Tree::CO_Tree(const std::vector<data_type>& v) {
   //   operation is 1.
   // * Go to the right child, then visit the current tree with n elements, if
   //   operation is 2.
-  // * Fill the current tree with n elements, if operation is 4.
+  // * Fill the current tree with n elements, if operation is 3.
 
   stack[0].first = n;
-  stack[0].second = 4;
+  stack[0].second = 3;
   ++stack_first_empty;
 
   while (stack_first_empty != 0) {
@@ -88,7 +94,7 @@ PPL::CO_Tree::CO_Tree(const std::vector<data_type>& v) {
       root.get_right_child();
       break;
 #ifndef NDEBUG
-    case 4:
+    case 3:
       break;
 
     default:
@@ -117,7 +123,7 @@ PPL::CO_Tree::CO_Tree(const std::vector<data_type>& v) {
         const dimension_type half = (top_n + 1) / 2;
         stack[stack_first_empty - 1].second = 0;
         stack[stack_first_empty    ] = std::make_pair(top_n - half, 2);
-        stack[stack_first_empty + 1] = std::make_pair(1, 4);
+        stack[stack_first_empty + 1] = std::make_pair(1, 3);
         stack[stack_first_empty + 2].second = 0;
         stack[stack_first_empty + 3] = std::make_pair(half - 1, 1);
         stack_first_empty += 4;
