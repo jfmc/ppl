12f60158e2122303413a67c19425ba8714196f1e
Roberto Bagnara <bagnara@cs.unipr.it>
2010-08-29 12:10:11 +0200

    When the `--check' option is used, input data is perturbed the same way as GLPK does.

--- demos/ppl_lpsol/ppl_lpsol.c
+++ demos/ppl_lpsol/ppl_lpsol.c
@@ -128,20 +128,22 @@ static struct option long_options[] = {
   "Reads a file in MPS format and attempts solution using the optimization\n" \
   "algorithms provided by the PPL.\n\n"                                 \
   "Options:\n"                                                          \
-  "  -c, --check[=THRESHOLD] checks the obtained results;  optima are checked\n" \
-  "                          with a tolerance of THRESHOLD (default %.10g)\n" \
-  "  -i, --incremental       solves the problem incrementally\n"        \
-  "  -m, --min               minimizes the objective function\n"        \
-  "  -M, --max               maximizes the objective function (default)\n"
+  "  -c, --check[=THRESHOLD] checks the obtained results using GLPK;\n" \
+  "                          optima are checked with a tolerance of\n"  \
+  "                          THRESHOLD (default %.10g);  input data\n"  \
+  "                          are also perturbed the same way as GLPK does\n" \
+  "  -i, --incremental       solves the problem incrementally\n"
 #define USAGE_STRING1                                                   \
+  "  -m, --min               minimizes the objective function\n"        \
+  "  -M, --max               maximizes the objective function (default)\n" \
   "  -n, --no-optimization   checks for satisfiability only\n"          \
   "  -r, --no-mip            consider integer variables as real variables\n" \
   "  -CSECS, --max-cpu=SECS  limits CPU usage to SECS seconds\n"        \
   "  -RMB, --max-memory=MB   limits memory usage to MB megabytes\n"     \
   "  -h, --help              prints this help text to stdout\n"         \
-  "  -oPATH, --output=PATH   appends output to PATH\n"                  \
-  "  -e, --enumerate         use the (expensive!) enumeration method\n"
+  "  -oPATH, --output=PATH   appends output to PATH\n"
 #define USAGE_STRING2                                                   \
+  "  -e, --enumerate         use the (expensive!) enumeration method\n" \
   "  -pM, --pricing=M        use pricing method M for simplex (assumes -s);\n" \
   "                          M is an int from 0 to 2, default 0:\n"     \
   "                          0 --> steepest-edge using floating point\n" \
@@ -209,7 +211,7 @@ my_exit(int status) {
   exit(status);
 }
 
-static void
+void
 fatal(const char* format, ...) {
   va_list ap;
   fprintf(stderr, "%s: ", program_name);
@@ -995,6 +997,15 @@ solve_with_simplex(ppl_const_Constraint_System_t cs,
 }
 
 static void
+set_mpq_t_from_double(mpq_t q, double d) {
+  void set_d_eps(mpq_t x, double val);
+  if (check_results)
+    set_d_eps(q, d);
+  else
+    mpq_set_d(q, d);
+}
+
+static void
 solve(char* file_name) {
   ppl_Constraint_System_t ppl_cs;
 #ifndef NDEBUG
@@ -1091,14 +1102,14 @@ solve(char* file_name) {
     /* Set `nz' to the number of non-zero coefficients. */
     nz = lpx_get_mat_row(glpk_lp, row, coefficient_index, coefficient_value);
     for (i = 1; i <= nz; ++i) {
-      mpq_set_d(rational_coefficient[i], coefficient_value[i]);
+      set_mpq_t_from_double(rational_coefficient[i], coefficient_value[i]);
       /* Update den_lcm. */
       mpz_lcm(den_lcm, den_lcm, mpq_denref(rational_coefficient[i]));
     }
     lpx_get_row_bnds(glpk_lp, row, &type, &lb, &ub);
-    mpq_set_d(rational_lb, lb);
+    set_mpq_t_from_double(rational_lb, lb);
     mpz_lcm(den_lcm, den_lcm, mpq_denref(rational_lb));
-    mpq_set_d(rational_ub, ub);
+    set_mpq_t_from_double(rational_ub, ub);
     mpz_lcm(den_lcm, den_lcm, mpq_denref(rational_ub));
 
     ppl_new_Linear_Expression_with_dimension(&ppl_le, dimension);
@@ -1135,8 +1146,8 @@ solve(char* file_name) {
   for (column = 1; column <= dimension; ++column) {
     lpx_get_col_bnds(glpk_lp, column, &type, &lb, &ub);
 
-    mpq_set_d(rational_lb, lb);
-    mpq_set_d(rational_ub, ub);
+    set_mpq_t_from_double(rational_lb, lb);
+    set_mpq_t_from_double(rational_ub, ub);
 
     /* Initialize the least common multiple computation. */
     mpz_set_si(den_lcm, 1);
@@ -1162,10 +1173,10 @@ solve(char* file_name) {
   mpz_set_si(den_lcm, 1);
 
   mpq_init(objective[0]);
-  mpq_set_d(objective[0], lpx_get_obj_coef(glpk_lp, 0));
+  set_mpq_t_from_double(objective[0], lpx_get_obj_coef(glpk_lp, 0));
   for (i = 1; i <= dimension; ++i) {
     mpq_init(objective[i]);
-    mpq_set_d(objective[i], lpx_get_obj_coef(glpk_lp, i));
+    set_mpq_t_from_double(objective[i], lpx_get_obj_coef(glpk_lp, i));
     /* Update den_lcm. */
     mpz_lcm(den_lcm, den_lcm, mpq_denref(objective[i]));
   }
