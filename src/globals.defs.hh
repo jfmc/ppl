5347e4de5077a0315111638071a2359ba4beb860
Roberto Bagnara <bagnara@cs.unipr.it>
2010-09-03 15:04:17 +0200

    By default, use sparse matrices both for MIP_Problem and PIP_Problem.

--- src/globals.defs.hh
+++ src/globals.defs.hh
@@ -469,9 +469,9 @@ FOK(mpq_class)
 #include "Weight_Profiler.defs.hh"
 #endif
 
-// By default, use dense matrices both for MIP_Problem and PIP_Problem.
+// By default, use sparse matrices both for MIP_Problem and PIP_Problem.
 #ifndef USE_PPL_SPARSE_MATRIX
-#define USE_PPL_SPARSE_MATRIX 0
+#define USE_PPL_SPARSE_MATRIX 1
 #endif
 
 #include "globals.inlines.hh"
