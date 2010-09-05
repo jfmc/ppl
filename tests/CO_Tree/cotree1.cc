eaf5206df76c2d7ce769d476e5b030fecb15837a
Marco Poletti <poletti.marco@gmail.com>
2010-09-05 12:14:47 +0200

    tests/CO_Tree: use DO_TEST_F8 when a test is expected to fail with 8-bit coefficients.

--- tests/CO_Tree/cotree1.cc
+++ tests/CO_Tree/cotree1.cc
@@ -2463,12 +2463,12 @@ test16() {
 BEGIN_MAIN
   DO_TEST(test01);
   DO_TEST(test02);
-  DO_TEST(test03);
-  DO_TEST(test04);
-  DO_TEST(test05);
-  DO_TEST(test06);
-  DO_TEST(test07);
-  DO_TEST(test08);
+  DO_TEST_F8(test03);
+  DO_TEST_F8(test04);
+  DO_TEST_F8(test05);
+  DO_TEST_F8(test06);
+  DO_TEST_F8(test07);
+  DO_TEST_F8(test08);
   DO_TEST(test09);
   DO_TEST(test10);
   DO_TEST(test11);
