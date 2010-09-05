e690648799fb6e642a96e8a1658915776c908f15
Marco Poletti <poletti.marco@gmail.com>
2010-09-05 19:08:47 +0200

    Row_Flags: make base_type, the constructor from a base_type and get_bits() public.

--- src/Row_Flags.defs.hh
+++ src/Row_Flags.defs.hh
@@ -36,8 +36,17 @@ site: http://www.cs.unipr.it/ppl/ . */
 #endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
 class Parma_Polyhedra_Library::Row_Flags {
 public:
+  //! A native integral type holding the bits that encode the flags.
+  typedef unsigned int base_type;
+
   //! Constructs an object with all the flags unset.
   Row_Flags();
+
+  //! Constructs an object with flags set as in \p n.
+  explicit Row_Flags(base_type n);
+
+  //! Returns the integer encoding \p *this.
+  base_type get_bits() const;
 
   //! Returns <CODE>true</CODE> if and only if \p *this and \p y are equal.
   bool operator==(const Row_Flags& y) const;
@@ -62,20 +71,12 @@ public:
   void serialize(Archive & ar, const unsigned int /* version */);
 
 protected:
-  //! A native integral type holding the bits that encode the flags.
-  typedef unsigned int base_type;
 
   //! Index of the first bit derived classes can use.
   static const unsigned first_free_bit = 0;
 
   //! Total number of bits that can be stored.
   static const unsigned num_bits = std::numeric_limits<base_type>::digits;
-
-  //! Constructs an object with flags set as in \p n.
-  explicit Row_Flags(base_type n);
-
-  //! Returns the integer encoding \p *this.
-  base_type get_bits() const;
 
   //! Sets the bits in \p mask.
   void set_bits(base_type mask);
