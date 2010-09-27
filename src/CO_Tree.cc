3cb542d4fe761e30e8db4a59f596bec8fd2598ea
Marco Poletti <poletti.marco@gmail.com>
2010-09-27 10:50:20 +0200

    CO_Tree: avoid shadowing a field with a local variable, in external_memory_in_bytes().

--- src/CO_Tree.cc
+++ src/CO_Tree.cc
@@ -28,16 +28,16 @@ namespace PPL = Parma_Polyhedra_Library;
 
 PPL::dimension_type
 PPL::CO_Tree::external_memory_in_bytes() const {
-  dimension_type size = 0;
+  dimension_type memory_size = 0;
   if (reserved_size != 0) {
     // Add the size of data[]
-    size += (reserved_size + 1)*sizeof(data[0]);
+    memory_size += (reserved_size + 1)*sizeof(data[0]);
     // Add the size of indexes[]
-    size += (reserved_size + 2)*sizeof(indexes[0]);
+    memory_size += (reserved_size + 2)*sizeof(indexes[0]);
     for (const_iterator itr = begin(), itr_end = end(); itr != itr_end; ++itr)
-      size += PPL::external_memory_in_bytes(*itr);
+      memory_size += PPL::external_memory_in_bytes(*itr);
   }
-  return size;
+  return memory_size;
 }
 
 PPL::CO_Tree::iterator
