fbfb2232f9296687e66d16da67b3f5ccfc8b48b6
Fabio Bossi <bossi@cs.unipr.it>
2010-11-30 15:29:01 +0100

    Renamed Integer_Interval_Type into Integer_Interval and moved it to Integer_Interval.hh.

--- src/Concrete_Expression.defs.hh
+++ src/Concrete_Expression.defs.hh
@@ -116,25 +116,6 @@ private:
   Implementation impl;
 };
 
-struct Integer_Interval_Info_Policy {
-  const_bool_nodef(store_special, true);
-  const_bool_nodef(store_open, false);
-  const_bool_nodef(cache_empty, true);
-  const_bool_nodef(cache_singleton, true);
-  const_bool_nodef(cache_normalized, false);
-  const_int_nodef(next_bit, 0);
-  const_bool_nodef(may_be_empty, true);
-  const_bool_nodef(may_contain_infinity, false);
-  const_bool_nodef(check_empty_result, false);
-  const_bool_nodef(check_inexact, false);
-};
-
-typedef Interval_Restriction_None
-<Interval_Info_Bitset<unsigned int, Integer_Interval_Info_Policy> >
-Integer_Interval_Info;
-
-typedef Interval<mpz_class, Integer_Interval_Info> Integer_Interval_Type;
-
 template <typename Target>
 class Concrete_Expression_Common {
 public:
