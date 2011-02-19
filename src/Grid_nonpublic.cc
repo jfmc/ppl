1fa1d0105f4f01ac65cebabf4c8da4e7656d432c
Marco Poletti <poletti.marco@gmail.com>
2011-02-19 14:59:29 +0100

    Grid: remove a useless cast in set_empty().

--- src/Grid_nonpublic.cc
+++ src/Grid_nonpublic.cc
@@ -458,7 +458,7 @@ PPL::Grid::set_empty() {
   // dimension and then swap it with `con_sys'.
   Congruence_System cgs(Congruence::zero_dim_false());
   cgs.set_space_dimension(space_dim);
-  const_cast<Congruence_System&>(con_sys).swap(cgs);
+  std::swap(con_sys, cgs);
 }
 
 void
