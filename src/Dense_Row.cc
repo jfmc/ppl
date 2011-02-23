jfmc/ppl

commit 0db87bcbd172939fbe34db0f98f2d2cc8f1e0646

A Row_Flags field is now contained in Linear_Row, instead of Sparse_Row and Dense_Row.

src/Dense_Row.cc
@@ -244,7 +244,6 @@ PPL::Dense_Row::Dense_Row(const Sparse_Row& row)
 void
 PPL::Dense_Row::init(const Sparse_Row& row) {
   impl.capacity = row.size();
-  impl.flags = row.flags();
   impl.vec = static_cast<Coefficient*>(
       operator new(sizeof(Coefficient) * impl.capacity));
   Sparse_Row::const_iterator itr = row.begin();
@@ -264,7 +263,6 @@ PPL::Dense_Row::init(const Sparse_Row& row) {
 
 PPL::Dense_Row&
 PPL::Dense_Row::operator=(const Sparse_Row& row) {
-  impl.flags = row.flags();
   if (size() > row.size()) {
     // TODO: If the shrink() is modified to reallocate a smaller chunk,
     // this can be optimized.
@@ -392,9 +390,7 @@ PPL::Dense_Row::ascii_dump(std::ostream& s) const {
   const dimension_type x_size = x.size();
   s << "size " << x_size << " ";
   for (dimension_type i = 0; i < x_size; ++i)
-    s << x[i] << ' ';
-  s << "f ";
-  flags().ascii_dump(s);
+    s << x[i];
   s << "\n";
 }
 
@@ -414,9 +410,8 @@ PPL::Dense_Row::ascii_load(std::istream& s) {
   for (dimension_type col = 0; col < new_size; ++col)
     if (!(s >> (*this)[col]))
       return false;
-  if (!(s >> str) || str != "f")
-    return false;
-  return impl.flags.ascii_load(s);
+
+  return true;
 }
 
 PPL::memory_size_type
@@ -506,9 +501,6 @@ PPL::operator==(const Dense_Row& x, const Dense_Row& y) {
   const dimension_type y_size = y.size();
   
   if (x_size != y_size)
-    return false;
-
-  if (x.flags() != y.flags())
     return false;
 
   for (dimension_type i = x_size; i-- > 0; )

src/Congruence.cc
@@ -287,7 +287,7 @@ PPL::Congruence::ascii_load(std::istream& s) {
   if (new_size < old_size)
     x.shrink(new_size);
   else if (new_size > old_size) {
-    Dense_Row y(new_size, Dense_Row::Flags());
+    Dense_Row y(new_size);
     x.swap(y);
   }
 

src/Congruence.inlines.hh
@@ -33,7 +33,7 @@ namespace Parma_Polyhedra_Library {
 
 inline
 Congruence::Congruence(dimension_type n)
-  : Dense_Row(n + 2, n + 2, Dense_Row::Flags()) {
+  : Dense_Row(n + 2, n + 2) {
   PPL_ASSERT(OK());
 }
 

src/Dense_Row.defs.hh
@@ -29,7 +29,6 @@ site: http://www.cs.unipr.it/ppl/ . */
 #include "globals.defs.hh"
 
 #include "Sparse_Row.types.hh"
-#include "Row_Flags.defs.hh"
 #include "Coefficient.defs.hh"
 #include <vector>
 #include <limits>
@@ -40,23 +39,19 @@ site: http://www.cs.unipr.it/ppl/ . */
 #endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
 class Parma_Polyhedra_Library::Dense_Row {
 public:
-  typedef Row_Flags Flags;
-
   class iterator;
   class const_iterator;
 
-  //! Pre-constructs a row: construction must be completed by construct().
+  //! Constructs an empty row.
   Dense_Row();
 
   explicit Dense_Row(const Sparse_Row& row);
   
-  explicit Dense_Row(Flags f);
-
   //! Tight constructor: resizing may require reallocation.
   /*!
-    Constructs a row with size and capacity \p sz, and flags \p f.
+    Constructs a row with size and capacity \p sz.
   */
-  Dense_Row(dimension_type sz, Flags f);
+  Dense_Row(dimension_type sz);
 
   //! Sizing constructor with capacity.
   /*!
@@ -66,14 +61,10 @@ public:
     \param capacity
     The capacity of the row that will be constructed;
 
-    \param f
-    Flags for the row that will be constructed.
-
     The row that is constructed has storage for \p capacity elements,
     \p sz of which are default-constructed now.
-    The row flags are set to \p f.
   */
-  Dense_Row(dimension_type sz, dimension_type capacity, Flags f);
+  Dense_Row(dimension_type sz, dimension_type capacity);
 
   //! Ordinary copy constructor.
   Dense_Row(const Dense_Row& y);
@@ -143,12 +134,6 @@ public:
     It is assumed that \p new_size is not greater than the current size.
   */
   void shrink(dimension_type new_size);
-
-  //! Returns the flags of \p *this.
-  const Flags flags() const;
-
-  //! Sets \p f as the flags of \p *this.
-  void set_flags(Flags f);
 
   //! Returns the size() of the largest possible Dense_Row.
   static dimension_type max_size();
@@ -406,9 +391,6 @@ private:
     
     //! The capacity of the row.
     dimension_type capacity;
-
-    //! The flags of this row.
-    Row_Flags flags;
 
     //! The vector of coefficients.
     //! An empty vector may be stored as NULL instead of using a valid pointer.

src/Dense_Row.inlines.hh
@@ -37,7 +37,7 @@ namespace Parma_Polyhedra_Library {
 
 inline
 Dense_Row::Impl::Impl()
-  : size(0), capacity(0), flags(), vec(0) {
+  : size(0), capacity(0), vec(0) {
 }
 
 inline
@@ -59,16 +59,6 @@ Dense_Row::size() const {
   return impl.size;
 }
 
-inline const Dense_Row::Flags
-Dense_Row::flags() const {
-  return impl.flags;
-}
-
-inline void
-Dense_Row::set_flags(Dense_Row::Flags f) {
-  impl.flags = f;
-}
-
 inline dimension_type
 Dense_Row::capacity() const {
   return impl.capacity;
@@ -82,21 +72,10 @@ Dense_Row::Dense_Row()
 }
 
 inline
-Dense_Row::Dense_Row(Flags f)
-  : impl() {
-  
-  impl.flags = f;
-
-  PPL_ASSERT(OK());
-}
-
-inline
 Dense_Row::Dense_Row(const dimension_type sz,
-                     const dimension_type capacity,
-                     const Flags f)
+                     const dimension_type capacity)
   : impl() {
 
-  impl.flags = f;
   resize(sz, capacity);
 
   PPL_ASSERT(size() == sz);
@@ -105,10 +84,9 @@ Dense_Row::Dense_Row(const dimension_type sz,
 }
 
 inline
-Dense_Row::Dense_Row(const dimension_type sz, const Flags f)
+Dense_Row::Dense_Row(const dimension_type sz)
   : impl() {
 
-  impl.flags = f;
   resize(sz);
 
   PPL_ASSERT(size() == sz);
@@ -118,8 +96,6 @@ Dense_Row::Dense_Row(const dimension_type sz, const Flags f)
 inline
 Dense_Row::Dense_Row(const Dense_Row& y)
   : impl() {
-
-  impl.flags = y.flags();
 
   if (y.impl.vec != 0) {
     impl.capacity = y.capacity();
@@ -141,8 +117,6 @@ Dense_Row::Dense_Row(const Dense_Row& y,
   : impl() {
   PPL_ASSERT(y.size() <= capacity);
   PPL_ASSERT(capacity <= max_size());
-
-  impl.flags = y.flags();
 
   impl.vec = static_cast<Coefficient*>(
       operator new(sizeof(Coefficient) * capacity));
@@ -168,8 +142,6 @@ Dense_Row::Dense_Row(const Dense_Row& y,
   PPL_ASSERT(sz <= capacity);
   PPL_ASSERT(capacity <= max_size());
   PPL_ASSERT(capacity != 0);
-  
-  impl.flags = y.flags();
   
   impl.vec = static_cast<Coefficient*>(
       operator new(sizeof(Coefficient) * capacity));
@@ -205,7 +177,6 @@ inline void
 Dense_Row::swap(Dense_Row& y) {
   std::swap(impl.size, y.impl.size);
   std::swap(impl.capacity, y.impl.capacity);
-  std::swap(impl.flags, y.impl.flags);
   std::swap(impl.vec, y.impl.vec);
   PPL_ASSERT(OK());
   PPL_ASSERT(y.OK());

src/Grid_Generator.cc
@@ -158,7 +158,7 @@ PPL::Grid_Generator::ascii_load(std::istream& s) {
   if (new_size < old_size)
     x.shrink(new_size);
   else if (new_size > old_size) {
-    Dense_Row y(new_size, Dense_Row::Flags());
+    Dense_Row y(new_size);
     x.swap(y);
   }
 

src/Linear_Row.cc
@@ -177,7 +177,7 @@ PPL::Linear_Row::Flags::ascii_load(std::istream& s) {
     if (!(s >> str))
       return false;
     if (str[0] == '+')
-      set_bits(1 << (Dense_Row::Flags::first_free_bit + bit));
+      set_bits(1 << (Row_Flags::first_free_bit + bit));
     else if (str[0] != '-')
       return false;
     if (str.compare(1, strlen(bit_names[bit]), bit_names[bit]) != 0)
@@ -214,7 +214,7 @@ PPL::Linear_Row::ascii_load(std::istream& s) {
   if (new_size < old_size)
     x.shrink(new_size);
   else if (new_size > old_size) {
-    Dense_Row y(new_size, Dense_Row::Flags());
+    Dense_Row y(new_size);
     x.swap(y);
   }
 
@@ -224,10 +224,8 @@ PPL::Linear_Row::ascii_load(std::istream& s) {
   if (!(s >> str) || str != "f")
     return false;
   
-  Flags f;
-  if (!f.ascii_load(s))
+  if (!flags_.ascii_load(s))
     return false;
-  set_flags(f);
   
   return true;
 }

src/Linear_Row.defs.hh
@@ -26,6 +26,7 @@ site: http://www.cs.unipr.it/ppl/ . */
 
 #include "Linear_Row.types.hh"
 #include "globals.defs.hh"
+#include "Row_Flags.defs.hh"
 #include "Dense_Row.defs.hh"
 #include "Topology.hh"
 #include "Linear_Expression.types.hh"
@@ -141,7 +142,7 @@ public:
     of a Linear_Row object.
   */
 #endif // defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
-  class Flags : public Dense_Row::Flags {
+  class Flags : public Row_Flags {
   public:
     //! Default constructor: builds an object where all flags are invalid.
     Flags();
@@ -189,26 +190,26 @@ public:
     //! Builds the type from a bit-mask.
     explicit Flags(base_type mask);
     
-    //! Constructor from a Dense_Row::Flags object.
-    explicit Flags(Dense_Row::Flags flags);
+    //! Constructor from a Row_Flags object.
+    explicit Flags(Row_Flags flags);
 
     //! \name The bits that are currently in use
     //@{
     // NB: ascii_load assumes that these are sequential.
     static const unsigned rpi_validity_bit
-    = Dense_Row::Flags::first_free_bit + 0;
+    = Row_Flags::first_free_bit + 0;
     static const unsigned rpi_bit
-    = Dense_Row::Flags::first_free_bit + 1;
+    = Row_Flags::first_free_bit + 1;
     static const unsigned nnc_validity_bit
-    = Dense_Row::Flags::first_free_bit + 2;
+    = Row_Flags::first_free_bit + 2;
     static const unsigned nnc_bit
-    = Dense_Row::Flags::first_free_bit + 3;
+    = Row_Flags::first_free_bit + 3;
     //@}
 
   protected:
     //! Index of the first bit derived classes can use.
     static const unsigned first_free_bit
-    = Dense_Row::Flags::first_free_bit + 4;
+    = Row_Flags::first_free_bit + 4;
 
     friend class Parma_Polyhedra_Library::Linear_Row;
   };
@@ -240,6 +241,12 @@ public:
 
   //! Destructor.
   ~Linear_Row();
+
+  //! Swaps \p *this with \p y .
+  void swap(Linear_Row& y);
+
+  //! Swaps the i-th and j-th elements of the row.
+  void swap(dimension_type i, dimension_type j);
 
   //! \name Flags inspection methods
   //@{
@@ -377,6 +384,9 @@ public:
   bool OK(dimension_type row_size) const;
 
 private:
+
+  Flags flags_;
+
   friend class Parma_Polyhedra_Library::Linear_Expression;
   friend class Parma_Polyhedra_Library::Constraint;
   friend class Parma_Polyhedra_Library::Generator;

commit 626802ef4576906224ee719a836d9649cf91ac0c

Dense_Row: fix regression in ascii_dump(), introduced in commit 0db87bcb.

src/Dense_Row.cc
@@ -390,7 +390,7 @@ PPL::Dense_Row::ascii_dump(std::ostream& s) const {
   const dimension_type x_size = x.size();
   s << "size " << x_size << " ";
   for (dimension_type i = 0; i < x_size; ++i)
-    s << x[i];
+    s << x[i] << ' ';
   s << "\n";
 }
 