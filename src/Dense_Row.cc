3d70dcea0e43df2337c919dc4bf3680955ba0e44
Marco Poletti <poletti.marco@gmail.com>
2011-10-30 19:37:20 +0100

    Dense_Row: the linear_combine() methods now detect when coeff1==1 and save some work.

--- src/Dense_Row.cc
+++ src/Dense_Row.cc
@@ -380,14 +380,27 @@ PPL::Dense_Row::linear_combine(const Dense_Row& y,
                                Coefficient_traits::const_reference coeff2) {
   Dense_Row& x = *this;
   PPL_ASSERT(x.size() == y.size());
-  for (dimension_type i = x.size(); i-- > 0; ) {
-    Coefficient& x_i = x[i];
-    x_i *= coeff1;
-    // The test against 0 gives rise to a consistent speed up: see
-    // http://www.cs.unipr.it/pipermail/ppl-devel/2009-February/014000.html
-    Coefficient_traits::const_reference y_i = y[i];
-    if (y_i != 0)
-      add_mul_assign(x_i, y_i, coeff2);
+
+  if (coeff1 == 1) {
+    // Optimized implementation.
+    for (dimension_type i = x.size(); i-- > 0; ) {
+      Coefficient& x_i = x[i];
+      // The test against 0 gives rise to a consistent speed up: see
+      // http://www.cs.unipr.it/pipermail/ppl-devel/2009-February/014000.html
+      Coefficient_traits::const_reference y_i = y[i];
+      if (y_i != 0)
+        add_mul_assign(x_i, y_i, coeff2);
+    }
+  } else {
+    for (dimension_type i = x.size(); i-- > 0; ) {
+      Coefficient& x_i = x[i];
+      x_i *= coeff1;
+      // The test against 0 gives rise to a consistent speed up: see
+      // http://www.cs.unipr.it/pipermail/ppl-devel/2009-February/014000.html
+      Coefficient_traits::const_reference y_i = y[i];
+      if (y_i != 0)
+        add_mul_assign(x_i, y_i, coeff2);
+    }
   }
 }
 
@@ -400,14 +413,27 @@ PPL::Dense_Row::linear_combine(const Dense_Row& y,
   PPL_ASSERT(start <= end);
   PPL_ASSERT(end <= x.size());
   PPL_ASSERT(end <= y.size());
-  for (dimension_type i = start; i < end; ++i) {
-    Coefficient& x_i = x[i];
-    x_i *= coeff1;
-    // The test against 0 gives rise to a consistent speed up: see
-    // http://www.cs.unipr.it/pipermail/ppl-devel/2009-February/014000.html
-    Coefficient_traits::const_reference y_i = y[i];
-    if (y_i != 0)
-      add_mul_assign(x_i, y_i, coeff2);
+
+  if (coeff1 == 1) {
+    // Optimized implementation.
+    for (dimension_type i = start; i < end; ++i) {
+      Coefficient& x_i = x[i];
+      // The test against 0 gives rise to a consistent speed up: see
+      // http://www.cs.unipr.it/pipermail/ppl-devel/2009-February/014000.html
+      Coefficient_traits::const_reference y_i = y[i];
+      if (y_i != 0)
+        add_mul_assign(x_i, y_i, coeff2);
+    }
+  } else {
+    for (dimension_type i = start; i < end; ++i) {
+      Coefficient& x_i = x[i];
+      x_i *= coeff1;
+      // The test against 0 gives rise to a consistent speed up: see
+      // http://www.cs.unipr.it/pipermail/ppl-devel/2009-February/014000.html
+      Coefficient_traits::const_reference y_i = y[i];
+      if (y_i != 0)
+        add_mul_assign(x_i, y_i, coeff2);
+    }
   }
 }
 
