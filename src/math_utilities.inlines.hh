efed831cda3f447da48885d3a3f52282d5abc4a4
Abramo Bagnara <abramo.bagnara@gmail.com>
2011-11-03 19:57:23 +0100

    Simplified implementation.

--- src/math_utilities.inlines.hh
+++ src/math_utilities.inlines.hh
@@ -44,7 +44,7 @@ template <typename T>
 inline T
 low_bits_mask(const unsigned n) {
   PPL_ASSERT(n < unsigned(std::numeric_limits<T>::digits));
-  return (n == 0) ? 0 : ~(~(T(0u)) << n);
+  return ~((~static_cast<T>(0)) << n);
 }
 
 template <typename T>
