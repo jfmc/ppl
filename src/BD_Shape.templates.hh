d61bdb7283e69847b9dfb79e33ac13ef15ac50eb
Marco Poletti <poletti.marco@gmail.com>
2011-11-13 14:49:08 +0100

    BD_Shape: optimize bounded_affine_image().

--- src/BD_Shape.templates.hh
+++ src/BD_Shape.templates.hh
@@ -4423,37 +4423,39 @@ BD_Shape<T>
   // Speculative allocation of temporaries to be used in the following loop.
   PPL_DIRTY_TEMP(N, coeff_i);
   PPL_DIRTY_TEMP_COEFFICIENT(minus_sc_i);
-  // TODO: This loop can be optimized more, if needed.
   // Note: indices above `w' can be disregarded, as they all have
   // a zero coefficient in `sc_expr'.
-  for (dimension_type i = w; i > 0; --i) {
-    const Coefficient& sc_i = sc_expr.get(i);
+  for (Linear_Expression::const_iterator i = sc_expr.begin(),
+        i_end = sc_expr.lower_bound(Variable(w)); i != i_end; ++i) {
+    const Coefficient& sc_i = *i;
+    const dimension_type i_dim = i.variable().space_dimension();
     const int sign_i = sgn(sc_i);
     if (sign_i > 0) {
       assign_r(coeff_i, sc_i, ROUND_UP);
       // Approximating `sc_expr'.
       if (pos_pinf_count <= 1) {
-        const N& up_approx_i = dbm_0[i];
+        const N& up_approx_i = dbm_0[i_dim];
         if (!is_plus_infinity(up_approx_i))
           add_mul_assign_r(pos_sum, coeff_i, up_approx_i, ROUND_UP);
         else {
           ++pos_pinf_count;
-          pos_pinf_index = i;
+          pos_pinf_index = i_dim;
         }
       }
     }
-    else if (sign_i < 0) {
+    else {
+      PPL_ASSERT(sign_i < 0);
       neg_assign(minus_sc_i, sc_i);
       // Note: using temporary named `coeff_i' to store -coeff_i.
       assign_r(coeff_i, minus_sc_i, ROUND_UP);
       // Approximating `sc_expr'.
       if (pos_pinf_count <= 1) {
-        const N& up_approx_minus_i = dbm[i][0];
+        const N& up_approx_minus_i = dbm[i_dim][0];
         if (!is_plus_infinity(up_approx_minus_i))
           add_mul_assign_r(pos_sum, coeff_i, up_approx_minus_i, ROUND_UP);
         else {
           ++pos_pinf_count;
-          pos_pinf_index = i;
+          pos_pinf_index = i_dim;
         }
       }
     }
