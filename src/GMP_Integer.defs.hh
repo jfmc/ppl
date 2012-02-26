cb5de9a40b4ce3ccde1104e90da8a154ca877607
Enea Zaffanella <zaffanella@cs.unipr.it>
2012-02-26 14:09:45 +0100

    Get rid of redundant declarations of a couple of functions. Detected by ECLAIR service funtypes.

--- src/GMP_Integer.defs.hh
+++ src/GMP_Integer.defs.hh
@@ -43,23 +43,6 @@ mpz_class& raw_value(GMP_Integer& x);
 
 //@} // Accessor Functions
 
-//! \name Memory Size Inspection Functions
-//@{
-
-#ifndef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
-//! Returns the total size in bytes of the memory occupied by \p x.
-/*! \relates GMP_Integer */
-#endif // !defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
-memory_size_type total_memory_in_bytes(const GMP_Integer& x);
-
-#ifndef PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS
-//! Returns the size in bytes of the memory managed by \p x.
-/*! \relates GMP_Integer */
-#endif // !defined(PPL_DOXYGEN_INCLUDE_IMPLEMENTATION_DETAILS)
-memory_size_type external_memory_in_bytes(const GMP_Integer& x);
-
-//@} // Memory Size Inspection Functions
-
 //! \name Arithmetic Operators
 //@{
 
