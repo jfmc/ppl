35df86fca704f42c4979121633613a86ce86cda7
Enea Zaffanella <zaffanella@cs.unipr.it>
2012-03-12 11:38:03 +0100

    Prefer testing explicitly against zero. Detected by ECLAIR service utypflag.

--- src/BDS_Status.inlines.hh
+++ src/BDS_Status.inlines.hh
@@ -47,7 +47,7 @@ BD_Shape<T>::Status::test_all(flags_t mask) const {
 template <typename T>
 inline bool
 BD_Shape<T>::Status::test_any(flags_t mask) const {
-  return flags & mask;
+  return (flags & mask) != 0;
 }
 
 template <typename T>
