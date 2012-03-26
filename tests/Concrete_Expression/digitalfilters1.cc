4d9e4e176fb7479cb0160ec8a13571744f39c272
Roberto Bagnara <bagnara@cs.unipr.it>
2012-03-26 15:43:32 +0200

    Do not rely on doubles being supported.

--- tests/Concrete_Expression/digitalfilters1.cc
+++ tests/Concrete_Expression/digitalfilters1.cc
@@ -70,8 +70,8 @@ public:
 };
 
 using namespace Parma_Polyhedra_Library::IO_Operators;
-Concrete_Expression_Type FP_Type =
-  Concrete_Expression_Type::floating_point(ANALYZED_FP_FORMAT);
+Concrete_Expression_Type FP_Type
+= Concrete_Expression_Type::floating_point(ANALYZED_FP_FORMAT);
 
 typedef Integer_Interval Int_Interval;
 
