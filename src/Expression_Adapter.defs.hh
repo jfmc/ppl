079da25d79e9c5a4f6773efa6b77811dc25d2924
Roberto Bagnara <bagnara@cs.unipr.it>
2012-08-16 11:58:16 +0200

    Use a standard comment header block. Detected by ECLAIR service cmntfile.

--- src/Expression_Adapter.defs.hh
+++ src/Expression_Adapter.defs.hh
@@ -1,4 +1,5 @@
 /* Expression_Adapter class declaration.
+   Copyright (C) 2001-2010 Roberto Bagnara <bagnara@cs.unipr.it>
    Copyright (C) 2010-2012 BUGSENG srl (http://bugseng.com)
 
 This file is part of the Parma Polyhedra Library (PPL).
