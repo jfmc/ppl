1b2d7f553fd8910db6b419d0012b9595d8220ea6
Roberto Bagnara <roberto.bagnara@bugseng.com>
2013-10-28 08:43:44 +0100

    Improved.

--- tests/Polyhedron/postimeelapse1.cc
+++ tests/Polyhedron/postimeelapse1.cc
@@ -115,7 +115,7 @@ test03() {
   known_result.add_constraint(y <= 1);
 
   bool ok = (ph1 == known_result);
-  
+
   print_generators(ph1, "*** ph1_positive_time_elapse_assign(ph2) ***");
 
   return ok;
@@ -150,11 +150,11 @@ test04() {
   known_result.add_constraint(y <= 1);
 
   bool ok = (ph1 == known_result);
-  
+
   return ok;
 }
 
-// Positive time elapse between: true and a point
+// Positive time elapse between the universe and a point.
 bool
 test05() {
   Variable x(0);
@@ -176,11 +176,11 @@ test05() {
   NNC_Polyhedron known_result(2);
 
   bool ok = (ph1 == known_result);
-  
+
   return ok;
 }
 
-// Positive time elapse between: a point and true 
+// Positive time elapse between a point and the universe.
 bool
 test06() {
   Variable x(0);
@@ -205,7 +205,8 @@ test06() {
   return ok;
 }
 
-// Positive time elapse between: two rational points with different denominators
+// Positive time elapse between two rational points with different
+// denominators.
 bool
 test07() {
   Variable x(0);
@@ -233,7 +234,7 @@ test07() {
 }
 
 
-// Positive time elapse between: Polyhedra with two different dimension
+// Positive time elapse between Polyhedra with two different dimensions.
 bool
 test08() {
   Variable x(0);
@@ -250,7 +251,7 @@ test08() {
   print_constraints(ph2, "*** ph2 ***");
 
   bool known_result = false;
-  
+
   try
   {
     ph1.positive_time_elapse_assign(ph2);
@@ -260,13 +261,14 @@ test08() {
   {
     known_result=true;
   }
-  
+
   bool ok = (known_result == true);
   return ok;
 }
 
 
-// Positive time elapse between: two zero-dimensional polyhedra where the second one is EMPTY
+// Positive time elapse between two zero-dimensional polyhedra where
+// the second one is empty.
 bool
 test09() {
   Variable x(0);
@@ -287,7 +289,7 @@ test09() {
   return ok;
 }
 
-// Positive time elapse between: a polyhedron and FALSE (the empty polyhedron) 
+// Positive time elapse between a polyhedron and the empty polyhedron.
 bool
 test10() {
   Variable x(0);
@@ -310,7 +312,7 @@ test10() {
   return ok;
 }
 
-// Positive time elapse between: FALSE and a polyhedron
+// Positive time elapse between the empty polyhedron and another polyhedron.
 bool
 test11() {
   Variable x(0);
@@ -333,29 +335,29 @@ test11() {
   return ok;
 }
 
-// Positive time elapse between: an empty polyhedron with non-processed pending constraints
-// and a normal polyhedron
+// Positive time elapse between an empty polyhedron with non-processed
+// pending constraints and a normal polyhedron.
 bool
 test12() {
   Variable x(0);
   Variable y(1);
-  
+
   NNC_Polyhedron ph1(2);
   ph1.add_constraint(x >= 255);
   ph1.add_constraint(x <= 257);
   ph1.add_constraint(y >= 255);
   ph1.add_constraint(y <= 257);
- 
+
   Variables_Set vars(x, y);
 
-  // Defines constraints of an EMPTY Polyhedron
+  // Defines constraints of an empty polyhedron.
   Constraint_System cs;
   cs.insert(x > y);
   cs.insert(x < y);
 
-  // Adding pending constraints
+  // Adding pending constraints.
   ph1.wrap_assign(vars, BITS_8, UNSIGNED, OVERFLOW_WRAPS, &cs, 16, false);
-  
+
   NNC_Polyhedron ph2(2);
   ph2.add_constraint(x > 0);
   ph2.add_constraint(y > 1);
@@ -371,12 +373,13 @@ test12() {
   return ok;
 }
 
-// Positive time elapse between: a normal polyhedron and an empty polyhedron with non-processed pending constraints
+// Positive time elapse between a normal polyhedron and an empty
+// polyhedron with non-processed pending constraints.
 bool
 test13() {
   Variable x(0);
   Variable y(1);
-  
+
   NNC_Polyhedron ph1(2);
   ph1.add_constraint(x > 0);
   ph1.add_constraint(y > 1);
@@ -386,17 +389,17 @@ test13() {
   ph2.add_constraint(x <= 257);
   ph2.add_constraint(y >= 255);
   ph2.add_constraint(y <= 257);
- 
+
   Variables_Set vars(x, y);
 
   // Defines constraints of an EMPTY Polyhedron
   Constraint_System cs;
   cs.insert(x > y);
   cs.insert(x < y);
-  
+
   // Adding pending constraints
   ph2.wrap_assign(vars, BITS_8, UNSIGNED, OVERFLOW_WRAPS, &cs, 16, true);
-  
+
   ph1.positive_time_elapse_assign(ph2);
 
   print_constraints(ph1, "*** ph1 ***");
