c9b20bc27d2a9931f131b6c3350eadf3c7f33405
Roberto Bagnara <roberto.bagnara@bugseng.com>
2015-11-24 10:05:32 +0100

    Adapted to SWI-Prolog version 7.x.

--- interfaces/Prolog/SWI/pl_clpq.pl
+++ interfaces/Prolog/SWI/pl_clpq.pl
@@ -21,7 +21,8 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02111-1307, USA.
 For the most up-to-date information see the Parma Polyhedra Library
 site: http://bugseng.com/products/ppl/ . */
 
-eat_eol.
+eat_eol :-
+        get_code(user_input, _EOL).
 
 main :-
   % FIXME: clearing the prompt as it is done here is a temporary workaround.
